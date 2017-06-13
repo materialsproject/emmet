e44f13a31fba969cff4e203fd4b1ef4c3f173361
montoyjh <montoyjh@gmail.com>
2017-06-13 15:07:44 -0700

    add initial testing framework

---
+++ emmet/tests/test_submissions.py
@@ -0,0 +1,19 @@
+import unittest2 as unittest
+
+def MpSubmissionFilterTest(unittest.TestCase):
+    
+    def setUp(self):
+        # Set up test db, set up mpsft, etc.
+        pass
+
+    def test_init(self):
+        pass
+
+    def test_check_for_duplicates(self):
+        pass
+
+    def test_submit(self):
+        pass
+
+    def test_add_snl_to_submissions(self):
+        pass
