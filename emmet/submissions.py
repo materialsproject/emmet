71796e2c3b92de206729ea46b645a98a54344d5b
montoyjh <montoyjh@gmail.com>
2017-06-14 12:42:04 -0700

    update submissions

--- emmet/submissions.py
+++ emmet/submissions.py
@@ -2,19 +2,24 @@ from atomate import SubmissionFilter
 
 class MPSubmissionFilter(SubmissionFilter):
     """
-    Minimal filter that takes an SNL, checks
-    against a database of previous submissions,
-    generates a set of production workflows, 
-    and adds those 
+    Minimal filter that takes an SNL, checks against 
+    a database of previous submissions, optionally
+    checks against a materials collection, generates
+    a set of production workflows, adds them to a
+    launchpad
     """
 
     # Set of default matching params
     default_match_params = {}
 
-    def init(self, submission_db_file=None):
-        pass
+    def init(self, submission_coll=None, materials_coll=None):
+        """
+        """
+        self.submission_coll = submission_coll
+        self.materials_coll = materials_coll
 
-    def check_for_duplicate(self, snl, match_params=None):
+    @staticmethod
+    def find_duplicate(snl, collection, match_params=None):
         """
         Args:
             structure (Structure): struct object to attempt
@@ -23,15 +28,30 @@ class MPSubmissionFilter(SubmissionFilter):
                 for structure matcher
         """
         match_params = match_params or default_match_params
+        sm = StructureMatcher(**match_params)
+        formula_matches = collection.find({
+            "pretty_formula": snl.composition.reduced_formula},
+            {"snl": 1, "submission_id": 1})
+        for formula_match in formula_matches:
+            prev_snl = formula_match["snl"]
+            if sm.fit(prev_snl, snl):
+                return formula_match
+        return None
 
-    def add_snl_to_submission_db(self, snl):
+    def add_submission(self, snl):
+        """
+        Submission document is minimal, simply an SNL and formula
+        """
+        snl_doc = {"snl":snl.as_dict(), 
+                   "pretty_formula": snl.composition.reduced_formula}
+        return self.submissions_coll.insert(snl_doc)
+
+    def update_submission(self, prev_submission_id, new_snl):
+        self.submissions_coll.update({"submission_id": prev_submission_id},
+                                     {"$push": {"submitted": new_snl}})
+
+    def submit(snl, dupecheck=True, match_params=None, launchpad=None):
         """
         TODO: Add this docstring
         """
-        pass
 
-    def submit(snl, match_params=None, launchpad=None):
-        """
-        TODO: Add this docstring
-        """
-        pass
