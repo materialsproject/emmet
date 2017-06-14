b44353afa96c1c066e7c30a206a69ca16ee4c471
montoyjh <montoyjh@gmail.com>
2017-06-14 16:45:35 -0700

    update setup, add test files

--- emmet/tests/test_submissions.py
+++ emmet/tests/test_submissions.py
@@ -1,13 +1,22 @@
 import unittest2 as unittest
 
+__author__ = "Joey Montoya"
+__email__ = "montoyjh@lbl.gov"
+
+module_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+db_cred = loadfn(os.path.join(module_dir, "..", "..", "..", "test_files"))
+
 def MpSubmissionFilterTest(unittest.TestCase):
     
     def setUp(self):
         # Set up test db, set up mpsft, etc.
-        pass
+        conn = MongoClient(db_cred["database"], db_cred["host"], db_cred["port"])
+        submissions_coll = conn.submissions
+        materials_coll = conn.materials
+        self.mpsft = 
 
     def test_init(self):
-        pass
+        
 
     def test_check_for_duplicates(self):
         pass
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == "__main__":
+    setup(
+        name='emmet',
+        version='0.1',
+        description='Emmet is a builder framework for the Materials Project',
+        long_description=open(os.path.join(module_dir, 'README.md')).read(),
+        url='https://github.com/materialsproject/emmet',
+        #author='Anubhav Jain, Kiran Mathew',
+        #author_email='anubhavster@gmail.com, kmathew@lbl.gov',
+        license='modified BSD',
+        packages=find_packages(),
+        package_data={},
+        zip_safe=False,
+        # TODO: finalize requirements
+        install_requires=['FireWorks>=1.4.0', 'pymatgen>=4.7.1', 'pymatgen-db>=0.5.1', 
+                          'monty>=0.9.5', 'six'],
+        classifiers=['Programming Language :: Python :: 2.7',
+                     "Programming Language :: Python :: 3",
+                     "Programming Language :: Python :: 3.5",
+                     'Development Status :: 4 - Beta',
+                     'Intended Audience :: Science/Research',
+                     'Intended Audience :: System Administrators',
+                     'Intended Audience :: Information Technology',
+                     'Operating System :: OS Independent',
+                     'Topic :: Other/Nonlisted Topic',
+                     'Topic :: Scientific/Engineering'],
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        scripts=[os.path.join('scripts', f) for f in os.listdir(os.path.join(module_dir, 'scripts'))]
+    )
