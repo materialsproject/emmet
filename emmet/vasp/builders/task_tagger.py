6ff9d666543c3e1ad154188d6d268aa078dd0257
shyamd <shyamd@lbl.gov>
2017-06-15 10:41:47 -0700

    PEP cleanup

--- emmet/vasp/builders/task_tagger.py
+++ emmet/vasp/builders/task_tagger.py
@@ -2,7 +2,7 @@ from maggma.builder import Builder
 
 
 class TaskTagger(Builder):
-    def __init__(self, tasks ,tag_defs, tags, **kwargs):
+    def __init__(self, tasks, tag_defs, tags, **kwargs):
         """
         Initialize the builder the framework.
 
@@ -15,10 +15,9 @@ class TaskTagger(Builder):
         self.tags = tags
         self.tag_defs = tag_defs
 
-        super().__init__(sources=[tasks,tag_defs],
+        super().__init__(sources=[tasks, tag_defs],
                          targets=[tags],
                          **kwargs)
-
 
     def get_items(self):
         """
@@ -34,7 +33,7 @@ class TaskTagger(Builder):
         if self.tag_defs.last_updated() > self.tags.last_updated():
             to_process = set(all_task_ids)
         else:
-            previous_task_ids = self.tags.distinct("task_id")
+            previous_task_ids = self.tags.distinct("task_id", {"task_type": {"$exists": 0}})
             to_process = set(all_task_ids) - set(previous_task_ids)
 
         tag_defs = list(self.tag_defs.find())
@@ -42,7 +41,7 @@ class TaskTagger(Builder):
         for t_id in to_process:
             print("Processing task_id: {}".format(t_id))
             try:
-                yield {"task_doc": self.tasks.find_one({"task_id":t_id}),
+                yield {"task_doc": self.tasks.find_one({"task_id": t_id}),
                        "tag_defs": tag_defs}
             except:
                 import traceback
@@ -54,36 +53,33 @@ class TaskTagger(Builder):
         tag_defs = item["tag_defs"]
 
         for tag_def in tag_defs:
-            if self.task_matches_def(task_doc,tag_def):
+            if self.task_matches_def(task_doc, tag_def):
                 return {"task_id": task_doc["task_id"],
                         "task_type": tag_def["task_type"]}
             else
                 pass
         pass
 
-
     def update_targets(self, items):
 
         for doc in items:
-            self.tags.collection.update({'task_id': doc['task_id']},doc,upsert=True)
-
+            self.tags.collection.update({'task_id': doc['task_id']}, doc, upsert=True)
 
     def finalize(self):
         pass
 
-
     # TODO: Add in more sophisticated matching criteria
-    def task_matches_def(self,task_doc,tag_def):
-        for k,v in tag_def['EXACT'].items():
+    def task_matches_def(self, task_doc, tag_def):
+        for k, v in tag_def['EXACT'].items():
             if task_doc['input_orig']['INCAR'].get(k) is not v:
                 return False
 
-        for k,v in tag_def['GREATER'].items():
-            if task_doc['input_orig']['INCAR'].get(k,0) < v:
+        for k, v in tag_def['GREATER'].items():
+            if task_doc['input_orig']['INCAR'].get(k, 0) < v:
                 return False
 
-        for k,v in tag_def['LESS'].items():
-            if task_doc['input_orig']['INCAR'].get(k,0) > v:
+        for k, v in tag_def['LESS'].items():
+            if task_doc['input_orig']['INCAR'].get(k, 0) > v:
                 return False
 
         return True
