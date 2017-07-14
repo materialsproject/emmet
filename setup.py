e90ba0967b70ae37fbf13f6eb7119a556e49593a
Donny Winston <dwinston@alum.mit.edu>
2017-07-14 15:01:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
         zip_safe=False,
         # TODO: finalize requirements
         install_requires=[
-            'FireWorks>=1.4.0', 'pymatgen>=4.7.1', 'pymatgen-db>=0.5.1',
-            'maggma', 'monty>=0.9.5', 'six', 'pydash', 'atomate',
+            'atomate', 'FireWorks>=1.4.0', 'pymatgen>=2017.7.4', 'pymatgen-db>=0.5.1',
+            'maggma', 'monty>=0.9.5', 'six', 'pydash',
         ],
         classifiers=["Programming Language :: Python :: 3",
                      "Programming Language :: Python :: 3.6",
