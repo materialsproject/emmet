4d4c4bb844e36b66580e1423195ed54b16abb9c8
shyamd <shyamd@lbl.gov>
2017-12-14 15:26:59 -0800

    Switch to WebPlotters

--- emmet/vasp/builders/electronic_structure.py
+++ emmet/vasp/builders/electronic_structure.py
@@ -278,3 +278,249 @@ def get_small_plot(bs):
                     new_bands.append(band)
             branch[spin] = new_bands
     return plot_small
+
+
+#
+# Obtain web-friendly images by subclassing pymatgen plotters.
+#
+
+class WebBSPlotter(BSPlotter):
+
+    def get_plot(self, zero_to_efermi=True, ylim=None, smooth=False):
+        """
+        get a matplotlib object for the bandstructure plot.
+        Blue lines are up spin, red lines are down
+        spin.
+        Args:
+            zero_to_efermi: Automatically subtract off the Fermi energy from
+                the eigenvalues and plot (E-Ef).
+            ylim: Specify the y-axis (energy) limits; by default None let
+                the code choose. It is vbm-4 and cbm+4 if insulator
+                efermi-10 and efermi+10 if metal
+            smooth: interpolates the bands by a spline cubic
+        """
+
+        plt = pretty_plot(6, 5.5)  # Was 12, 8
+
+        rc('text', usetex=True)
+
+        width = 4
+        ticksize = int(width * 2.5)
+        axes = plt.gca()
+        axes.set_title(axes.get_title(), size=width * 4)
+        labelsize = int(width * 3)
+        axes.set_xlabel(axes.get_xlabel(), size=labelsize)
+        axes.set_ylabel(axes.get_ylabel(), size=labelsize)
+
+        plt.xticks(fontsize=ticksize)
+        plt.yticks(fontsize=ticksize)
+
+        for axis in ['top', 'bottom', 'left', 'right']:
+            axes.spines[axis].set_linewidth(0.5)
+
+        # main internal config options
+        e_min = -4
+        e_max = 4
+        if self._bs.is_metal():
+            e_min = -10
+            e_max = 10
+        band_linewidth = 1  # Was 3 in pymatgen
+
+        data = self.bs_plot_data(zero_to_efermi)
+        if not smooth:
+            for d in range(len(data['distances'])):
+                for i in range(self._nb_bands):
+                    plt.plot(data['distances'][d],
+                             [data['energy'][d][str(Spin.up)][i][j]
+                              for j in range(len(data['distances'][d]))], 'b-',
+                             linewidth=band_linewidth)
+                    if self._bs.is_spin_polarized:
+                        plt.plot(data['distances'][d],
+                                 [data['energy'][d][str(Spin.down)][i][j]
+                                  for j in range(len(data['distances'][d]))],
+                                 'r--', linewidth=band_linewidth)
+        else:
+            for d in range(len(data['distances'])):
+                for i in range(self._nb_bands):
+                    tck = scint.splrep(
+                        data['distances'][d],
+                        [data['energy'][d][str(Spin.up)][i][j]
+                         for j in range(len(data['distances'][d]))])
+                    step = (data['distances'][d][-1]
+                            - data['distances'][d][0]) / 1000
+
+                    plt.plot([x * step + data['distances'][d][0]
+                              for x in range(1000)],
+                             [scint.splev(x * step + data['distances'][d][0],
+                                          tck, der=0)
+                              for x in range(1000)], 'b-',
+                             linewidth=band_linewidth)
+
+                    if self._bs.is_spin_polarized:
+
+                        tck = scint.splrep(
+                            data['distances'][d],
+                            [data['energy'][d][str(Spin.down)][i][j]
+                             for j in range(len(data['distances'][d]))])
+                        step = (data['distances'][d][-1]
+                                - data['distances'][d][0]) / 1000
+
+                        plt.plot([x * step + data['distances'][d][0]
+                                  for x in range(1000)],
+                                 [scint.splev(x * step + data['distances'][d][0],
+                                              tck, der=0)
+                                  for x in range(1000)], 'r--',
+                                 linewidth=band_linewidth)
+        self._maketicks(plt)
+
+        # Main X and Y Labels
+        plt.xlabel(r'$\mathrm{Wave\ Vector}$')
+        ylabel = r'$\mathrm{E\ -\ E_f\ (eV)}$' if zero_to_efermi \
+            else r'$\mathrm{Energy\ (eV)}$'
+        plt.ylabel(ylabel)
+
+        # Draw Fermi energy, only if not the zero
+        if not zero_to_efermi:
+            ef = self._bs.efermi
+            plt.axhline(ef, linewidth=2, color='k')
+
+        # X range (K)
+        # last distance point
+        x_max = data['distances'][-1][-1]
+        plt.xlim(0, x_max)
+
+        if ylim is None:
+            if self._bs.is_metal():
+                # Plot A Metal
+                if zero_to_efermi:
+                    plt.ylim(e_min, e_max)
+                else:
+                    plt.ylim(self._bs.efermi + e_min, self._bs._efermi + e_max)
+            else:
+                for cbm in data['cbm']:
+                    plt.scatter(cbm[0], cbm[1], color='r', marker='o', s=100)
+
+                for vbm in data['vbm']:
+                    plt.scatter(vbm[0], vbm[1], color='g', marker='o', s=100)
+                plt.ylim(data['vbm'][0][1] + e_min, data['cbm'][0][1] + e_max)
+        else:
+            plt.ylim(ylim)
+
+        plt.tight_layout()
+
+        return plt
+
+
+class WebDosVertPlotter(DosPlotter):
+
+    def get_plot(self, xlim=None, ylim=None,
+                 plt=None, handle_only=False):
+        """
+        Get a matplotlib plot showing the DOS.
+        Args:
+            xlim: Specifies the x-axis limits. Set to None for automatic
+                determination.
+            ylim: Specifies the y-axis limits.
+            plt: Handle on existing plot.
+            handle_only: Quickly return just a handle. Useful if this method
+                raises an exception so that one can close() the figure.
+        """
+
+        plt = plt or pretty_plot(2, 5.5)
+        if handle_only:
+            return plt
+
+        ncolors = max(3, len(self._doses))
+        ncolors = min(9, ncolors)
+        colors = brewer2mpl.get_map('Set1', 'qualitative', ncolors).mpl_colors
+
+        y = None
+        alldensities = []
+        allenergies = []
+
+        width = 4
+        ticksize = int(width * 2.5)
+        axes = plt.gca()
+        axes.set_title(axes.get_title(), size=width * 4)
+        labelsize = int(width * 3)
+        axes.set_xlabel(axes.get_xlabel(), size=labelsize)
+        axes.set_ylabel(axes.get_ylabel(), size=labelsize)
+        axes.xaxis.labelpad = 6
+
+        # Note that this complicated processing of energies is to allow for
+        # stacked plots in matplotlib.
+        for key, dos in self._doses.items():
+            energies = dos['energies']
+            densities = dos['densities']
+            if not y:
+                y = {Spin.up: np.zeros(energies.shape),
+                     Spin.down: np.zeros(energies.shape)}
+            newdens = {}
+            for spin in [Spin.up, Spin.down]:
+                if spin in densities:
+                    if self.stack:
+                        y[spin] += densities[spin]
+                        newdens[spin] = y[spin].copy()
+                    else:
+                        newdens[spin] = densities[spin]
+            allenergies.append(energies)
+            alldensities.append(newdens)
+
+        keys = list(self._doses.keys())
+        keys.reverse()
+        alldensities.reverse()
+        allenergies.reverse()
+        allpts = []
+        for i, key in enumerate(keys):
+            x = []
+            y = []
+            for spin in [Spin.up, Spin.down]:
+                if spin in alldensities[i]:
+                    densities = list(int(spin) * alldensities[i][spin])
+                    energies = list(allenergies[i])
+                    if spin == Spin.down:
+                        energies.reverse()
+                        densities.reverse()
+                    y.extend(energies)
+                    x.extend(densities)
+            allpts.extend(list(zip(x, y)))
+            if self.stack:
+                plt.fill(x, y, color=colors[i % ncolors],
+                         label=str(key))
+            else:
+                ppl.plot(x, y, color=colors[i % ncolors],
+                         label=str(key), linewidth=1)
+            if not self.zero_at_efermi:
+                xlim = plt.xlim()
+                ppl.plot(xlim, [self._doses[key]['efermi'],
+                                self._doses[key]['efermi']],
+                         color=colors[i % ncolors],
+                         linestyle='--', linewidth=1)
+
+        if ylim:
+            plt.ylim(ylim)
+        if xlim:
+            plt.xlim(xlim)
+        else:
+            ylim = plt.ylim()
+            relevantx = [p[0] for p in allpts
+                         if ylim[0] < p[1] < ylim[1]]
+            plt.xlim(min(relevantx), max(relevantx))
+        if self.zero_at_efermi:
+            xlim = plt.xlim()
+            plt.plot(xlim, [0, 0], 'k--', linewidth=1)
+
+        plt.ylabel(r'$\mathrm{E\ -\ E_f\ (eV)}$')
+        plt.xlabel(r'$\mathrm{Density\ of\ states}$')
+
+        locs, _ = plt.xticks()
+        plt.xticks([0], fontsize=ticksize)
+        plt.yticks(fontsize=ticksize)
+        plt.grid(which='major', axis='y')
+
+        plt.legend(fontsize='x-small',
+                   loc='upper right', bbox_to_anchor=(1.15, 1))
+        leg = plt.gca().get_legend()
+        leg.get_frame().set_alpha(0.25)
+        plt.tight_layout()
+        return plt
