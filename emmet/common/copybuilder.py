2348b33a86912399217989ad2cd5fed1469174e1
Donny Winston <dwinston@alum.mit.edu>
2018-04-17 19:35:34 -0700

    Ensure target.lu_field index; don't try to create index.
    Index may have been already created with different options
    (e.g. unique, background) and will raise
    pymongo.errors.OperationFailure

--- emmet/common/copybuilder.py
+++ emmet/common/copybuilder.py
@@ -22,8 +22,7 @@ class CopyBuilder(Builder):
         lu_filter = source.lu_filter(target)
         self.logger.debug("lu_filter: {}".format(lu_filter))
         confirm_field_index(source, source.lu_field)
-        if isinstance(self.key, str):
-            target.ensure_index(self.key)
+        confirm_field_index(target, target.lu_field)
         confirm_field_index(target, self.key)
         cursor = source.query(criteria=lu_filter,
                               sort=[(source.lu_field, 1)])
