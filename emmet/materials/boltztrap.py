d17de06b95389f3e03d4fcb58affc22f99206349
Francesco Ricci <frankyricci@gmail.com>
2018-04-30 12:32:10 +0200

    Added BoltztrapDosBuilder class

--- emmet/materials/boltztrap.py
+++ emmet/materials/boltztrap.py
@@ -10,6 +10,150 @@ from maggma.builder import Builder
 
 __author__ = "Shyam Dwaraknath <shyamd@lbl.gov>"
 
+class BoltztrapDosBuilder(Builder):
+    def __init__(self, materials, boltztrap, bandstructure_fs="bandstructure_fs", btz_cdos_fs=None, query={}, **kwargs):
+        """
+        Calculates Density of States (DOS) using BoltzTrap
+        Saves the dos object
+
+        Args:
+            materials (Store): Store of materials documents
+            boltztrap (Store): Store of boltztrap
+            bandstructure_fs (str): Name of the GridFS where bandstructures are stored
+            query (dict): dictionary to limit materials to be analyzed
+
+        """
+
+        self.materials = materials
+        self.boltztrap = boltztrap
+        self.bandstructure_fs = bandstructure_fs
+        self.btz_cdos_fs = btz_cdos_fs
+        self.query = query
+
+        super().__init__(sources=[materials],
+                         targets=[boltztrap],
+                         **kwargs)
+
+    def get_items(self):
+        """
+        Gets all materials that need a new DOS
+
+        Returns:
+            generator of materials to calculate DOS
+        """
+
+        self.logger.info("BoltzTrap Dos Builder Started")
+
+        # All relevant materials that have been updated since boltztrap was last run
+        # and a uniform bandstructure exists
+        q = dict(self.query)
+        q.update(self.materials.lu_filter(self.boltztrap))
+        q["bandstructure.uniform_oid"] = {"$exists": 1}
+        #q["output.bandgap"] = {"$gt": 0.0}
+        mats = set(self.materials.distinct(self.materials.key, criteria=q))
+
+        # initialize the gridfs
+        bfs = gridfs.GridFS(self.materials.database, self.bandstructure_fs)
+
+        self.logger.info(
+            "Found {} new materials for calculating boltztrap dos".format(len(mats)))
+        for m in mats:
+            mat = self.materials.query(
+                [self.materials.key, "structure", "input.parameters.NELECT", "bandstructure"], criteria={self.materials.key: m})
+
+            # If a bandstructure oid exists
+            if "uniform_bs_oid" in mat.get("bandstructure", {}):
+                bs_json = bfs.get(mat["bandstructure"][
+                                  "uniform_bs_oid"]).read()
+
+                if "zlib" in mat["bandstructure"].get("uniform_bs_compression", ""):
+                    bs_json = zlib.decompress(bs_json)
+
+                bs_dict = json.loads(bs_json.decode())
+                mat["bandstructure"]["uniform_bs"] = bs_dict
+
+            yield mat
+
+    def process_item(self, item):
+        """
+        Calculates dos running Boltztrap in DOS run mode
+
+        Args:
+            item (dict): a dict with a material_id, bs and a structure
+
+        Returns:
+            cdos: a complete dos object
+        """
+        self.logger.debug(
+            "Calculating Boltztrap for {}".format(item[self.materials.key]))
+
+        nelect = item["input"]["parameters"]["NELECT"]
+
+        bs_dict = item["uniform_bandstructure"]["bs"]
+        bs_dict['structure'] = item['structure']
+        bs = BandStructure.from_dict(bs_dict)
+
+        with ScratchDir("."):
+            if bs.is_spin_polarized:
+                run_path = os.path.join(os.getcwd(), "dos_up")
+                makedirs_p(run_path)
+                BoltztrapRunner(bs=bs, nelec=nelect,run_type="DOS",dos_type="TETRA",
+                                spin=1).run(path_dir=run_path)
+                btrap_dir = os.path.join(run_path, "boltztrap")
+                bta_up = BoltztrapAnalyzer.from_files(btrap_dir)
+                
+                run_path = os.path.join(os.getcwd(), "dos_dw")
+                makedirs_p(run_path)
+                BoltztrapRunner(bs=bs, nelec=nelect,run_type="DOS",dos_type="TETRA",
+                                spin=-1).run(path_dir=run_path)
+                btrap_dir = os.path.join(run_path, "boltztrap")
+                bta_dw = BoltztrapAnalyzer.from_files(btrap_dir)
+                 
+                cdos=an_up.get_complete_dos(bs.structure,an_dw)
+             
+             else:
+                run_path = os.path.join(os.getcwd(), "dos")
+                makedirs_p(run_path)
+                BoltztrapRunner(bs=bs, nelec=nelect,run_type="DOS",
+                                dos_type="TETRA").run(path_dir=run_path)
+                btrap_dir = os.path.join(run_path, "boltztrap")
+                bta = BoltztrapAnalyzer.from_files(btrap_dir)
+
+                cdos = an.get_complete_dos(bs.structure)
+                
+        return {'cdos':cdos.as_dict()}
+
+    def update_targets(self, items):
+        """
+        Inserts the new task_types into the task_types collection
+
+        Args:
+            items ([[dict]]): a list of list of thermo dictionaries to update
+        """
+        items = list(filter(None, items))
+
+        btz_cdos_fs = gridfs.GridFS(self.materials.database,
+                               self.btz_cdos_fs) if self.btz_cdos_fs else None
+
+        if len(items) > 0:
+            self.logger.info("Updating {} boltztrap dos".format(len(items)))
+
+            for doc in items:
+                if self.bta_fs:
+                    btz_dos_doc = dict(doc["cdos"])
+                    btz_dos_json = json.dumps(jsanitize(btz_dos_doc))
+                    btz_dos_gz = zlib.compress(btz_dos_json)
+                    btz_dos_oid = btz_dos_fs.put(btz_dos_gz)
+                    doc['btz_dos_oid'] = btz_dos_oid
+                    doc['btz_dos_compression'] = "zlib"
+
+                del doc["cdos"]
+
+            self.boltztrap.update(items)
+
+        else:
+            self.logger.info("No items to update")
+
 
 class BoltztrapBuilder(Builder):
 
@@ -127,7 +271,7 @@ class BoltztrapBuilder(Builder):
                     bta_doc = dict(doc["bta"])
                     bta_json = json.dumps(jsanitize(bta_doc))
                     bta_gz = zlib.compress(bta_json)
-                    bs_oid = bta_fs.put(bta_gz)
+                    bta_oid = bta_fs.put(bta_gz)
                     doc['bta_oid'] = bta_oid
                     doc['bta_compression'] = "zlib"
 
