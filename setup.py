8728f48c6f4befb6d55483119efa6f540be80c85
Donny Winston <dwinston@alum.mit.edu>
2018-05-02 14:48:28 -0700

    Tell PyPi to parse README as markdown

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
         version='2018.4.2',
         description='Emmet is a builder framework for the Materials Project',
         long_description=open(os.path.join(module_dir, 'README.md'),encoding='utf-8').read(),
+        long_description_content_type="text/markdown",
         url='https://github.com/materialsproject/emmet',
         author='MP team',
         author_email='matproj-develop@googlegroups.com',
