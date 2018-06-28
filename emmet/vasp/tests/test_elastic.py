a63e3e4fbb10f388aff6f4c2ed3fb5c8985eda05
montoyjh <montoyjh@gmail.com>
2018-06-28 10:37:05 -0700

    refactor tests

--- emmet/vasp/tests/test_elastic.py
+++ emmet/vasp/tests/test_elastic.py
@@ -87,49 +87,91 @@ class ElasticAnalysisBuilderTest(unittest.TestCase):
             defo_tasks.append(defo_task)
 
         defo_tasks.pop(0)
-        #opt_task['output']['structure'] = test_struct.as_dict()
         explicit, derived = process_elastic_calcs(opt_task, defo_tasks)
         self.assertEqual(len(explicit), 23)
         self.assertEqual(len(derived), 1)
 
 
-class ElasticAggregateBuilder(unittest.TestCase):
+class ElasticAggregateBuilderTest(unittest.TestCase):
     def setUp(self):
-        self.test_elasticity = MongoStore("test_emmet", "elasticity")
-        self.test_elasticity.connect()
+        # Empty aggregated collection
+        self.test_elasticity_agg = MongoStore("test_emmet", "elasticity_agg")
+        self.test_elasticity_agg.connect()
 
         # Generate test materials collection
         self.test_materials = MongoStore("test_emmet", "materials")
         self.test_materials.connect()
         mat_docs = [{
             "task_id": "mp-{}".format(n),
-            "structure": PymatgenTest.get_structure(formula),
+            "structure": PymatgenTest.get_structure(formula).as_dict(),
             "pretty_formula": formula
         } for n, formula in enumerate(['Si', 'BaNiO3', 'Li2O2', 'TiO2'])]
         self.test_materials.update(mat_docs, update_lu=False)
 
+        # Create elasticity collection and add docs
+        self.test_elasticity = MongoStore("test_emmet", "elasticity",
+                                          key="optimization_task_id")
+        self.test_elasticity.connect()
+
+        si = PymatgenTest.get_structure("Si")
+        doc = {"input_structure": si.copy().as_dict(),
+               "optimization_task_id": "mp-1",
+               "last_updated": datetime.utcnow(),
+               "completed_at": datetime.utcnow(),
+               "optimized_structure": si.copy().as_dict(),
+               "pretty_formula": "Si", "state": "successful"}
+        doc['elastic_tensor'] = [[50, 25, 25, 0, 0, 0],
+                                 [25, 50, 25, 0, 0, 0],
+                                 [25, 25, 50, 0, 0, 0],
+                                 [0, 0, 0, 75, 0, 0],
+                                 [0, 0, 0, 0, 75, 0],
+                                 [0, 0, 0, 0, 0, 75]]
+        self.test_elasticity.update([doc])
+        # Insert second doc with diff params
+        si.perturb(0.005)
+        doc.update({"optimized_structure": si.copy().as_dict(),
+                    "updated_at": datetime.utcnow(),
+                    "optimization_task_id": "mp-5"})
+        self.test_elasticity.update([doc])
+        self.builder = self.get_a_new_builder()
+
     def tearDown(self):
         if not DEBUG_MODE:
             self.test_elasticity.collection.drop()
+            self.test_elasticity_agg.collection.drop()
             self.test_materials.collection.drop()
 
     def test_materials_aggregator(self):
         materials_dict = generate_formula_dict(self.test_materials)
-        grouped_by_mpid = group_by_task_id(
-            materials_dict['NaN3'], {})
+        docs = []
+        grouped_by_mpid = group_by_material_id(
+            materials_dict['Si'],
+            [{'structure': PymatgenTest.get_structure('Si').as_dict()}])
         self.assertEqual(len(grouped_by_mpid), 1)
         materials_dict = generate_formula_dict(self.test_materials)
 
     def test_get_items(self):
-        builder = ElasticAggregateBuilder()
-        cursor = builder.get_items()
+        iterator = self.builder.get_items()
+        for item in iterator:
+            self.assertIsNotNone(item)
 
     def test_process_items(self):
-        builder = ElasticAggregateBuilder()
-        test_doc = {"_id": {"formula_pretty": "Si", "":""}}
+        builder = self.get_a_new_builder()
+        docs = list(self.test_elasticity.query(None, {"pretty_formula": "Si"}))
+        formula_dict = generate_formula_dict(self.test_materials)
+        processed = builder.process_item((docs, formula_dict['Si']))
+        self.assertEqual(len(processed), 1)
+        self.assertEqual(len(processed[0]['all_elastic_fits']), 2)
 
     def test_aggregation(self):
-        pass
+        runner = Runner([self.builder])
+        runner.run()
+        all_agg_docs = list(self.test_elasticity_agg.query())
+        self.assertTrue(bool(all_agg_docs))
+
+    def get_a_new_builder(self):
+        return ElasticAggregateBuilder(
+            self.test_elasticity, self.test_materials, self.test_elasticity_agg)
 
 
 if __name__ == "__main__":
