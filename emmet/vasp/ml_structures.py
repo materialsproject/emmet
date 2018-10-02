9503907f11090aac74abf95af92115f7f4f5dc0d
shyamd <shyamd@lbl.gov>
2018-10-02 13:13:49 -0700

    clean up style

--- emmet/vasp/ml_structures.py
+++ emmet/vasp/ml_structures.py
@@ -1,4 +1,3 @@
-
 from itertools import chain
 from pymatgen import Structure
 from pymatgen.entries.computed_entries import ComputedStructureEntry
@@ -10,8 +9,7 @@ __author__ = "Shyam Dwaraknath <shyamd@lbl.gov>"
 
 
 class MLStructuresBuilder(MapBuilder):
-
-    def __init__(self, tasks, ml_strucs, task_types= ("Structure Optimization"), **kwargs):
+    def __init__(self, tasks, ml_strucs, task_types=("Structure Optimization"), **kwargs):
         """
         Creates a collection of structures, energies, forces, and stresses for machine learning efforts
 
@@ -24,12 +22,12 @@ class MLStructuresBuilder(MapBuilder):
         self.tasks = tasks
         self.ml_strucs = ml_strucs
         self.task_types = task_types
-        super().__init__(source=tasks,
-                         target=ml_strucs,
-                         ufn=self.calc,
-                         projection=["orig_inputs","input","calcs_reversed"],
-                         query=query,
-                         **kwargs)
+        super().__init__(
+            source=tasks,
+            target=ml_strucs,
+            ufn=self.calc,
+            projection=["orig_inputs", "input", "calcs_reversed"],
+            **kwargs)
 
     def calc(self, task):
         """
@@ -51,17 +49,18 @@ class MLStructuresBuilder(MapBuilder):
         # Get some calculation properties
         is_hubbard = get(task, "input.is_hubbard", False)
         hubbards = get(task, "input.hubbards", [])
-        
+
         # Global iterator to label entry_id's with a sub-step_id
         i = 0
 
         for calc in task.get("calcs_reversed", []):
 
-            parameters = {"is_hubbard": is_hubbard,
-                          "hubbards": hubbards,
-                          "potcar_spec": get(calc, "input.potcar_spec", []),
-                          "run_type": calc.get("run_type", "GGA")
-                          }
+            parameters = {
+                "is_hubbard": is_hubbard,
+                "hubbards": hubbards,
+                "potcar_spec": get(calc, "input.potcar_spec", []),
+                "run_type": calc.get("run_type", "GGA")
+            }
 
             for step_num, step in enumerate(get(calc, "output.ionic_steps")):
                 struc = Structure.from_dict(step.get("structure"))
@@ -70,17 +69,17 @@ class MLStructuresBuilder(MapBuilder):
                     struc.add_site_property("forces", forces)
                 stress = calc.get("stress", None)
                 data = {"stress": stress} if stress else {}
-                c = ComputedStructureEntry(structure=struc,
-                                           correction=0,
-                                           energy=step.get("e_wo_entrp"),
-                                           parameters=parameters,
-                                           entry_id="{}-{}".format(task[self.tasks.key],i),
-                                           data=data)
+                c = ComputedStructureEntry(
+                    structure=struc,
+                    correction=0,
+                    energy=step.get("e_wo_entrp"),
+                    parameters=parameters,
+                    entry_id="{}-{}".format(task[self.tasks.key], i),
+                    data=data)
                 i += 1
 
                 d = c.as_dict()
-                d["chemsys"] = '-'.join(
-                    sorted(set([e.symbol for e in struc.composition.elements])))
+                d["chemsys"] = '-'.join(sorted(set([e.symbol for e in struc.composition.elements])))
                 d["task_type"] = task_type(get(calc, 'input'))
                 d["calc_name"] = get(calc, "task.name")
                 d["task_id"] = task[self.tasks.key]
