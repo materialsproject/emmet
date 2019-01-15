a346a20aaf388a15b4849741db4c6bef51dfc334
Shyam Dwaraknath <shyamd@lbl.gov>
2019-01-14 17:37:26 -0800

    Re-enable tests

--- emmet/materials/tests/test_boltztrap4dos.py
+++ emmet/materials/tests/test_boltztrap4dos.py
@@ -23,17 +23,16 @@ class TestBoltztrap4DosBuilder(unittest.TestCase):
         self.dos = MemoryStore("dos")
         self.dos.connect()
 
-    #def test_process_items(self):
-        #dosbuilder = Boltztrap4DosBuilder(self.materials, self.bandstructure, self.dos)
+    def test_process_items(self):
+        dosbuilder = Boltztrap4DosBuilder(self.materials, self.bandstructure, self.dos)
 
-        #item = self.materials.query_one()
-        #bs_dict = self.bandstructure.query_one()
-        #item["bandstructure_uniform"] = bs_dict
+        item = self.materials.query_one()
+        bs_dict = self.bandstructure.query_one()
+        item["bandstructure_uniform"] = bs_dict
 
-        #dos = dosbuilder.process_item(item)
-        #density = dos['cdos']['densities']['1'][3900]
-        #self.assertAlmostEqual(density,5.446126162946311,5)
-
+        dos = dosbuilder.process_item(item)
+        density = dos['cdos']['densities']['1'][3900]
+        self.assertAlmostEqual(density, 5.446126162946311, 5)
 
     def test_update_targets(self):
         dos = self.dos_ref.query_one()
@@ -42,17 +41,17 @@ class TestBoltztrap4DosBuilder(unittest.TestCase):
         dosbuilder = Boltztrap4DosBuilder(self.materials, self.bandstructure, self.dos)
         dosbuilder.update_targets(items)
 
-        self.assertListEqual(self.dos.query_one.distinct("mp_id"),['mp-12103'])
+        self.assertListEqual(self.dos.query_one.distinct("task_id"), ['mp-12103'])
 
-    #def test_dos_from_boltztrap(self):
-        #item = self.materials.query_one()
-        #bs_dict = self.bandstructure.query_one()
-        #bs_dict['structure'] = item['structure']
+    def test_dos_from_boltztrap(self):
+        item = self.materials.query_one()
+        bs_dict = self.bandstructure.query_one()
+        bs_dict['structure'] = item['structure']
 
-        #bzt_dos = dos_from_boltztrap(bs_dict)
+        bzt_dos = dos_from_boltztrap(bs_dict)
 
-        #density = bzt_dos['densities']['1'][3900]
-        #self.assertAlmostEqual(density,5.446126162946311,5)
+        density = bzt_dos['densities']['1'][3900]
+        self.assertAlmostEqual(density, 5.446126162946311, 5)
 
 
 if __name__ == "__main__":
