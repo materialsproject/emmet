ef653c1142bd181470870597e3d3afb6ca75fa36
Patrick Huck <phuck@lbl.gov>
2019-01-15 14:19:12 -0800

    garden_to_hpss: pack in archives, access hpss from user account

--- emmet/scripts/garden_to_hpss.sh
+++ emmet/scripts/garden_to_hpss.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+[[ ! -d $1/archives ]] && mkdir -v $1/archives
+
 for block_dir in `find $1 -maxdepth 1 -type d -name "block_*"`; do
   echo $block_dir
   subdir=`basename $block_dir`
-  if [ ! -e ${subdir}.tar.gz ]; then
-    tar -czvf ${subdir}.tar.gz ${block_dir}
+  if [ ! -e $1/archives/${subdir}.tar.gz ]; then
+    tar -czvf $1/archives/${subdir}.tar.gz -C $1 $subdir
   fi
-  hsi cput ${subdir}.tar.gz : garden/${subdir}.tar.gz
-  [[ $? -ne 0 ]] && echo "not removing ${block_dir}" && continue
-  rm -rv $block_dir && rm -v ${subdir}.tar.gz
+  hsi -l matcomp cput $1/archives/${subdir}.tar.gz : garden/${subdir}.tar.gz
+  flag=$?
+  [[ $flag -ne 0 ]] && echo "not removing ${subdir}.tar.gz (flag=$flag)" && continue
+  rm -v $1/archives/${subdir}.tar.gz
 done
