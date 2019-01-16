77ec41c02234c0ecd908690ede2d48045e57775a
Shyam Dwaraknath <shyamd@lbl.gov>
2019-01-16 15:42:37 -0800

    Some docs

--- emmet/vasp/materials.py
+++ emmet/vasp/materials.py
@@ -186,6 +186,7 @@ class MaterialsBuilder(Builder):
             # Sort for highest quality score and lowest energy
             sorted_props = sorted(props, key=lambda x: (x["quality_score"], -1.0 * x["energy"]), reverse=True)
             if sorted_props[0].get("aggregate", False):
+                # Make this a list of lists and then flatten to deal with mixed value typing
                 vals = [prop["value"] if isinstance(prop["value"], list) else [prop["value"]] for prop in sorted_props]
                 vals = list(chain.from_iterable(vals))
                 prop = sorted_props[0]
