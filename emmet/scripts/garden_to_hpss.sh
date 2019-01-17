9ea8a991d35392a29348f528db638a24ca402884
Patrick Huck <phuck@lbl.gov>
2019-01-17 13:38:43 -0800

    htar ready: garden_to_hpss & targz_to_htar

--- emmet/scripts/garden_to_hpss.sh
+++ emmet/scripts/garden_to_hpss.sh
@@ -4,17 +4,12 @@ cd $1 && pwd
 
 for block_dir in `find $1 -maxdepth 1 -type d -name "block_*"`; do
   echo $block_dir
-  chmod -Rv ug+rw $block_dir
+  find $block_dir -not -perm -660 -exec chmod -v g+rw {} \;
   [[ $? -ne 0 ]] && echo 'error in chmod' && exit
   find $block_dir -type f -not -name "*.gz" -exec pigz -9v {} \;
   [[ $? -ne 0 ]] && echo "error in pigz" && exit
   block=`basename $block_dir`
-  htar -cvf garden/${block}.tar $block
-  flag=$?
-  if [ $flag -ne 0 ]; then
-    echo "error with htar (flag=$flag)"
-    exit
-  fi
-  #rm -rfv $block_dir
-  break
+  htar -M 5000000 -cvf garden/${block}.tar $block
+  [[ $? -ne 0 ]] && echo "error with htar" && exit
+  rm -rfv $block_dir
 done
--- emmet/scripts/targz_to_htar.sh
+++ emmet/scripts/targz_to_htar.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
 # NOTE make sure matcomp is first entry in ~/.netrc!
-[[ ! -e garden.txt ]] && hsi -P -l matcomp ls -1 "garden/*.tar.gz" > garden.txt
+cd $1 && pwd
+hsi -P -l matcomp ls -1 "garden/*.tar.gz" > garden.txt
 
 while read block_tar_gz; do
   block=`basename ${block_tar_gz%%.tar.gz}`
   echo $block
-  if [ ! -e ${block}.tar.gz ]; then
-    hsi -q -l matcomp get garden/${block}.tar.gz
-    [[ $? -ne 0 ]] && echo 'error in hsi get' && exit
-  fi
-  if [ ! -d ${block} ]; then
-    tar -xvzf ${block}.tar.gz
-    [[ $? -ne 0 ]] && echo 'error in tar -x' && exit
-  fi
-  chmod -Rv ug+rw ${block}
+  hsi -q -l matcomp cget garden/${block}.tar.gz
+  [[ $? -ne 0 ]] && echo 'error in hsi cget' && exit
+  tar --skip-old-files -xvzf ${block}.tar.gz
+  [[ $? -ne 0 ]] && echo 'error in tar -x' && exit
+  find $block -not -perm -660 -exec chmod -v g+rw {} \;
   [[ $? -ne 0 ]] && echo 'error in chmod' && exit
   find ${block} -type f -not -name "*.gz" -exec pigz -9v {} \;
   [[ $? -ne 0 ]] && echo "error in pigz" && exit
-  htar -cvf garden/${block}.tar ${block}
+  htar -M 5000000 -cvf garden/${block}.tar ${block}
   [[ $? -ne 0 ]] && echo 'error in htar -c' && exit
   hsi -q -l matcomp rm garden/${block}.tar.gz
   [[ $? -ne 0 ]] && echo 'error in htar rm' && exit
   rm -rv ${block}
   rm -v ${block}.tar.gz
-  break # TODO remove
 done < garden.txt
 
 
