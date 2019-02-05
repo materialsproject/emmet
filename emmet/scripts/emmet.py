0227a9eb4436cc92ba2e0820f013941462dcd94c
Patrick Huck <phuck@lbl.gov>
2019-02-05 11:53:50 -0800

    cli.gdrive: fix store init

--- emmet/scripts/emmet.py
+++ emmet/scripts/emmet.py
@@ -1292,10 +1292,13 @@ def gdrive(target_db_file, block_filter):
     print('connected to target db with', target.collection.count(), 'tasks')
     print(target.db.materials.count(), 'materials')
 
-    store = file.Storage('token.json')
-    creds = store.get()
+    creds, store = None, None
+    if os.path.exists('token.json'):
+        store = file.Storage('token.json')
+        creds = store.get()
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        store = file.Storage('token.json')
         creds = tools.run_flow(flow, store)
     service = build('drive', 'v3', http=creds.authorize(Http()))
     garden_id = os.environ.get('MPDRIVE_GARDEN_ID')
