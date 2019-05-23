2ec987e92510b65c3991d8899742440e65527f66
Donny Winston <dwinston@alum.mit.edu>
2019-05-23 09:24:47 -0700

    Output all issues with test

--- mp_acceptance_tests/test_staging_db.py
+++ mp_acceptance_tests/test_staging_db.py
@@ -96,6 +96,7 @@ class TestMaterialsDb(unittest.TestCase):
         self.assertEqual(len(unfaithful_taskids), 0)
 
     def test_nprops_nondecreasing(self):
+        issues = []
         depr_mids = self.mats_stag.distinct("task_id", {"deprecated": True})
         for prop in filter(None, self.mats_prod.distinct("has")):
             prod_mids = set(self.mats_prod.distinct("task_id", {"has": prop, "deprecated": {"$ne": True}}))
@@ -114,7 +115,10 @@ class TestMaterialsDb(unittest.TestCase):
                 prod_mids = set(self.db_phonons.phonon_bs_img.distinct(
                     "mp-id", {"mp-id": {"$in": list(prod_mids)}}))
             for mid in sorted(prod_mids):
-                self.assertTrue(mid in depr_mids or mid in stag_tids, f'non-deprecated {mid} missing {prop}')
+                if not (mid in depr_mids or mid in stag_tids):
+                    issues.append(f'non-deprecated {mid} missing {prop}')
+        if issues:
+            self.fail('\n'.join(issues))
 
     @unittest.skip("Many of these calculations need to be redone.")
     def test_piezo_og_formulae_present(self):
