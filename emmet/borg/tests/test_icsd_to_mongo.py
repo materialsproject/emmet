a498a5109091e18b2020413504756aa68af119b2
Koki Muraoka <koki.muraoka@gmail.com>
2019-08-22 16:22:15 -0700

    Fix tests

--- emmet/borg/tests/test_icsd_to_mongo.py
+++ emmet/borg/tests/test_icsd_to_mongo.py
@@ -1,14 +1,14 @@
 import unittest
-from monty.serialization import loadfn
-from emmet.borg.icsd_to_mongo import icsdDrone
+from monty.serialization import loadfn, dumpfn
+from emmet.borg.icsd_to_mongo import IcsdDrone
 
 
 class TestIcsdToMongo(unittest.TestCase):
     def test_icsd_drone(self):
-        drone = icsdDrone()
+        drone = IcsdDrone()
         obtained = drone.assimilate(
-            'test_files/999999999', store_mongo=False)
-        expected = loadfn('test_files/999999999_expected.json')
+            'test_files/icsd/999999999')
+        expected = loadfn('test_files/icsd/999999999_expected.json')
 
         keys = [
             "chem_name",
@@ -18,7 +18,6 @@ class TestIcsdToMongo(unittest.TestCase):
             "formula_anonymous",
             "formula_reduced",
             "formula_reduced_abc",
-            "icsd_id",
             "is_ordered",
             "is_valid",
             "nelements",
@@ -26,4 +25,7 @@ class TestIcsdToMongo(unittest.TestCase):
             "pressure"]
 
         for key in keys:
-            self.assertAlmostEqual(obtained[key], expected[key])
+            self.assertAlmostEqual(obtained['cifmetadata'][key], expected[key])
+
+
+        dumpfn(obtained, "tmp.json")
