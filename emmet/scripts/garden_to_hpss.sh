8d4d1ea1c908e865ebda3df7c6628a7148a7ce4f
Patrick Huck <phuck@lbl.gov>
2019-09-18 10:16:30 -0700

    cli: updates to hpss scripts

--- emmet/scripts/garden_to_hpss.sh
+++ emmet/scripts/garden_to_hpss.sh
@@ -20,25 +20,34 @@ for block in $(find . -maxdepth 1 -type d -name "$filter" -exec basename {} \;);
   find $block -type f -not -name "*.gz" -exec pigz -9v {} \;
   [[ $? -ne 0 ]] && echo "error in pigz" && exit
 
-  htar -vtf garden/${block}.tar | awk '{ print $7 }' | sort -u > ${block}.tar.idx
-  [[ $? -ne 0 ]] && echo "error in htar -t" && exit # TODO upload new archive if not exists
-  find $block -type f | sort -u > ${block}.idx
-
-  comm -13 ${block}.tar.idx ${block}.idx > ${block}.missing
-  if [ -s ${block}.missing ]; then
-    nfiles=$(wc -l ${block}.missing | awk '{ print $1}')
-    echo need syncing of $nfiles files
-    htar -xvf garden/${block}.tar
-    [[ $? -ne 0 ]] && echo "error in htar -x" && exit
-    hsi -q -l matcomp mv garden/${block}.tar garden/${block}.tar.bkp
-    hsi -q -l matcomp mv garden/${block}.tar.idx garden/${block}.tar.idx.bkp
+  hsi -q -l matcomp ls -1 garden/${block}.tar
+  if [ $? -ne 0 ]; then
+    echo "upload new archive for ${block}"
     htar -M 5000000 -cvf garden/${block}.tar ${block}
     [[ $? -ne 0 ]] && echo "error in htar -c" && exit
-    hsi -q -l matcomp rm garden/${block}.tar*.bkp
-    [[ $? -ne 0 ]] && echo 'error in htar rm' && exit
   else
-    echo all files already in HTAR archive
+    echo "update existing archive for ${block}"
+    htar -vtf garden/${block}.tar | awk '{ print $7 }' | sort -u > ${block}.tar.idx
+    [[ $? -ne 0 ]] && echo "error in htar -t" && exit
+    find $block -type f | sort -u > ${block}.idx
+
+    # TODO check remote and local file sizes and keep larger file when extracting
+    comm -13 ${block}.tar.idx ${block}.idx > ${block}.missing
+    if [ -s ${block}.missing ]; then
+      nfiles=$(wc -l ${block}.missing | awk '{ print $1 }')
+      echo need syncing of $nfiles files
+      htar -xvf garden/${block}.tar # TODO only extract what's not available locally (avoid overriding)
+      [[ $? -ne 0 ]] && echo "error in htar -x" && exit
+      # TODO make & keep timestamped backups
+      hsi -q -l matcomp mv garden/${block}.tar garden/${block}.tar.bkp
+      hsi -q -l matcomp mv garden/${block}.tar.idx garden/${block}.tar.idx.bkp
+      htar -M 5000000 -cvf garden/${block}.tar ${block}
+      [[ $? -ne 0 ]] && echo "error in htar -c" && exit
+    else
+      echo all files already in HTAR archive
+    fi
+    rm -v ${block}.tar.idx ${block}.idx ${block}.missing
   fi
+
   rm -rv ${block}
-  rm -v ${block}.tar.idx ${block}.idx ${block}.missing
 done
