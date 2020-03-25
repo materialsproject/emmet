861b7caa91d6fabea75e99e2cc4a88fe56f673df
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-25 16:51:13 -0700

    update stubs

--- emmet/stubs/__init__.py
+++ emmet/stubs/__init__.py
@@ -1,5 +1,8 @@
 from emmet.stubs.utils import patch_msonable, use_model
 from emmet.stubs.pymatgen import Structure as StubStructure
-from pymatgen import Structure
+from emmet.stubs.pymatgen import Composition as StubComposition
+from pymatgen import Structure, Composition
 
 use_model(Structure, StubStructure)
+use_model(Composition, StubComposition)
+
