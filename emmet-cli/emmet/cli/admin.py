28d6398e142f34f5bc5c244dc0be1cc56a1e24de
Patrick Huck <phuck@lbl.gov>
2020-03-27 17:10:44 -0700

    cli: fix admin meta command for mp_core.snls

--- emmet-cli/emmet/cli/admin.py
+++ emmet-cli/emmet/cli/admin.py
@@ -2,7 +2,7 @@ import click
 
 from pymatgen import Structure
 
-from emmet.cli.config import structure_keys
+from emmet.cli.config import structure_keys, meta_keys, snl_indexes
 from emmet.cli.utils import ensure_indexes, get_meta_from_structure
 
 
@@ -41,9 +41,8 @@ def index(ctx, fields, collection):
 def meta(ctx, collection):
     """create meta-data fields and indexes for SNL collection"""
     coll = ctx.obj['CLIENT'].db[collection]
-    meta_keys = ['formula_pretty', 'nelements', 'nsites', 'is_ordered', 'is_valid']
     q = {'$or': [{k: {'$exists': 0}} for k in meta_keys]}
-    docs = coll.find(q, structure_keys)  # TODO structure_keys nested in `snl` for mpcore.snls
+    docs = coll.find(q, structure_keys)
 
     ndocs = docs.count()
     if ndocs > 0:
@@ -54,14 +53,11 @@ def meta(ctx, collection):
             for idx, doc in enumerate(docs):
                 if idx and not idx%1000:
                     click.echo(f'{idx} ...')
-                struct = Structure.from_dict(doc)
-                coll.update({'snl_id': doc['snl_id']}, {'$set': get_meta_from_structure(struct)})
+                struct = Structure.from_dict(doc.get('snl', doc))
+                key = 'task_id' if 'task_id' in doc else 'snl_id'
+                coll.update({key: doc[key]}, {'$set': get_meta_from_structure(struct)})
 
-    fields = [
-        'snl_id', 'reduced_cell_formula', 'formula_pretty', 'about.remarks', 'about.projects',
-        'sites.label', 'nsites', 'nelements', 'is_ordered', 'is_valid'
-    ]
-    clean_ensure_indexes(ctx.obj['DRY_RUN'], fields, coll)
+    clean_ensure_indexes(ctx.obj['DRY_RUN'], snl_indexes, coll)
 
 # TODO clear logs command
 #@click.option('--clear-logs/--no-clear-logs', default=False, help='clear MongoDB logs collection for specific tag')
--- emmet-cli/emmet/cli/config.py
+++ emmet-cli/emmet/cli/config.py
@@ -1,7 +1,30 @@
-exclude = {'about.remarks': {'$nin': ['DEPRECATED', 'deprecated']}}
+exclude = {'about.remarks': {
+    '$nin': ['DEPRECATED', 'deprecated']
+}}
 skip_labels = ['He', 'He0+', 'Ar', 'Ar0+', 'Ne', 'Ne0+', 'D', 'D+', 'T', 'M']
-base_query = {'is_ordered': True, 'is_valid': True, 'nsites': {'$lt': 200}, 'sites.label': {'$nin': skip_labels}}
-task_base_query = {'tags': {'$nin': ['DEPRECATED', 'deprecated']}, '_mpworks_meta': {'$exists': 0}}
+base_query = {
+    'is_ordered': True, 'is_valid': True, 'nsites': {'$lt': 200}, '$or': [
+        {'sites.label': {'$nin': skip_labels}},
+        {'snl.sites.label': {'$nin': skip_labels}}
+    ]
+}
+task_base_query = {
+    'tags': {'$nin': ['DEPRECATED', 'deprecated']},
+    '_mpworks_meta': {'$exists': 0}
+}
 aggregation_keys = ['formula_pretty', 'reduced_cell_formula']
-structure_keys = ['snl_id', 'lattice', 'sites', 'charge', 'about._materialsproject.task_id']
-NO_POTCARS = ['Po', 'At', 'Rn', 'Fr', 'Ra', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr']
+meta_keys = ['formula_pretty', 'nelements', 'nsites', 'is_ordered', 'is_valid']
+structure_keys = [
+    'snl_id', 'task_id', 'lattice', 'sites', 'charge', 'about._materialsproject.task_id',
+    'snl.lattice', 'snl.sites', 'snl.charge', 'snl.about._materialsproject.task_id'
+]
+NO_POTCARS = [
+    'Po', 'At', 'Rn', 'Fr', 'Ra', 'Am', 'Cm',
+    'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr'
+]
+snl_indexes = [
+    'snl_id', 'task_id', 'reduced_cell_formula', 'formula_pretty',
+    'nsites', 'nelements', 'is_ordered', 'is_valid',
+    'about.remarks', 'about.projects', 'sites.label',
+    'snl.about.remarks', 'snl.about.projects', 'snl.sites.label'
+]
