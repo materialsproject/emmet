8701b8e93b186cdb12812f0b46186ed3569e241b
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-31 14:39:44 -0700

    made to use standard namespace package methods

--- emmet-core/setup.py
+++ emmet-core/setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import datetime
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(SETUP_PTH, 'requirements.txt')) as f:
@@ -9,12 +9,13 @@ with open(os.path.join(SETUP_PTH, 'requirements.txt')) as f:
 
 setup(
     name='emmet-core',
-    version=datetime.datetime.today().strftime('%Y.%m.%d'),
-    description='core emmet library',
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description='Core Emmet Library',
     author="The Materials Project",
     author_email="feedback@materialsproject.org",
     url='https://github.com/materialsproject/emmet',
-    packages=['emmet.core'],
+    packages=find_namespace_packages(include=['emmet.*']),
     install_requires=required,
     license='modified BSD',
     zip_safe=False,
