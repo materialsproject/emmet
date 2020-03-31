f44cff8aa380dd0aa23c53e988d454855d5b3fb1
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-31 14:39:32 -0700

    add emmet-builders module

---
+++ emmet-builders/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import os
+import datetime
+from setuptools import setup, find_namespace_packages
+
+SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(SETUP_PTH, 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
+setup(
+    name='emmet-builders',
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description='Builders for the Emmet Library',
+    author="The Materials Project",
+    author_email="feedback@materialsproject.org",
+    url='https://github.com/materialsproject/emmet',
+    packages=find_namespace_packages(include=['emmet.*']),
+    install_requires=required,
+    license='modified BSD',
+    zip_safe=False,
+)
