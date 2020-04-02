b78079970606d7cbe7312e78215b0af8263132e5
Shyam Dwaraknath <shyamd@lbl.gov>
2020-04-02 12:50:57 -0700

    fix scm versioning

--- emmet-core/setup.py
+++ emmet-core/setup.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
-import os
 import datetime
+from pathlib import Path
 from setuptools import setup, find_namespace_packages
 
-SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(SETUP_PTH, 'requirements.txt')) as f:
+with open(Path(__file__).parent / "requirements.txt") as f:
     required = f.read().splitlines()
 
 setup(
-    name='emmet-core',
-    use_scm_version=True,
+    name="emmet-core",
+    use_scm_version={"relative_to": Path(__file__).parent},
     setup_requires=["setuptools_scm"],
-    description='Core Emmet Library',
+    description="Core Emmet Library",
     author="The Materials Project",
     author_email="feedback@materialsproject.org",
-    url='https://github.com/materialsproject/emmet',
-    packages=find_namespace_packages(include=['emmet.*']),
+    url="https://github.com/materialsproject/emmet",
+    packages=find_namespace_packages(include=["emmet.*"]),
     install_requires=required,
-    license='modified BSD',
+    license="modified BSD",
     zip_safe=False,
 )
