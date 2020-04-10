a167d5b52f81802b71fa7464b43f26057a5d7b1d
Patrick Huck <phuck@lbl.gov>
2020-04-10 15:14:17 -0700

    core: separate dependencies, dependabot monitors requirements
    @shyamd

--- emmet-core/setup.py
+++ emmet-core/setup.py
@@ -3,9 +3,6 @@ import datetime
 from pathlib import Path
 from setuptools import setup, find_namespace_packages
 
-with open(Path(__file__).parent / "requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name="emmet-core",
     use_scm_version={"relative_to": Path(__file__).parent},
@@ -15,7 +12,7 @@ setup(
     author_email="feedback@materialsproject.org",
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
-    install_requires=required,
+    install_requires=["pymatgen", "monty", "pydantic", "pybtex"],
     license="modified BSD",
     zip_safe=False,
 )
