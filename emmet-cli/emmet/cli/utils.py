17a02a4a210d6916e74895216fd6edf6cd81784f
Patrick Huck <phuck@lbl.gov>
2020-04-14 16:03:03 -0700

    cli: save progress on tracker workflow

--- emmet-cli/emmet/cli/utils.py
+++ emmet-cli/emmet/cli/utils.py
@@ -260,6 +260,25 @@ def reconstruct_command(sbatch=False):
     return " ".join(command)
 
 
+COMMENT_TEMPLATE = """
+<details><summary><b>{}</b> returned "{}"</summary><p>
+
+```
+{}
+```
+
+[Logs]({})
+
+</p></details>
+"""
+
+
+GIST_COMMENT_TEMPLATE = """
+**[Gist]({url}) created** to collect command logs for this issue. The current set of
+log files can be downloaded [here]({url}/archive/master.zip).
+"""
+
+
 def track(func):
     """decorator to track command in GH issue / gists"""
     def wrapper(*args, **kwargs):
@@ -271,16 +290,50 @@ def track(func):
             logger.info(ret)
             command = reconstruct_command()
             gh = ctx.grand_parent.obj["GH"]
-            now = str(datetime.now()).replace(" ", "-")
-            fn = ctx.command_path.replace(" ", "-") + f"_{now}.log"
-            logs = ctx.grand_parent.obj["LOG_STREAM"]
-            files = {fn: {"content": logs.getvalue()}}
-            gist = gh.create_gist(command, files, public=False)
-            logger.info(gist.html_url)
             issue_number = ctx.grand_parent.params["issue"]
             issue = gh.issue(tracker["org"], tracker["repo"], issue_number)
-            txt = f"*{ctx.command_path}* returned \"{ret}\" "
-            txt += f"([logs]({gist.html_url})):\n\n```\n{command}\n```"
+
+            # gists iterator/resource based on latest etag
+            ETAG = os.path.join(os.path.expanduser("~"), '.emmet_etag')
+            etag = None
+            if os.path.exists(ETAG):
+                with open(ETAG, 'r') as fd:
+                    etag = fd.readline().strip()
+
+            gists_iterator = gh.gists(etag=etag)
+            if gists_iterator.etag != etag:
+                with open(ETAG, 'w') as fd:
+                    fd.write(gists_iterator.etag)
+
+            # create or retrieve gist for log files
+            gist_name = f"#{issue_number}-{tracker['repo']}.md"
+            for gist in gists_iterator:
+                if gist.files[0].filename == gist_name:
+                    break
+            else:
+                description = f"Logs for {tracker['repo']}#{issue_number}"
+                files = {gist_name: {"content": issue.html_url}}
+                gist = gh.create_gist(description, files, public=False)
+                txt = GIST_COMMENT_TEMPLATE.format(url=gist.html_url)
+                comment = issue.create_comment(txt)
+                logger.info(f"Gist Comment: {comment.html_url}")
+
+            logger.info(f"Log Gist: {gist.html_url}")
+
+
+                # TODO update
+            now = str(datetime.now()).replace(" ", "-")
+            fn = ctx.command_path.replace(" ", "-") + f"_{now}"
+            logs = ctx.grand_parent.obj["LOG_STREAM"]
+            files = {fn: {"content": logs.getvalue()}}
+
+
+
+            # TODO hide previous comments?
+            # TODO link to raw log file
+            raw_url =
+            # https://gist.githubusercontent.com/tschaume/43ee0a71e74eb68d23368f6fcd56b04f/raw/ecf10823056fe07fe8f83acf9d5bb169090a9af9/emmet-hpss-prep_2020-04-14-01:51:29.944896
+            txt = COMMENT_TEMPLATE.format(ctx.command_path, ret, command, raw_url)
             comment = issue.create_comment(txt)
             logger.info(comment.html_url)
 
@@ -323,6 +376,7 @@ def sbatch(func):
         )
 
         command = reconstruct_command(sbatch=True)
-        return s.run(command, _cmd="sbatch" if run else "ls")
+        ret = s.run(command, _cmd="sbatch" if run else "ls")
+        return f"SBatch JobId: {ret}" if run else ret
 
     return update_wrapper(wrapper, func)
