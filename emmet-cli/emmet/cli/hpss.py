53313749d0bdd133252981cde6c762ef8d97497e
Patrick Huck <phuck@lbl.gov>
2020-04-16 23:27:46 -0700

    cli: done with hpss command

--- emmet-cli/emmet/cli/hpss.py
+++ emmet-cli/emmet/cli/hpss.py
@@ -13,6 +13,7 @@ from emmet.cli.decorators import sbatch
 
 
 logger = logging.getLogger("emmet")
+PREFIX = "block_"
 
 
 @click.group()
@@ -26,7 +27,7 @@ logger = logging.getLogger("emmet")
     "-p",
     "--pattern",
     show_default=True,
-    default="block_*",
+    default=f"{PREFIX}*",
     help="Pattern for sub-paths to include.",
 )
 def hpss(directory, nmax, pattern):
@@ -79,42 +80,63 @@ def recursive_chown(path, group):
             shutil.chown(os.path.join(dirpath, filename), group=group)
 
 
-@hpss.command()
-@sbatch
-def backup():
-    """Backup directory to HPSS"""
+def check_pattern():
     ctx = click.get_current_context()
-    run = ctx.parent.parent.params["run"]
-    nmax = ctx.parent.params["nmax"]
-    directory = ctx.parent.params["directory"]
     pattern = ctx.parent.params["pattern"]
-    prefix = "block_"
-
     if os.sep in pattern:
         raise EmmetCliError(f"Nested pattern ({pattern}) not allowed!")
-    elif not pattern.startswith(prefix):
-        raise EmmetCliError(f"Pattern ({pattern}) only allowed to start with {prefix}!")
+    elif not pattern.startswith(PREFIX):
+        raise EmmetCliError(f"Pattern ({pattern}) only allowed to start with {PREFIX}!")
 
+
+def load_block_launchers():
+    ctx = click.get_current_context()
+    nmax = ctx.parent.params["nmax"]
     block_launchers = defaultdict(list)
-    for vasp_dir in get_vasp_dirs():
-        launch_dir = prefix + vasp_dir.split(prefix, 1)[-1]
+    for idx, vasp_dir in enumerate(get_vasp_dirs()):
+        if idx and not idx % 500:
+            logger.info(f"{idx} launchers found ...")
+        launch_dir = PREFIX + vasp_dir.split(PREFIX, 1)[-1]
         block, launcher = launch_dir.split(os.sep, 1)
         if len(block_launchers) == nmax and block not in block_launchers:
             # already found nmax blocks. Order of launchers not guaranteed
             continue
         block_launchers[block].append(launcher)
+    return block_launchers
 
-    logger.info(f"Backing up {len(block_launchers)} block(s) ...")
 
-    counter = 0
+def extract_filename(line):
+    ls = line.strip().split()
+    return ls[-1] if len(ls) == 7 else None
+
+
+@hpss.command()
+@sbatch
+@click.option("--clean", is_flag=True, help="Remove original launchers.")
+@click.option("--check", is_flag=True, help="Check backup consistency.")
+def backup(clean, check):
+    """Backup directory to HPSS"""
+    ctx = click.get_current_context()
+    run = ctx.parent.parent.params["run"]
+    directory = ctx.parent.params["directory"]
+    if not check and clean:
+        logger.error("Not running --clean without --check enabled.")
+        return ReturnCodes.ERROR
+
+    check_pattern()
+
+    logger.info("Discover launch directories ...")
+    block_launchers = load_block_launchers()
+    logger.info(f"Back up {len(block_launchers)} block(s) ...")
+
+    counter, nremove_total = 0, 0
+    os.chdir(directory)
     for block, launchers in block_launchers.items():
         logger.info(f"{block} with {len(launchers)} launcher(s)")
         try:
             isfile(f"garden/{block}.tar")
-        except HpssOSError:
-            # back up block if not in HPSS
+        except HpssOSError:  # block not in HPSS
             if run:
-                os.chdir(directory)
                 filelist = [os.path.join(block, l) for l in launchers]
                 args = shlex.split(f"htar -M 5000000 -Phcvf garden/{block}.tar")
                 try:
@@ -125,13 +147,49 @@ def backup():
                     return ReturnCodes.ERROR
                 counter += 1
         else:
-            logger.warning(f"Skipping {block} - already in HPSS")
+            logger.warning(f"Skip {block} - already in HPSS")
 
-    logger.info(f"{counter}/{len(block_launchers)} blocks backed up to HPSS.")
+        # Check backup here to allow running it separately
+        if check:
+            logger.info(f"Verify {block}.tar ...")
+            args = shlex.split(
+                f"htar -Kv -Hrelpaths -Hverify=all -f garden/{block}.tar"
+            )
+            files_remove = []
+            try:
+                for line in run_command(args, []):
+                    line = line.strip()
+                    if line.startswith("HTAR: V "):
+                        ls = line.split(", ")
+                        if len(ls) == 3:
+                            nfiles = len(files_remove)
+                            if nfiles and not nfiles % 1000:
+                                logger.info(f"{nfiles} files ...")
+                            files_remove.append(ls[0].split()[-1])
+                    else:
+                        logger.info(line)
+            except subprocess.CalledProcessError as e:
+                logger.error(str(e))
+                return ReturnCodes.ERROR
+
+            if clean:
+                nremove = len(files_remove)
+                nremove_total += nremove
+                if run:
+                    with click.progressbar(files_remove, label="Removing files") as bar:
+                        for fn in bar:
+                            os.remove(fn)
+                    logger.info(f"Removed {nremove} files from disk for {block}.")
+                else:
+                    logger.info(f"Would remove {nremove} files from disk for {block}.")
+
+    logger.info(f"{counter}/{len(block_launchers)} blocks newly backed up to HPSS.")
+    if clean:
+        if run:
+            logger.info(f"Verified and removed a total of {nremove_total} files.")
+        else:
+            logger.info(f"Would verify and remove a total of {nremove_total} files.")
     return ReturnCodes.SUCCESS
-
-
-# TODO cleanup command to double-check HPSS backup and remove source
 
 
 @hpss.command()
@@ -197,12 +255,11 @@ def restore(inputfile, file_filter):
         filelist_restore, cnt = [], 0
         try:
             for line in run_command(args, filelist):
-                ls = line.strip().split()
-                if len(ls) == 7:
-                    fn = ls[-1]
+                fn = extract_filename(line)
+                if fn:
                     cnt += 1
                     if os.path.exists(fn):
-                        logger.debug(f"Skipping {fn} - already exists on disk.")
+                        logger.debug(f"Skip {fn} - already exists on disk.")
                     else:
                         filelist_restore.append(fn)
         except subprocess.CalledProcessError as e:
@@ -214,7 +271,7 @@ def restore(inputfile, file_filter):
             nfiles_restore = len(filelist_restore)
             if run:
                 logger.info(
-                    f"Restoring {nfiles_restore}/{cnt} files for {block} to {directory} ..."
+                    f"Restore {nfiles_restore}/{cnt} files for {block} to {directory} ..."
                 )
                 args = shlex.split(f"htar -xvf garden/{block}.tar")
                 try:
@@ -231,7 +288,7 @@ def restore(inputfile, file_filter):
             logger.warning(f"Nothing to restore for {block}!")
 
         if run:
-            logger.info(f"Setting group of {block} to matgen recursively ...")
+            logger.info(f"Set group of {block} to matgen recursively ...")
             recursive_chown(block, "matgen")
 
     return ReturnCodes.SUCCESS
--- emmet-cli/emmet/cli/utils.py
+++ emmet-cli/emmet/cli/utils.py
@@ -1,7 +1,10 @@
 import os
+import grp
+import pwd
 import stat
 import mgzip
 import click
+import shutil
 import logging
 import itertools
 
@@ -226,25 +229,39 @@ def get_vasp_dirs():
 
         files[:] = [f for f in files if not os.path.islink(os.path.join(root, f))]
         if is_vasp_dir(files):
-            with click.progressbar(files, label="Check permissions & gzip") as bar:
-                for f in bar:
-                    fn = os.path.join(root, f)
+            gzipped = False
+            for f in files:
+                fn = os.path.join(root, f)
+                st = os.stat(fn)
+                if not bool(st.st_mode & perms):
+                    raise EmmetCliError(
+                        f"Insufficient permissions {st.st_mode} for {fn}."
+                    )
+
+                if run and not f.endswith(".gz"):
+                    fn_gz = fn + ".gz"
+                    if os.path.exists(fn_gz):
+                        os.remove(fn_gz)  # remove left-over gz (cancelled job)
+
+                    with open(fn, "rb") as fo, mgzip.open(fn_gz, "wb", thread=0) as fw:
+                        fw.write(fo.read())
+
+                    os.remove(fn)  # remove original
+                    shutil.chown(fn_gz, group="matgen")
+                    gzipped = True
+
+                # TODO remove
+                if run and os.path.exists(fn):
                     st = os.stat(fn)
-                    if not bool(st.st_mode & perms):
-                        raise EmmetCliError(
-                            f"Insufficient permissions {st.st_mode} for {fn}."
-                        )
-                    if run and not f.endswith(".gz") and not os.path.exists(fn + ".gz"):
-                        with open(fn, "rb") as fo, mgzip.open(
-                            fn + ".gz", "wb", thread=0
-                        ) as fw:
-                            fw.write(fo.read())
-                        os.remove(fn)
+                    user = pwd.getpwuid(st.st_uid)[0]
+                    group = grp.getgrgid(st.st_gid)[0]
+                    if user == "huck" and group == "huck":
+                        shutil.chown(fn, group="matgen")
 
             vasp_dir = get_symlinked_path(root, base_path_index)
             create_orig_inputs(vasp_dir)
             dirs[:] = []  # don't descend further (i.e. ignore relax1/2)
-            logger.info(vasp_dir)
+            logger.log(logging.INFO if gzipped else logging.DEBUG, vasp_dir)
             yield vasp_dir
 
 
