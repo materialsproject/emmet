3f4da65a5e4dc2f4313662a1c3a02dc619f214fc
Shyam Dwaraknath <shyamd@lbl.gov>
2020-04-17 19:01:06 -0700

    add test for from url

--- tests/test_core/test_settings.py
+++ tests/test_core/test_settings.py
@@ -29,3 +29,13 @@ def test_allow_extra_fields(tmp_path: PosixPath):
     os.environ["EMMET_CONFIG_FILE"] = str(tmp_path.resolve() / "temp_config.json")
 
     test_config = EmmetSettings()
+
+
+def test_from_url():
+    """Makes sure loading from a URL Works"""
+
+    os.environ["EMMET_CONFIG_FILE"] = "https://raw.githubusercontent.com/materialsproject/emmet/refactor/tests/test_core/test_settings.json"
+
+    test_config = EmmetSettings()
+
+    assert test_config.ANGLE_TOL == 1.0
