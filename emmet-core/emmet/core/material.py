8ef052b0d3d461b73cb6a10e760a2f7c64e26cd2
Shyam D <shyamd@lbl.gov>
2020-09-02 09:15:30 -0700

    fix import to vasp module

--- emmet-core/emmet/core/material.py
+++ emmet-core/emmet/core/material.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, Field, create_model
 from pymatgen.analysis.magnetism import CollinearMagneticStructureAnalyzer, Ordering
 
 from emmet.core.structure import StructureMetadata
-from emmet.core.utils import CalcType, RunType, TaskType
+from emmet.core.vasp import CalcType, RunType, TaskType
 from emmet.stubs import Structure
 
 
