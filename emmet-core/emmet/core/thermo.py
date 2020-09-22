ef86326ff3bc0707f5cd3f85bb555197fc0521f4
Shyam D <shyamd@lbl.gov>
2020-09-22 10:41:34 -0700

    make names clearer

--- emmet-core/emmet/core/thermo.py
+++ emmet-core/emmet/core/thermo.py
@@ -1,15 +1,17 @@
 """ Core definition of a Thermo Document """
 from datetime import datetime
 from typing import ClassVar, Dict, List
+from enum import Enum
 
 from pydantic import BaseModel, Field
+from pymatgen.core.periodic_table import Element
 
 from emmet.core.material import PropertyDoc
 from emmet.core.structure import StructureMetadata
-from emmet.stubs import ComputedEntry
+from emmet.stubs import ComputedEntry, Composition
 
 
-class Decomposition(BaseModel):
+class DecompositionProduct(BaseModel):
     """
     Entry metadata for a decomposition process
     """
@@ -29,19 +31,26 @@ class Decomposition(BaseModel):
 
 class Thermo(PropertyDoc):
     """
-    A thermo package block
+    A thermo entry document
     """
 
     property_name: ClassVar[str] = Field(
         "thermo", description="The subfield name for this property"
     )
 
-    energy_per_atom: float = Field(
+    uncorrected_energy_per_atom: float = Field(
         None, description="The total DFT energy of this material per atom in eV/atom"
     )
-    energy: float = Field(
-        None, description="The total DFT energy of this material in eV"
+
+    energy_per_atom: float = Field(
+        None,
+        description="The total corrected DFT energy of this material per atom in eV/atom",
     )
+
+    energy_uncertainy_per_atom: float = Field(
+        None, description=""
+    )  # From correction_uncertainty / N
+
     formation_energy_per_atom: float = Field(
         None, description="The formation energy per atom in eV/atom"
     )
@@ -52,13 +61,13 @@ class Thermo(PropertyDoc):
         None,
         description="Flag for whether this material is on the hull and therefore stable",
     )
-    eq_reaction_e: float = Field(
+    equillibrium_reaction_energy_per_atmo: float = Field(
         None,
         description="The reaction energy of a stable entry from the neighboring equilibrium stable materials in eV."
         " Also known as the inverse distance to hull.",
     )
 
-    decomposes_to: List[Decomposition] = Field(
+    decomposes_to: List[DecompositionProduct] = Field(
         None,
         description="List of decomposition data for this material. Only valid for metastable or unstable material.",
     )
@@ -67,6 +76,11 @@ class Thermo(PropertyDoc):
         None,
         description="The type of calculation this energy evaluation comes from. TODO: Convert to enum?",
     )
+
+    entry_types: List[str] = Field(
+        description="List of available energy types computed for this material"
+    )
+
     entries: Dict[str, ComputedEntry] = Field(
         None,
         description="List of all entries that are valid for this material."
