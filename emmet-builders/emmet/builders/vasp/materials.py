6777700ad51462b96330893cc34651f7bc8d8c28
Shyam D <shyamd@lbl.gov>
2020-10-16 12:50:46 -0700

    switch to generator method

--- emmet-builders/emmet/builders/vasp/materials.py
+++ emmet-builders/emmet/builders/vasp/materials.py
@@ -1,26 +1,20 @@
-from typing import Optional, Dict, List, Iterator
+from datetime import datetime
 from itertools import chain
 from operator import itemgetter
-from datetime import datetime
+from typing import Dict, Iterator, List, Optional
 
-from pymatgen import Structure
-from pymatgen.analysis.structure_matcher import StructureMatcher, ElementComparator
-
-from pymatgen.analysis.structure_analyzer import oxide_type
-
-
-from maggma.stores import Store
 from maggma.builders import Builder
+from maggma.stores import Store
+from pymatgen import Structure
+from pymatgen.analysis.structure_analyzer import oxide_type
+from pymatgen.analysis.structure_matcher import ElementComparator, StructureMatcher
 
 from emmet.builders import SETTINGS
-from emmet.core.vasp.calc_types import task_type, run_type, TaskType, CalcType
-
-from emmet.core.utils import group_structures, jsanitize
-
-from emmet.core.vasp.material import MaterialsDoc, PropertyOrigin
 from emmet.builders.utils import maximal_spanning_non_intersecting_subsets
+from emmet.core.utils import group_structures, jsanitize
+from emmet.core.vasp.calc_types import CalcType, TaskType, run_type, task_type
+from emmet.core.vasp.material import MaterialsDoc, PropertyOrigin
 from emmet.stubs import ComputedEntry
-
 
 __author__ = "Shyam Dwaraknath <shyamd@lbl.gov>"
 
@@ -226,7 +220,7 @@ class MaterialsBuilder(Builder):
         materials = []
         grouped_tasks = self.filter_and_group_tasks(tasks)
 
-        materials = [self.make_mat(group) for group in grouped_tasks]
+        materials = [MaterialsDoc.from_tasks(group) for group in grouped_tasks]
         self.logger.debug(f"Produced {len(materials)} materials for {formula}")
 
         return [mat.dict() for mat in materials if mat is not None]
@@ -299,189 +293,3 @@ class MaterialsBuilder(Builder):
                     if len(set(task.get("sandboxes", ["core"])).intersection(sbx_set))
                     > 0
                 ]
-
-    def make_mat(self, task_group: List[Dict]) -> Dict:
-        """
-        Converts a group of tasks into one material
-        """
-
-        # Metadata
-        last_updated = max(task[self.tasks.last_updated_field] for task in task_group)
-        created_at = min(task[self.tasks.last_updated_field] for task in task_group)
-        task_ids = list({task[self.tasks.key] for task in task_group})
-        sandboxes = list(
-            {sbxn for task in task_group for sbxn in task.get("sbxn", ["core"])}
-        )
-
-        deprecated_tasks = list(
-            {
-                task[self.tasks.key]
-                for task in task_group
-                if not task.get("is_valid", True)
-            }
-        )
-        run_types = {
-            t[self.tasks.key]: run_type(t["output"]["parameters"]) for t in task_group
-        }
-        task_types = {
-            t[self.tasks.key]: task_type(t["orig_inputs"]) for t in task_group
-        }
-        calc_types = {
-            task[self.tasks.key]: CalcType(
-                f"{run_types[task[self.tasks.key]]}"
-                + " "
-                + f"{task_types[task[self.tasks.key]]}"
-            )
-            for task in task_group
-        }
-
-        structure_optimizations = [
-            task
-            for task in task_group
-            if task_types[task[self.tasks.key]] == "Structure Optimization"
-        ]
-        statics = [
-            task for task in task_group if task_types[task[self.tasks.key]] == "Static"
-        ]
-
-        # Material ID
-        possible_mat_ids = [task[self.tasks.key] for task in structure_optimizations]
-        possible_mat_ids = sorted(possible_mat_ids, key=ID_to_int)
-
-        if len(possible_mat_ids) == 0:
-            self.logger.error(f"Could not find a material ID for {task_ids}")
-            return None
-        else:
-            material_id = possible_mat_ids[0]
-
-        def _structure_eval(task: Dict):
-            """
-            Helper function to order structures optimziation and statics calcs by
-            - Functional Type
-            - Spin polarization
-            - Special Tags
-            - Energy
-            """
-            qual_score = SETTINGS.vasp_qual_scores
-
-            ispin = task.get("output", {}).get("parameters", {}).get("ISPIN", 1)
-            energy = task.get("output", {}).get("energy_per_atom", 0.0)
-            task_run_type = run_type(task["output"]["parameters"])
-            special_tags = [
-                task.get("output", {}).get("parameters", {}).get(tag, False)
-                for tag in ["LASPH"]
-            ]
-
-            is_valid = task[self.tasks.key] in deprecated_tasks
-
-            return (
-                -1 * is_valid,
-                -1 * qual_score.get(task_run_type, 0),
-                -1 * ispin,
-                -1 * sum(special_tags),
-                energy,
-            )
-
-        structure_calcs = structure_optimizations + statics
-        best_structure_calc = sorted(structure_calcs, key=_structure_eval)[0]
-        structure = Structure.from_dict(best_structure_calc["output"]["structure"])
-
-        # Initial Structures
-        initial_structures = [
-            Structure.from_dict(t["input"]["structure"]) for t in task_group
-        ]
-        sm = StructureMatcher(
-            ltol=0.1, stol=0.1, angle_tol=0.1, scale=False, attempt_supercell=False
-        )
-        initial_structures = [
-            group[0] for group in sm.group_structures(initial_structures)
-        ]
-
-        # Deprecated
-        deprecated = all(
-            t[self.tasks.key] in deprecated_tasks for t in structure_optimizations
-        )
-
-        # Origins
-        _run_type = run_type(best_structure_calc["output"]["parameters"])
-        _task_type = task_type(best_structure_calc["orig_inputs"])
-        origins = [
-            PropertyOrigin(
-                name="structure",
-                task_type=f"{_run_type} {_task_type}",
-                task_id=best_structure_calc[self.tasks.key],
-                last_updated=best_structure_calc[self.tasks.last_updated_field],
-            )
-        ]
-
-        # entries
-        entries = {}
-        all_run_types = set(run_types.keys())
-        for rt in all_run_types:
-            rt_tasks = {t_id for t_id in task_ids if run_types[t_id] == rt}
-            relevant_calcs = [
-                doc for doc in structure_calcs if doc[self.tasks.key] in rt_tasks
-            ]
-            if len(relevant_calcs) > 0:
-                entry_dict = task_doc_to_entry(
-                    sorted(relevant_calcs, key=_structure_eval)[0]
-                )
-                entry_dict["data"]["task_id"] = entry_dict["entry_id"]
-                entry_dict["entry_id"] = material_id
-                entries[rt] = ComputedEntry.from_dict(entry_dict)
-
-        # Warnings
-        # TODO: What warning should we process?
-
-        return MaterialsDoc.from_structure(
-            structure=structure,
-            material_id=material_id,
-            last_updated=last_updated,
-            created_at=created_at,
-            task_ids=task_ids,
-            calc_types=calc_types,
-            run_types=run_types,
-            task_types=task_types,
-            initial_structures=initial_structures,
-            deprecated=deprecated,
-            deprecated_tasks=deprecated_tasks,
-            origins=origins,
-            entries=entries,
-            sandboxes=sandboxes if sandboxes else None,
-        )
-
-
-def ID_to_int(s_id: str) -> int:
-    """
-    Converts a string id to tuple
-    falls back to assuming ID is an Int if it can't process
-    Assumes string IDs are of form "[chars]-[int]" such as mp-234
-    """
-    if isinstance(s_id, str):
-        return (s_id.split("-")[0], int(str(s_id).split("-")[-1]))
-    elif isinstance(s_id, (int, float)):
-        return s_id
-    else:
-        return None
-
-
-def task_doc_to_entry(task_doc: Dict) -> Dict:
-    """ Turns a Task Doc into a ComputedEntry"""
-
-    struc = Structure.from_dict(task_doc["output"]["structure"])
-    entry_dict = {
-        "correction": 0.0,
-        "entry_id": task_doc["task_id"],
-        "composition": struc.composition,
-        "energy": task_doc["output"]["energy"],
-        "parameters": {
-            "potcar_spec": task_doc["potcar_spec"],
-            "run_type": run_type(task_doc["output"]["parameters"]),
-        },
-        "data": {
-            "oxide_type": oxide_type(struc),
-            "last_updated": task_doc["last_updated"],
-        },
-    }
-
-    return entry_dict
