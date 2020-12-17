9e9eccd0712d4e8a25414601ee0eb48a62e2582c
Shyam D <shyamd@lbl.gov>
2020-12-17 13:07:57 -0800

    make computed structure entry stub

--- emmet-core/emmet/stubs/__init__.py
+++ emmet-core/emmet/stubs/__init__.py
@@ -7,11 +7,12 @@ Use pymatgen classes in pydantic models by importing them from there when you ne
 from pymatgen.analysis.diffraction.xrd import DiffractionPattern
 from pymatgen.analysis.xas.spectrum import XAS
 from pymatgen.core.structure import Composition, Lattice, Structure
-from pymatgen.entries.computed_entries import ComputedEntry
+from pymatgen.entries.computed_entries import ComputedEntry, ComputedStructureEntry
 
+from emmet.stubs.entries import ComputedEntry as StubComputedEntry
+from emmet.stubs.entries import ComputedStructureEntry as StubComputedStructureEntry
 from emmet.stubs.math import Matrix3D, Vector3D
 from emmet.stubs.misc import Composition as StubComposition
-from emmet.stubs.misc import ComputedEntry as StubComputedEntry
 from emmet.stubs.structure import Lattice as StubLattice
 from emmet.stubs.structure import Structure as StubStructure
 from emmet.stubs.utils import patch_msonable, use_model
@@ -27,6 +28,7 @@ use_model(Lattice, StubLattice)
 use_model(Composition, StubComposition, add_monty=False)
 use_model(ComputedEntry, StubComputedEntry)
 use_model(DiffractionPattern, StubXRDPattern)
+use_model(ComputedStructureEntry, StubComputedStructureEntry)
 
 # This is after the main block since it depends on that
 from emmet.stubs.xas import XASSpectrum  # noqa
