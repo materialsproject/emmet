6b4af842afb1bf0743660de817c6aadebc2161c1
Shyam D <shyamd@lbl.gov>
2021-02-24 15:09:56 -0800

    test to check ordering of two MPIDs

--- tests/emmet-core/test_mpid.py
+++ tests/emmet-core/test_mpid.py
@@ -6,4 +6,5 @@ def test_mpid():
     assert MPID("mp-3") == MPID("mp-3")
     assert MPID("mp-3") < 3
     assert MPID("mp-3") < MPID("np-3")
+    assert MPID("mp-3") > MPID("mp-2")
     assert 3 > MPID("mp-3")
