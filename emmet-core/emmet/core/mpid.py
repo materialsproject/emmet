9b45b567b70d18309d20bdacab9c57d4a5c990ee
Shyam D <shyamd@lbl.gov>
2021-02-24 06:59:48 -0800

    new MPID type

---
+++ emmet-core/emmet/core/mpid.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import Union
+
+NOTHING = object()
+
+
+class MPID(str):
+    """
+    A Materials Project type ID with a prefix and an integer
+    This class enables seemlessly mixing MPIDs and regular integer IDs
+    Prefixed IDs are considered less than non-prefixed IDs to enable proper
+    mixing with the Materials Project
+    """
+
+    def __lt__(self, other: Union["MPID", int]):
+
+        # Always sort MPIDs before pure integer IDs
+        if isinstance(other, int):
+            return True
+
+        self_parts = self.split("-")
+        other_parts = other.split()
+
+        return self_parts < other_parts
