245bf4740b4ea070daeae687a9cb018a0308b2a1
Shyam D <shyamd@lbl.gov>
2021-02-24 21:48:25 -0800

    update validator for SCF gradient

--- emmet-builders/emmet/builders/vasp/task_validator.py
+++ emmet-builders/emmet/builders/vasp/task_validator.py
@@ -9,6 +9,7 @@ from emmet.core import SETTINGS
 from emmet.core.vasp.calc_types import run_type, task_type
 from emmet.core.vasp.task import TaskDocument
 from emmet.core.vasp.validation import DeprecationMessage, ValidationDoc
+from emmet.core.settings import EmmetSettings
 
 __author__ = "Shyam Dwaraknath"
 __email__ = "shyamd@lbl.gov"
@@ -19,7 +20,7 @@ class TaskValidator(MapBuilder):
         self,
         tasks: Store,
         task_validation: Store,
-        kpts_tolerance: float = SETTINGS.VASP_KPTS_TOLERANCE,
+        settings: EmmetSettings = SETTINGS,
         **kwargs,
     ):
         """
@@ -28,20 +29,22 @@ class TaskValidator(MapBuilder):
         Args:
             tasks: Store of task documents
             task_validation: Store of task_types for tasks
-            input_sets: dictionary of task_type and pymatgen input set to validate against
-            kpts_tolerance: the minimum kpt density as dictated by the InputSet to require
-            LDAU_fields: LDAU fields to check for consistency
         """
         self.tasks = tasks
         self.task_validation = task_validation
-        self.kpts_tolerance = kpts_tolerance
+        self.settings = settings
 
         self.kwargs = kwargs
 
         super().__init__(
             source=tasks,
             target=task_validation,
-            projection=["orig_inputs", "output.structure", "input.parameters"],
+            projection=[
+                "orig_inputs",
+                "output.structure",
+                "input.parameters",
+                "calcs_reversed.output.ionic_steps.e_fr_energy",
+            ],
             **kwargs,
         )
 
@@ -53,4 +56,10 @@ class TaskValidator(MapBuilder):
             item (dict): a (projection of a) task doc
         """
         task_doc = TaskDocument(**item)
-        return ValidationDoc.from_task_doc(task_doc=task_doc).dict()
+        return ValidationDoc.from_task_doc(
+            task_doc=task_doc,
+            kpts_tolerance=self.settings.VASP_KPTS_TOLERANCE,
+            input_sets=self.settings.VASP_DEFAULT_INPUT_SETS,
+            LDAU_fields=self.settings.VASP_CHECKED_LDAU_FIELDS,
+            max_allowed_scf_gradient=self.settings.VASP_MAX_SCF_GRADIENT,
+        ).dict()
