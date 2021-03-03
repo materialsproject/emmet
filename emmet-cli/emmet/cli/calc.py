89801d1a8e16d71dbbb64ca5d28b1e8d4be407b4
Patrick Huck <phuck@lbl.gov>
2021-03-03 14:29:26 -0800

    cli: protect task_type import error

--- emmet-cli/emmet/cli/calc.py
+++ emmet-cli/emmet/cli/calc.py
@@ -15,7 +15,6 @@ from pymatgen.alchemy.materials import TransformedStructure
 from pymatgen.util.provenance import StructureNL, Author
 
 from emmet.core.utils import group_structures, get_sg
-from emmet.core.vasp.calc_types import task_type
 from emmet.cli import SETTINGS
 from emmet.cli.utils import calcdb_from_mgrant, aggregate_by_formula, structures_match
 from emmet.cli.utils import get_meta_from_structure, load_structure
@@ -37,6 +36,7 @@ def get_format(fname):
 
 
 def load_canonical_structures(ctx, full_name, formula):
+    from emmet.core.vasp.calc_types import task_type  # TODO import error
     collection = ctx.obj["COLLECTIONS"][full_name]
 
     if formula not in canonical_structures[full_name]:
