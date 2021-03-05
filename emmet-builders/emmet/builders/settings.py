a85acb03be5b94dbbe78fc4a21df6651dde0442b
Shyam D <shyamd@lbl.gov>
2021-03-05 13:28:29 -0800

    Move more build settings into EmmetBuildSettings

--- emmet-builders/emmet/builders/settings.py
+++ emmet-builders/emmet/builders/settings.py
@@ -1,9 +1,10 @@
 """
 Settings for defaults in the build pipelines for the Materials Project
 """
-from typing import Union
+from typing import List
 from pydantic.fields import Field
 from emmet.core.settings import EmmetSettings
+from emmet.core.vasp.calc_types import TaskType
 
 
 class EmmetBuildSettings(EmmetSettings):
@@ -13,5 +14,10 @@ class EmmetBuildSettings(EmmetSettings):
     EMMET_CONFIG_FILE to point to the json with emmet settings
     """
 
-    BUILD_TAGS: Union[str, list] = Field([], description="Tags for the build")
-    DEPRECATED_TAGS: Union[str, list] = Field([], description="Tags to deprecate")
+    BUILD_TAGS: List[str] = Field([], description="Tags for the build")
+    DEPRECATED_TAGS: List[str] = Field([], description="Tags to deprecate")
+
+    VASP_ALLOWED_VASP_TYPES: List[TaskType] = Field(
+        [t.value for t in TaskType],
+        description="Allowed task_types to build materials from",
+    )
