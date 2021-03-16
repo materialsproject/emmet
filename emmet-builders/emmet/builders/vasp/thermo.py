64d3dc8ef02216c863c42a1ded47befa65365499
jmmshn <jmmshn@gmail.com>
2021-03-15 22:03:28 -0700

    updated error message

--- emmet-builders/emmet/builders/vasp/thermo.py
+++ emmet-builders/emmet/builders/vasp/thermo.py
@@ -114,7 +114,7 @@ class Thermo(Builder):
         )
         chemsys = "-".join(elements)
 
-        self.logger.debug(f"Procesing {len(entries)} entries for {chemsys}")
+        self.logger.debug(f"Processing {len(entries)} entries for {chemsys}")
 
         material_entries = defaultdict(dict)
         pd_entries = []
@@ -128,6 +128,7 @@ class Thermo(Builder):
             elif "GGA" in material_entries[material_id]:
                 pd_entries.append(material_entries[material_id]["GGA"])
         pd_entries = self.compatibility.process_entries(pd_entries)
+        self.logger.debug(f"{len(pd_entries)} remain in {chemsys} after filtering")
 
         try:
             docs = ThermoDoc.from_entries(pd_entries)
