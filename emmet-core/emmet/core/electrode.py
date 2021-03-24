c07a7db2f9f1407223d83a6193cacc0461f9b9f1
jmmshn <jmmshn@gmail.com>
2021-03-24 09:49:04 -0700

    num steps int

--- emmet-core/emmet/core/electrode.py
+++ emmet-core/emmet/core/electrode.py
@@ -206,7 +206,7 @@ class ConversionElectrodeDoc(ConversionVoltagePairDoc):
         description="The working ion as an Element object",
     )
 
-    num_steps: float = Field(
+    num_steps: int = Field(
         None,
         description="The number of distinct voltage steps in from fully charge to "
         "discharge based on the stable intermediate states",
