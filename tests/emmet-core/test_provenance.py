78a60ba077bb1a827a4d222ee61827f37e6a64b2
Jason Munro <jmunro@lbl.gov>
2021-09-02 14:09:15 -0700

    Fix provenance tests

--- tests/emmet-core/test_provenance.py
+++ tests/emmet-core/test_provenance.py
@@ -49,7 +49,7 @@ def test_from_snls(snls, structure):
     }
 
     # Test experimental detection
-    snls[0].about.history[0].experimental = True
+    snls[0].about.history[0].description["experimental"] = True
     assert (
         ProvenanceDoc.from_SNLs(material_id="mp-3", snls=snls, structure=structure, deprecated=False).theoretical
         is False
