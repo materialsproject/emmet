7459d0357e39d8fa2cd6feac903f09aa6a624b5e
Mingjian Wen <wenxx151@gmail.com>
2021-09-24 16:22:57 -0700

    Add typing and description all fields

--- emmet-core/emmet/core/elasticity.py
+++ emmet-core/emmet/core/elasticity.py
@@ -1,6 +1,15 @@
+from typing import List, Tuple
+
 from pydantic import Field
 
 from emmet.core.material_property import PropertyDoc
+from emmet.core.math import Matrix3D
+
+# TODO, move to core.math?
+VoigtVector = Tuple[float, float, float, float, float, float]
+VoigtTensor = Tuple[
+    VoigtVector, VoigtVector, VoigtVector, VoigtVector, VoigtVector, VoigtVector
+]
 
 
 class ElasticityDoc(PropertyDoc):
@@ -10,45 +19,57 @@ class ElasticityDoc(PropertyDoc):
 
     property_name: str = "elasticity"
 
-    order: int = Field(..., description="Order of the elastic tensor.")
+    order: int = Field(..., default=2, description="Order of the elastic tensor")
 
-    strains = Field(..., description="Lagrangian strain tensors applied to structures.")
-    cauchy_stresses = Field(
-        ..., description="Cauchy stress tensors on strained structures."
+    strains: List[Matrix3D] = Field(
+        ..., description="Lagrangian strain tensors applied to structures"
+    )
+    cauchy_stresses: List[Matrix3D] = Field(
+        ..., description="Cauchy stress tensors on strained structures"
     )
 
-    elastic_tensor = Field(
+    elastic_tensor: VoigtTensor = Field(
         ...,
         description="Elastic tensor corresponding to IEEE orientation, symmetrized to "
-        "crystal structure.",
+        "crystal structure",
     )
-    compliance_tensor = Field(
-        ..., description="Compilance tensor corresponing to IEEE orientation."
+    compliance_tensor: VoigtTensor = Field(
+        ..., description="Compliance tensor corresponding to IEEE orientation"
     )
-    elastic_tensor_original = Field(
+    elastic_tensor_original: VoigtTensor = Field(
         ...,
         description="Elastic tensor corresponding to POSCAR (conventional standard "
         "cell) orientation unsymmetrized",
     )
 
-    k_voigt: float = Field(..., description="Bulk modulus Voigt average.")
-    k_reuss: float = Field(..., description="Bulk modulus Reuss average.")
-    k_vrh: float = Field(..., description="Bulk modulus VRH average.")
-    g_voigt: float = Field(..., description="Shear modulus Voigt average.")
-    g_reuss: float = Field(..., description="Shear modulus Reuss average.")
-    g_vrh: float = Field(..., description="Shear modulus VRH average.")
+    k_voigt: float = Field(..., description="Bulk modulus Voigt average")
+    k_reuss: float = Field(..., description="Bulk modulus Reuss average")
+    k_vrh: float = Field(..., description="Bulk modulus Voigt-Reuss-Hill average")
+    g_voigt: float = Field(..., description="Shear modulus Voigt average")
+    g_reuss: float = Field(..., description="Shear modulus Reuss average")
+    g_vrh: float = Field(..., description="Shear modulus Voigt-Reuss-Hill average")
 
-    snyder_ac: float = Field(...)
-    snyder_total: float = Field(...)
-    snyder_opt: float = Field(...)
+    snyder_ac: float = Field(
+        ..., description="Snyder's acoustic sound velocity (in SI units)"
+    )
+    snyder_opt: float = Field(
+        ..., description="Snyder's optical sound velocity (in SI units)"
+    )
+    snyder_total: float = Field(
+        ..., description="Snyder's total sound velocity (in SI units)"
+    )
 
-    clarke_thermalcond: float = Field(...)
-    cahill_thermalcond: float = Field(...)
-    debye_temperature: float = Field(...)
+    clarke_thermalcond: float = Field(
+        ..., description="Clarke's thermal conductivity (in SI units)"
+    )
+    cahill_thermalcond: float = Field(
+        ..., description="Cahill's thermal conductivity (in SI units)"
+    )
+    debye_temperature: float = Field(..., description="Debye temperature (in SI units)")
 
-    trans_v: float = Field(...)
-    loong_v: float = Field(...)
+    trans_v: float = Field(..., description="Transverse sound velocity (in SI units)")
+    long_v: float = Field(..., description="Longitudinal sound velocity (in SI units)")
 
-    universal_anisotropy: float = Field(...)
-    homogeneous_poisson: float = Field(...)
-    y_mod: float = Field(...)
+    universal_anisotropy: float = Field(..., description="Universal elastic anisotropy")
+    homogeneous_poisson: float = Field(..., description="Isotropic Poisson ratio")
+    y_mod: float = Field(..., description="Young's modulus")
