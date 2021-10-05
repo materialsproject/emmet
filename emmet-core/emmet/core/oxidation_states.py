fbaf724c6cda4cdd28d5277608f52679971e9c75
Matthew Horton <mkhorton@users.noreply.github.com>
2021-10-04 19:42:57 -0700

    Ensure that oxidation state doc always returns valid doc ...
    ... even in the case where oxidation state guessing fails, the input structure (with appropriate warning) is returned. This will make downstream building a lot simpler. Notifying @munrojm of change.

--- emmet-core/emmet/core/oxidation_states.py
+++ emmet-core/emmet/core/oxidation_states.py
@@ -18,20 +18,40 @@ class OxidationStateDoc(PropertyDoc):
 
     property_name = "oxidation"
 
-    structure: Structure = Field(..., description="The structure used in the generation of the oxidation state data")
-    possible_species: List[str] = Field(description="Possible charged species in this material")
-    possible_valences: List[float] = Field(description="List of valences for each site in this material")
-    average_oxidation_states: Dict[str, float] = Field(description="Average oxidation states for each unique species")
+    structure: Structure = Field(
+        ...,
+        description="The structure used in the generation of the oxidation state data",
+    )
+    possible_species: List[str] = Field(
+        description="Possible charged species in this material"
+    )
+    possible_valences: List[float] = Field(
+        description="List of valences for each site in this material"
+    )
+    average_oxidation_states: Dict[str, float] = Field(
+        description="Average oxidation states for each unique species"
+    )
     method: str = Field(description="Method used to compute oxidation states")
 
     @classmethod
     def from_structure(cls, structure: Structure, material_id: MPID, **kwargs):  # type: ignore[override]
+
+        # TODO: add check for if it already has oxidation states, if so pass this along unchanged ("method": "manual")
         structure.remove_oxidation_states()
+
+        # Null document
+        d = {
+            "possible_species": [],
+            "possible_valences": [],
+            "average_oxidation_states": {},
+        }
+
         try:
             bva = BVAnalyzer()
             valences = bva.get_valences(structure)
             possible_species = {
-                str(Specie(structure[idx].specie, oxidation_state=valence)) for idx, valence in enumerate(valences)
+                str(Specie(structure[idx].specie, oxidation_state=valence))
+                for idx, valence in enumerate(valences)
             }
 
             structure.add_oxidation_state_by_site(valences)
@@ -45,7 +65,10 @@ class OxidationStateDoc(PropertyDoc):
             for site in structure:
                 site_oxidation_list[site.specie.element].append(site.specie.oxi_state)
 
-            oxi_state_dict = {str(el): np.mean(oxi_states) for el, oxi_states in site_oxidation_list.items()}
+            oxi_state_dict = {
+                str(el): np.mean(oxi_states)
+                for el, oxi_states in site_oxidation_list.items()
+            }
 
             d = {
                 "possible_species": list(possible_species),
@@ -58,10 +81,15 @@ class OxidationStateDoc(PropertyDoc):
             logging.error("BVAnalyzer failed with: {}".format(e))
 
             try:
-                first_oxi_state_guess = structure.composition.oxi_state_guesses(max_sites=-50)[0]
-                valences = [first_oxi_state_guess[site.species_string] for site in structure]
+                first_oxi_state_guess = structure.composition.oxi_state_guesses(
+                    max_sites=-50
+                )[0]
+                valences = [
+                    first_oxi_state_guess[site.species_string] for site in structure
+                ]
                 possible_species = {
-                    str(Specie(el, oxidation_state=valence)) for el, valence in first_oxi_state_guess.items()
+                    str(Specie(el, oxidation_state=valence))
+                    for el, valence in first_oxi_state_guess.items()
                 }
 
                 structure.add_oxidation_state_by_site(valences)
@@ -75,8 +103,12 @@ class OxidationStateDoc(PropertyDoc):
 
             except Exception as e:
                 logging.error("Oxidation state guess failed with: {}".format(e))
-                raise e
+                d["warnings"] = ["Oxidation state guessing failed."]
 
         return super().from_structure(
-            meta_structure=structure, material_id=material_id, structure=structure, **d, **kwargs
+            meta_structure=structure,
+            material_id=material_id,
+            structure=structure,
+            **d,
+            **kwargs
         )
