7ba9ed3ec5fa2fba5d7b4a29701712a2138b6297
Jason Munro <jmunro@lbl.gov>
2021-11-16 13:23:40 -0800

    Nest emmet meta and include db version

--- emmet-core/emmet/core/base.py
+++ emmet-core/emmet/core/base.py
@@ -1,6 +1,6 @@
 """ Base emmet model to add default metadata """
 from datetime import datetime
-from typing import List, Optional, Type, TypeVar
+from typing import TypeVar, Dict
 
 from pydantic import BaseModel, Field
 from pymatgen.core import __version__ as pmg_version
@@ -10,9 +10,9 @@ from emmet.core import __version__
 T = TypeVar("T", bound="EmmetBaseModel")
 
 
-class EmmetBaseModel(BaseModel):
+class EmmetMeta(BaseModel):
     """
-    Base Model for default emmet metadata
+    Default emmet metadata
     """
 
     emmet_version: str = Field(
@@ -22,7 +22,22 @@ class EmmetBaseModel(BaseModel):
         pmg_version, description="The version of pymatgen this document was built with"
     )
 
-    build_date: datetime = Field(
-        default_factory=datetime.utcnow,
-        description="The build date for this document",
+    pull_request: int = Field(
+        None, description="The pull request number associated with this data build"
     )
+
+    database_version: str = Field(
+        None, description="The database version for the built data"
+    )
+
+    build_date: datetime = Field(
+        default_factory=datetime.utcnow, description="The build date for this document",
+    )
+
+
+class EmmetBaseModel(BaseModel):
+    """
+    Base Model for default emmet data
+    """
+
+    builder_meta: EmmetMeta
