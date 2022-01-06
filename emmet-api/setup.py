5967477dbcf02ab1fddb7ddc7e809c4ac1314935
Jason Munro <jmunro@lbl.gov>
2022-01-05 16:23:19 -0800

    Update api reqs

--- emmet-api/setup.py
+++ emmet-api/setup.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_namespace_packages, setup
+from setuptools_scm import get_version
+
+version = get_version(root="..", relative_to=__file__, version_scheme="post-release")
+version = version.split(".post")[0]
 
 setup(
     name="emmet-api",
@@ -11,7 +15,7 @@ setup(
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
     install_requires=[
-        "emmet-core",
+        f"emmet-core~={version}",
         "fastapi",
         "maggma",
         "uvicorn",
