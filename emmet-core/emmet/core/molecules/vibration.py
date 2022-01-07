3eb598db8f014ee29bfb24d761f39e8fde6118c9
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2022-01-07 07:09:37 -0800

    Vibrational doc

--- emmet-core/emmet/core/molecules/vibration.py
+++ emmet-core/emmet/core/molecules/vibration.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import numpy as np
 from pydantic import Field
 
-from pymatgen.core.periodic_table import Element
+from pymatgen.core.structure import Molecule
 
 from emmet.core.mpid import MPID
 from emmet.core.structure import MoleculeMetadata
@@ -44,7 +44,7 @@ class SpectrumDoc(MoleculeMetadata):
 
 class VibSpectrumDoc(SpectrumDoc):
 
-    spectrum_name = "IR Vibrational"
+    spectrum_name = "IR_vibrational"
 
     frequencies: List[float] = Field(..., title="Vibrational Frequencies",
                                      description="List of vibrational frequencies in the molecules' spectrum")
@@ -55,11 +55,11 @@ class VibSpectrumDoc(SpectrumDoc):
     task_id: MPID = Field(..., title="Calculation ID", description="Task ID used to make this IR spectrum")
 
     @classmethod
-    def from_task(cls, task: TaskDocument, molecule_id: MPID, **kwargs):
+    def from_task(cls, task: TaskDocument, molecule_id: MPID, **kwargs): # type: ignore[override]
         """
         Construct a vibrational spectrum document
 
-        task document from which bonding properties can be extracted
+        task document from which vibrational spectrum can be extracted
         :param molecule_id: mpid
         :param kwargs: to pass to SpectrumDoc
         :return:
@@ -93,5 +93,53 @@ class VibSpectrumDoc(SpectrumDoc):
             **kwargs,
         )
 
-class VibrationalModesDoc(PropertyDoc):
-    pass
+class VibrationDoc(PropertyDoc):
+
+    property_name = "vibrational_modes"
+
+    molecule: Molecule
+
+    frequencies: List[float] = Field(description="List of molecular vibrational frequencies")
+
+    frequency_modes: List[List[List[float]]] = Field(description="Vibrational frequency modes of the molecule")
+
+    spectrum: VibSpectrumDoc = Field(description="Vibrational frequency spectrum, including IR intensities")
+
+    @classmethod
+    def from_task(
+        cls,
+        task: TaskDocument,
+        molecule_id: MPID,
+        **kwargs
+    ): # type: ignore[override]
+        """
+        Construct a vibration document from a task document
+
+        task document from which vibrational properties can be extracted
+        :param molecule_id: mpid
+        :param kwargs: to pass to PropertyDoc
+        :return:
+        """
+
+        if task.output.frequencies is None or task.output.frequency_modes is None:
+            raise Exception("No frequencies in task!")
+
+        if task.output.optimized_molecule is not None:
+            mol = task.output.optimized_molecule
+        else:
+            mol = task.output.initial_molecule
+
+        spectrum = VibSpectrumDoc.from_task(task, molecule_id=molecule_id, **kwargs)
+
+        frequencies = task.output.frequencies
+        frequency_modes =task.output.frequency_modes
+
+        return super().from_molecule(
+            meta_molecule=mol,
+            molecule_id=molecule_id,
+            molecule=mol,
+            frequencies=frequencies,
+            frequency_modes=frequency_modes,
+            spectrum=spectrum,
+            **kwargs
+        )
