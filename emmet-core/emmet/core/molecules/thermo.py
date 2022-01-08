8b24cc72d3fd486f9486397fc2ab498cf137c01d
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2022-01-08 12:19:37 -0800

    Small changes to thermo; still not making a ThermoDoc for now (but probably should eventually?)

--- emmet-core/emmet/core/molecules/thermo.py
+++ emmet-core/emmet/core/molecules/thermo.py
@@ -1,3 +1,26 @@
+import logging
+from collections import defaultdict
+from typing import Dict, List, Any, Tuple, Union
+import copy
+
+from typing_extensions import Literal
+
+import numpy as np
+from pydantic import Field
+import networkx as nx
+
+from pymatgen.core.structure import Molecule
+from pymatgen.analysis.graphs import MoleculeGraph
+from pymatgen.analysis.local_env import OpenBabelNN, metal_edge_extender
+
+from pymatgen.core.periodic_table import Specie, Element
+
+from emmet.core.mpid import MPID
+from emmet.core.qchem.task import TaskDocument
+from emmet.core.material import PropertyOrigin
+from emmet.core.molecules.molecule_property import PropertyDoc
+
+
 def get_free_energy(energy, enthalpy, entropy, temperature=298.15):
     """
     Helper function to calculate Gibbs free energy from electronic energy, enthalpy, and entropy
@@ -11,3 +34,5 @@ def get_free_energy(energy, enthalpy, entropy, temperature=298.15):
 
     """
     return energy * 27.2114 + enthalpy * 0.043363 - temperature * entropy * 0.000043363
+
+
