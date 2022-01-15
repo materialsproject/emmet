2448200e3a2007734773d2a5c923cfe53d6ad8e7
Jason Munro <jmunro@lbl.gov>
2022-01-15 11:57:32 -0800

    Alter how version is determined (#337)
    * Alter how version is determined
    
    * Fix github install issues with version

--- emmet-builders/setup.py
+++ emmet-builders/setup.py
@@ -1,20 +1,28 @@
 from setuptools import find_namespace_packages, setup
-from setuptools_scm import get_version
 
-version = get_version(root="..", relative_to=__file__, version_scheme="post-release")
-version = version.split(".post")[0]
+with open("../_version.py") as file:
+    for line in file.readlines():
+        lsplit = line.split("=")
+        if lsplit[0].strip() == "__version__":
+            fallback_version = lsplit[1].strip().replace('"', "").split("+")[0]
 
 
 setup(
     name="emmet-builders",
-    use_scm_version={"root": "..", "relative_to": __file__},
+    use_scm_version={
+        "root": "..",
+        "relative_to": __file__,
+        "write_to": "_version.py",
+        "write_to_template": '__version__ = "{version}"',
+        "fallback_version": fallback_version,
+    },
     setup_requires=["setuptools_scm"],
     description="Builders for the Emmet Library",
     author="The Materials Project",
     author_email="feedback@materialsproject.org",
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
-    install_requires=[f"emmet-core~={version}", "maggma>=0.38.1"],
+    install_requires=[f"emmet-core~={fallback_version}", "maggma>=0.38.1"],
     license="modified BSD",
     zip_safe=False,
 )
