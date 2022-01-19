f57e69735333871e752490adede01b1933b9951e
Jason Munro <jmunro@lbl.gov>
2022-01-19 15:18:51 -0800

    Revert to get_distribution

--- emmet-api/emmet/api/__init__.py
+++ emmet-api/emmet/api/__init__.py
@@ -3,4 +3,10 @@ Core module exposes the document interfaces
 These will be ingested via Drones, built by Builders, and served via the API
 """
 
-from emmet.api._version import __version__
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution("emmet-api").version
+except DistributionNotFound:  # pragma: no cover
+    # package is not installed
+    pass
