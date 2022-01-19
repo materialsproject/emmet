83053b1dca66ad313aba4a02ad9bf4dfffd3d43a
Jason Munro <jmunro@lbl.gov>
2022-01-19 13:26:26 -0800

    Revert to get_distribution in init

--- emmet-builders/emmet/builders/__init__.py
+++ emmet-builders/emmet/builders/__init__.py
@@ -1,4 +1,10 @@
+from pkg_resources import DistributionNotFound, get_distribution
 from emmet.builders.settings import EmmetBuildSettings
-from emmet.builders._version import __version__
 
 SETTINGS = EmmetBuildSettings()
+
+try:
+    __version__ = get_distribution("emmet-builders").version
+except DistributionNotFound:  # pragma: no cover
+    # package is not installed
+    pass
