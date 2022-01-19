f8bed63f023d5abf8563fc6e568fe98b0ae808dd
Jason Munro <jmunro@lbl.gov>
2022-01-19 15:42:30 -0800

    Revert nesting _verion.py (#351)

--- emmet-builders/setup.py
+++ emmet-builders/setup.py
@@ -1,19 +1,16 @@
 from setuptools import find_namespace_packages, setup
-from emmet.builders import __version__ as fallback_version
-from emmet.core import __version__ as core_version
+from _version import __version__ as fallback_version
 
 if "+" in fallback_version:
     fallback_version = fallback_version.split("+")[0]
 
-if "+" in core_version:
-    core_version = core_version.split("+")[0]
 
 setup(
     name="emmet-builders",
     use_scm_version={
         "root": "..",
         "relative_to": __file__,
-        "write_to": "emmet-builders/emmet/builders/_version.py",
+        "write_to": "emmet-builders/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "fallback_version": fallback_version,
     },
@@ -23,7 +20,7 @@ setup(
     author_email="feedback@materialsproject.org",
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
-    install_requires=[f"emmet-core~={core_version}", "maggma>=0.38.1"],
+    install_requires=["emmet-core>=0.21.9", "maggma>=0.38.1"],
     python_requires=">=3.8",
     license="modified BSD",
     zip_safe=False,
