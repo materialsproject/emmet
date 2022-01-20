2ac09c6a040071815f245407b22123f9c72efbab
Jason Munro <jmunro@lbl.gov>
2022-01-19 16:48:16 -0800

    Remove version req in setup

--- emmet-api/setup.py
+++ emmet-api/setup.py
@@ -21,7 +21,7 @@ setup(
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
     install_requires=[
-        "emmet-core>=0.21.9",
+        "emmet-core",
         "fastapi",
         "uvicorn",
         "gunicorn",
