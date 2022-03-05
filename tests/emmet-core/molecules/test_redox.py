14996377e0e667914a5490954211146f5f84b5f8
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2022-03-05 12:32:53 -0800

    Fixing tests part 2 (now with JSONs!)

--- tests/emmet-core/molecules/test_redox.py
+++ tests/emmet-core/molecules/test_redox.py
@@ -8,16 +8,16 @@ import pytest
 from monty.io import zopen
 from monty.serialization import loadfn
 
-from emmet.core.qchem.task import TaskDocument
+from emmet.core.qchem.molecule import MoleculeDoc
 from emmet.core.molecules.redox import RedoxDoc
 
 
 @pytest.fixture(scope="session")
 def test_mols(test_dir):
-    with open((test_dir / "redox_mol_docs.pickle").as_posix(), "rb") as f:
-        data = pickle.load(f)
+    mols = loadfn((test_dir / "builder_mols_set.json").as_posix())
+    mols = [MoleculeDoc(**x) for x in mols]
 
-    return data
+    return mols
 
 
 def test_redox(test_mols):
@@ -28,14 +28,10 @@ def test_redox(test_mols):
 
     redox_docs = RedoxDoc.from_entries(entries)
 
-    assert len(redox_docs) == 3
+    assert len(redox_docs) == 17
     assert redox_docs[0].property_name == "redox"
 
-    assert redox_docs[1].electron_affinity == pytest.approx(-0.7226193632551298)
-    assert redox_docs[1].ea_id == 845318
-    assert redox_docs[1].ionization_energy == pytest.approx(11.058520187149718)
-    assert redox_docs[1].ie_id == 888790
-    assert redox_docs[1].reduction_free_energy == pytest.approx(-1.0584641522518723)
-    assert redox_docs[1].red_id == 116015
-    assert redox_docs[1].oxidation_free_energy == pytest.approx(11.049545505758942)
-    assert redox_docs[1].ox_id == 116016
+    assert redox_docs[1].electron_affinity == pytest.approx(-2.928377375046328)
+    assert str(redox_docs[1].ea_id) == "847651"
+    assert redox_docs[1].ionization_energy == pytest.approx(7.115431161042942)
+    assert str(redox_docs[1].ie_id) == "891146"
