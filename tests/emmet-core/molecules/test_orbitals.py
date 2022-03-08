d142e1d6fb1ad264ab6487ed99c30ab5a86c08be
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2022-03-07 17:25:10 -0800

    Fix one failed test (orbitals)

--- tests/emmet-core/molecules/test_orbitals.py
+++ tests/emmet-core/molecules/test_orbitals.py
@@ -30,19 +30,19 @@ def test_orbital(closed_shell, open_shell):
     assert doc.property_name == "natural bonding orbitals"
     assert doc.open_shell == False
 
-    assert len(doc.population) == len(closed_shell.output.initial_molecule)
-    assert doc.population[0].valence_electrons == pytest.approx(2.75426)
-    assert len(doc.lone_pairs) == 3
-    assert doc.lone_pairs[0].s_character == pytest.approx(0.02)
-    assert doc.lone_pairs[0].atom_index == 0
-    assert len(doc.bonds) == 10
-    assert doc.bonds[0].atom1_s_character == pytest.approx(29.93)
-    assert doc.bonds[0].atom1_index == 0
-    assert doc.bonds[0].atom2_index == 3
-    assert len(doc.interactions) == 95
-    assert doc.interactions[0].donor_index == 8
-    assert doc.interactions[0].acceptor_index == 19
-    assert doc.interactions[0].energy_difference == pytest.approx(0.95)
+    assert len(doc.nbo_population) == len(closed_shell.output.initial_molecule)
+    assert doc.nbo_population[0].valence_electrons == pytest.approx(2.75426)
+    assert len(doc.nbo_lone_pairs) == 3
+    assert doc.nbo_lone_pairs[0].s_character == pytest.approx(0.02)
+    assert doc.nbo_lone_pairs[0].atom_index == 0
+    assert len(doc.nbo_bonds) == 10
+    assert doc.nbo_bonds[0].atom1_s_character == pytest.approx(29.93)
+    assert doc.nbo_bonds[0].atom1_index == 0
+    assert doc.nbo_bonds[0].atom2_index == 3
+    assert len(doc.nbo_interactions) == 95
+    assert doc.nbo_interactions[0].donor_index == 8
+    assert doc.nbo_interactions[0].acceptor_index == 19
+    assert doc.nbo_interactions[0].energy_difference == pytest.approx(0.95)
     assert doc.alpha_population is None
     assert doc.beta_population is None
 
@@ -51,8 +51,8 @@ def test_orbital(closed_shell, open_shell):
 
     assert doc.open_shell == True
 
-    assert len(doc.population) == len(open_shell.output.initial_molecule)
+    assert len(doc.nbo_population) == len(open_shell.output.initial_molecule)
     assert doc.alpha_population is not None
     assert doc.beta_population is not None
-    assert doc.lone_pairs is None
-    assert doc.bonds is None
+    assert doc.nbo_lone_pairs is None
+    assert doc.nbo_bonds is None
