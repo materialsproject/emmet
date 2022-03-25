9a70b3203cb09feac69be31de4aeda780694118c
munrojm <jmunro@lbl.gov>
2022-03-25 12:19:12 -0700

    Trajectory endpoint fix

--- emmet-api/emmet/api/routes/tasks/utils.py
+++ emmet-api/emmet/api/routes/tasks/utils.py
@@ -17,10 +17,10 @@ def calcs_reversed_to_trajectory(calcs_reversed: List[dict]):
     trajectories = []
 
     for calculation in calcs_reversed:
+        structures = []
+        frame_props = defaultdict(list)  # type: dict
+        
         for step in calculation["output"]["ionic_steps"]:
-
-            structures = []
-            frame_props = defaultdict(list)  # type: dict
 
             structures.append(Structure.from_dict(step["structure"]))
 
