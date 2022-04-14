01a6116470dbb8f49b86b4dc829cee6faa72ad34
munrojm <jmunro@lbl.gov>
2022-04-14 15:14:54 -0700

    Fix aggregation and find structure tests

--- tests/emmet-api/robocrys/test_query_operators.py
+++ tests/emmet-api/robocrys/test_query_operators.py
@@ -55,10 +55,8 @@ def test_robocrys_search_query():
     with ScratchDir("."):
         dumpfn(op, "temp.json")
         new_op = loadfn("temp.json")
-        query = {
-            "pipeline": pipeline
-        }
-        assert new_op.query(keywords="cubic, octahedra", skip=0, limit=10) == query
+        query = {"pipeline": pipeline}
+        assert new_op.query(keywords="cubic, octahedra", _skip=0, _limit=10) == query
 
     assert op.post_process([{"total_doc": 10}], query) == [{"total_doc": 10}]
     assert op.meta() == {"total_doc": 10}
