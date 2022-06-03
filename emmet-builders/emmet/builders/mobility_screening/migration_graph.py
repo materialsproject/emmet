a74b3e6e6bd508712232428bbf9976611e504e8e
alexnguyen0512 <dknguyenngoc@lbl.gov>
2022-06-03 15:36:15 -0700

    Update migration_graph.py
    changed field labels to match convention and added docstrings

--- emmet-builders/emmet/builders/mobility_screening/migration_graph.py
+++ emmet-builders/emmet/builders/mobility_screening/migration_graph.py
@@ -3,46 +3,46 @@ class MigrationGraphBuilder(MapBuilder):
         self,
         electrodes: MongoStore,
         tasks: MongoStore,
-        target: MongoStore,
+        migration_graphs: MongoStore,
         ltol: float = 0.4,
         stol: float = 0.6,
         angle_tol: float = 15,
         **kwargs
-    ):
+    ) -> MapBuilder:
         self.electrodes = electrodes
         self.tasks = tasks
         self.ltol = ltol
         self.stol = stol
         self.angle_tol = angle_tol
-        self.target = target
+        self.migration_graphs = migration_graphs
         self.wi_entries = {}
-        
-        super().__init__(source=electrodes, target=target, **kwargs)
+
+        super().__init__(source=electrodes, target=migration_graphs, **kwargs)
         self.sources.append(tasks)
-    
-    def get_items(self):
+
+    def get_items(self) -> dict:
         for item in super(MigrationGraphBuilder,self).get_items():
             tds = list(self.tasks.query({"task_id":{"$in":item["material_ids"]}}))
             item.update({"task_docs":tds})
             yield item
-    
+
     #get structure entry from task store and attach migration graph dict to item
-    def unary_function(self, item):
+    def unary_function(self, item: dict) -> dict:
         new_item = dict(item)
         task_documents = [TaskDocument.parse_obj(td) for td in new_item["task_docs"]]
         entries = [task_doc.structure_entry for task_doc in task_documents]
         #get entry is working_ion is new. limit MPRester calls
         if new_item["working_ion"] not in self.wi_entries.keys():
             self.wi_entries[new_item["working_ion"]] = min(compatibility.process_entries(
-                mpr.get_entries_in_chemsys([new_item["working_ion"]])), 
+                mpr.get_entries_in_chemsys([new_item["working_ion"]])),
                                                            key = lambda x : x.energy_per_atom)
-        mg = get_migration_graph(compatibility.process_entries(entries), 
-                                 self.wi_entries[new_item["working_ion"]], 
+        mg = get_migration_graph(compatibility.process_entries(entries),
+                                 self.wi_entries[new_item["working_ion"]],
                                  ltol=self.ltol, stol=self.stol, angle_tol=self.angle_tol)
         new_item["mg"] = mg
         return new_item
-    
-    def get_migration_graph(cses, wi_entry, max_distance_min=0.1, max_distance_max=10, ltol=0.4, stol=0.6, angle_tol=15) -> dict:
+
+    def get_migration_graph(cses: [ComputedEntry], wi_entry: [ComputedEntry], max_distance_min=0.1, max_distance_max=10, ltol=0.4, stol=0.6, angle_tol=15) -> dict:
         wi = wi_entry.composition.chemical_system
         try:
             mg_structures = MigrationGraph.get_structure_from_entries(entries=cses, migrating_ion_entry=wi_entry, ltol=ltol, stol=stol, angle_tol=angle_tol)
