d96df2b71fe4f00542454b62f0f1cfc07728d90c
alexnguyen0512 <dknguyenngoc@lbl.gov>
2022-06-07 10:43:31 -0700

    Update import statements and added docstring
    added docstring and necessary import statements. reformatted with black.
    also added test file for this builder

--- emmet-builders/emmet/builders/mobility_screening/migration_graph.py
+++ emmet-builders/emmet/builders/mobility_screening/migration_graph.py
@@ -1,3 +1,10 @@
+from emmet.core.vasp.task_valid import TaskDocument
+from pymatgen.analysis.diffusion.neb.full_path_mapper import MigrationGraph
+from pymatgen.entries.computed_entries import ComputedEntry, ComputedStructureEntry
+from maggma.stores import MongoStore
+from maggma.builders import MapBuilder
+
+
 class MigrationGraphBuilder(MapBuilder):
     def __init__(
         self,
@@ -8,7 +15,22 @@ class MigrationGraphBuilder(MapBuilder):
         stol: float = 0.6,
         angle_tol: float = 15,
         **kwargs
-    ) -> MapBuilder:
+    ):
+        """
+        Attach corresponding migration graphs to each electrode document by getting its computed structure
+            entry from an auxilarry task store.
+        Args:
+        electrodes (Store): Store of electrode documents that contains task_ids for its materials
+        tasks (Store): Store of task documents that contain the structure entry, to be paired with
+            each electrode docs based on material ids
+        migration_graphs (Store): Target store for electrode docs with attached migration graph
+        query (dict): dictionary to limit materials to be analyzed ---
+            only applied to the materials when we need to group structures
+            the phase diagram is still constructed with the entire set
+        ltol: fractional length tolerance for StructureMatcher
+        stol: site tolerance for StructureMatcher
+        angle_tol: angle tolerance for StructureMatcher
+        """
         self.electrodes = electrodes
         self.tasks = tasks
         self.ltol = ltol
@@ -16,51 +38,81 @@ class MigrationGraphBuilder(MapBuilder):
         self.angle_tol = angle_tol
         self.migration_graphs = migration_graphs
         self.wi_entries = {}
+        from pymatgen.ext.matproj import MPRester
+        from pymatgen.entries.compatibility import MaterialsProject2020Compatibility
 
+        self.mpr = MPRester()
+        self.compatibility = MaterialsProject2020Compatibility()
         super().__init__(source=electrodes, target=migration_graphs, **kwargs)
         self.sources.append(tasks)
 
     def get_items(self) -> dict:
-        for item in super(MigrationGraphBuilder,self).get_items():
-            tds = list(self.tasks.query({"task_id":{"$in":item["material_ids"]}}))
-            item.update({"task_docs":tds})
+        for item in super(MigrationGraphBuilder, self).get_items():
+            tds = list(self.tasks.query({"task_id": {"$in": item["material_ids"]}}))
+            item.update({"task_docs": tds})
             yield item
 
-    #get structure entry from task store and attach migration graph dict to item
+    # get structure entry from task store and attach migration graph dict to item
     def unary_function(self, item: dict) -> dict:
         new_item = dict(item)
         task_documents = [TaskDocument.parse_obj(td) for td in new_item["task_docs"]]
         entries = [task_doc.structure_entry for task_doc in task_documents]
-        #get entry is working_ion is new. limit MPRester calls
+        # get entry is working_ion is new. limit MPRester calls
         if new_item["working_ion"] not in self.wi_entries.keys():
-            self.wi_entries[new_item["working_ion"]] = min(compatibility.process_entries(
-                mpr.get_entries_in_chemsys([new_item["working_ion"]])),
-                                                           key = lambda x : x.energy_per_atom)
-        mg = self.get_migration_graph(compatibility.process_entries(entries),
-                                 self.wi_entries[new_item["working_ion"]],
-                                 ltol=self.ltol, stol=self.stol, angle_tol=self.angle_tol)
+            self.wi_entries[new_item["working_ion"]] = min(
+                self.compatibility.process_entries(
+                    self.mpr.get_entries_in_chemsys([new_item["working_ion"]])
+                ),
+                key=lambda x: x.energy_per_atom,
+            )
+        mg = self.get_migration_graph(
+            self.compatibility.process_entries(entries),
+            self.wi_entries[new_item["working_ion"]],
+            ltol=self.ltol,
+            stol=self.stol,
+            angle_tol=self.angle_tol,
+        )
         new_item["mg"] = mg
         return new_item
 
-    def get_migration_graph(self, cses: [ComputedEntry], wi_entry: [ComputedEntry], max_distance_min=0.1, max_distance_max=10, ltol=0.4, stol=0.6, angle_tol=15) -> dict:
+    def get_migration_graph(
+        self,
+        cses: [ComputedStructureEntry],
+        wi_entry: [ComputedEntry],
+        ltol,
+        stol,
+        angle_tol,
+        max_distance_min=0.1,
+        max_distance_max=10,
+    ) -> dict:
         wi = wi_entry.composition.chemical_system
         try:
-            mg_structures = MigrationGraph.get_structure_from_entries(entries=cses, migrating_ion_entry=wi_entry, ltol=ltol, stol=stol, angle_tol=angle_tol)
+            mg_structures = MigrationGraph.get_structure_from_entries(
+                entries=cses,
+                migrating_ion_entry=wi_entry,
+                ltol=ltol,
+                stol=stol,
+                angle_tol=angle_tol,
+            )
             mg = None
             max_distance = max_distance_min
 
             if len(mg_structures) > 0:
                 while mg == None and max_distance < max_distance_max:
-                    mg_trial = MigrationGraph.with_distance(structure=mg_structures[0], migrating_specie=wi, max_distance=max_distance)
+                    mg_trial = MigrationGraph.with_distance(
+                        structure=mg_structures[0],
+                        migrating_specie=wi,
+                        max_distance=max_distance,
+                    )
                     if mg_trial.m_graph.graph.number_of_edges() > 0:
-                        mg_trial.assign_cost_to_graph(['hop_distance'])
+                        mg_trial.assign_cost_to_graph(["hop_distance"])
                         paths = list(mg_trial.get_path())
                         if len(paths) == 0:
-                            max_distance = max_distance*1.1
+                            max_distance = max_distance * 1.1
                         else:
                             mg = mg_trial
                     else:
-                        max_distance = max_distance*1.1
+                        max_distance = max_distance * 1.1
                 return mg.as_dict()
         except:
             return None
--- emmet-builders/emmet/builders/mobility_screening/test.py
+++ emmet-builders/emmet/builders/mobility_screening/test.py
@@ -1,5 +1,29 @@
-def test_function:
-	return "string!"
+from migration_graph import MigrationGraphBuilder
+from maggma.stores.advanced_stores import MongograntStore
+from maggma.stores.compound_stores import ConcatStore
+from maggma.stores import MongoStore
 
-def test2_function:
-	return "string2!"
+acrt_store = MongograntStore("ro:mongodb07-ext.nersc.gov/fw_acr_mv","tasks",key="task_id")
+acrt_store.connect()
+
+jst_store = MongograntStore("ro:mongodb07-ext.nersc.gov/fw_acr_mv","js_tasks",key="task_id")
+jst_store.connect()
+
+tasks_store = ConcatStore(stores=[acrt_store,jst_store],key="task_id")
+tasks_store.connect()
+
+electrodes_store = MongograntStore("ro:mongodb07-ext.nersc.gov/fw_acr_mv","insertion_electrodes_vw2",key="battery_id")
+electrodes_store.connect()
+
+sandbox_store = MongoStore(database= "local_dev",collection_name= "sandbox", key="battery_id")
+sandbox_store.connect()
+
+battery_ids = ["js-47173_Mg","js-43005_Mg","js-18_Mg"]
+mapbuilder = MigrationGraphBuilder(
+    electrodes=electrodes_store,
+    tasks=tasks_store,
+    migration_graphs=sandbox_store,
+    query= {"battery_id":{"$in":battery_ids}}
+)
+
+mapbuilder.run()
