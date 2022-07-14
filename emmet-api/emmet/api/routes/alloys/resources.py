5b6c4100987f159795f1b847dbae8479f644832e
Matthew Horton <mkhorton@users.noreply.github.com>
2022-07-13 18:42:29 -0700

    Remove commented-out code

--- emmet-api/emmet/api/routes/alloys/resources.py
+++ emmet-api/emmet/api/routes/alloys/resources.py
@@ -4,7 +4,10 @@ from maggma.api.query_operator import PaginationQuery, SortQuery, SparseFieldsQu
 
 from emmet.core.alloys import AlloyPairDoc, AlloySystemDoc
 
-from mp_api.routes.alloys.query_operators import MaterialIDsSearchQuery, FormulaSearchQuery
+from mp_api.routes.alloys.query_operators import (
+    MaterialIDsSearchQuery,
+    FormulaSearchQuery,
+)
 
 
 def alloys_resource(alloys_store):
@@ -16,30 +19,10 @@ def alloys_resource(alloys_store):
             FormulaSearchQuery(),
             SortQuery(),
             PaginationQuery(),
-            SparseFieldsQuery(
-                 AlloyPairDoc, default_fields=["pair_id"],
-            ),
+            SparseFieldsQuery(AlloyPairDoc, default_fields=["pair_id"],),
         ],
         tags=["Alloys"],
         disable_validation=True,
     )
 
     return resource
-
-# TODO
-# def alloy_systems_resource(alloy_systems_store):
-#     resource = ReadOnlyResource(
-#         alloy_systems_store,
-#         AlloySystemDoc,
-#         query_operators=[
-#             SortQuery(),
-#             PaginationQuery(),
-#             SparseFieldsQuery(
-#                  AlloySystemDoc, default_fields=["alloy_id"],
-#             ),
-#         ],
-#         tags=["Alloys"],
-#         disable_validation=True,
-#     )
-#
-#     return resource
