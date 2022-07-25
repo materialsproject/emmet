bb9a5c0d54d653ea74789a68b4abd781f294c030
Matthew Horton <mkhorton@users.noreply.github.com>
2022-07-25 16:42:46 -0700

    Remove `matminer` as a dependency

--- emmet-core/setup.py
+++ emmet-core/setup.py
@@ -33,7 +33,6 @@ setup(
         "pybtex~=0.24",
         "typing-extensions>=3.7,<5.0",
         "robocrys>=0.2.7",
-        "matminer>=0.7.3",
         "pymatgen-analysis-diffusion>=2022.1.15",
         "pymatgen-analysis-alloys>=0.0.3"
     ],
