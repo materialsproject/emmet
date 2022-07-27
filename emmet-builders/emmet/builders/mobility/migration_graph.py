f31cb60283814e4c1ba6b3a3cbf93b17089874e4
acrutt <acrutt@lbl.gov>
2022-07-27 15:37:44 -0700

    Wrapped getting MigrationGraphDoc in try/except. Better handle error when no migration graph generated.

--- emmet-builders/emmet/builders/mobility/migration_graph.py
+++ emmet-builders/emmet/builders/mobility/migration_graph.py
@@ -32,29 +32,43 @@ class MigrationGraphBuilder(MapBuilder):
         wi_entry = ie.working_ion_entry
 
         # get migration graph structure
-        struct = MigrationGraph.get_structure_from_entries(entries, wi_entry)
-        if type(struct) == list:
-            if len(struct) > 1:
+        structs = MigrationGraph.get_structure_from_entries(entries, wi_entry)
+        if len(structs) == 0:
+            self.logger.error(
+                f"cannot generate migration graph: {len(structs)} possible options"
+            )
+            d = None
+        else:
+            if len(structs) > 1:
                 self.logger.warn(
-                    f"migration graph ambiguous: {len(struct)} possible options"
+                    f"migration graph ambiguous: {len(structs)} possible options"
                 )
-            struct = struct[0]
-
-        # get hop cutoff distance
-        d = get_hop_cutoff(
-            migration_graph_struct=struct,
-            mobile_specie=wi_entry.composition.chemical_system,
-            algorithm=self.algorithm,
-            min_hop_distance=self.min_hop_distance,
-            max_hop_distance=self.max_hop_distance,
-        )
+            # get hop cutoff distance
+            d = get_hop_cutoff(
+                migration_graph_struct=structs[0],
+                mobile_specie=wi_entry.composition.chemical_system,
+                algorithm=self.algorithm,
+                min_hop_distance=self.min_hop_distance,
+                max_hop_distance=self.max_hop_distance,
+            )
 
         # get migration graph
-        mg_doc = MigrationGraphDoc.from_entries_and_distance(
-            battery_id=item["battery_id"],
-            grouped_entries=entries,
-            working_ion_entry=wi_entry,
-            hop_cutoff=d,
-        )
+        try:
+            mg_doc = MigrationGraphDoc.from_entries_and_distance(
+                battery_id=item["battery_id"],
+                grouped_entries=entries,
+                working_ion_entry=wi_entry,
+                hop_cutoff=d,
+            )
+        except Exception as e:
+            mg_doc = {
+                "battery_id": item["battery_id"],
+                "entries_for_generation": entries,
+                "working_ion_entry": wi_entry,
+                "hop_cutoff": d,
+                "migration_graph": None,
+            }
+            self.logger.error(f"error getting MigrationGraphDoc: {e}")
+            return jsanitize(mg_doc)
 
         return jsanitize(mg_doc.dict())
