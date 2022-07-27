dcb83ec1a148be6409ddb3c09c6da16db6492e71
Matthew Horton <mkhorton@users.noreply.github.com>
2022-07-27 12:31:58 -0700

    Fix for incorrect collection name, require `db_suffix` to be set

--- emmet-api/app.py
+++ emmet-api/app.py
@@ -10,7 +10,7 @@ default_settings = MAPISettings()
 
 db_uri = os.environ.get("MPCONTRIBS_MONGO_HOST", None)
 db_version = default_settings.DB_VERSION
-db_suffix = os.environ.get("MAPI_DB_NAME_SUFFIX", db_version)
+db_suffix = os.environ["MAPI_DB_NAME_SUFFIX"]
 debug = default_settings.DEBUG
 
 # allow db_uri to be set with a different protocol scheme
@@ -199,7 +199,7 @@ if db_uri:
         uri=db_uri,
         database=f"mp_core_{db_suffix}",
         key="material_id",
-        collection_name="summary",
+        collection_name="alloy_pairs",
     )
 
     summary_store = MongoURIStore(
