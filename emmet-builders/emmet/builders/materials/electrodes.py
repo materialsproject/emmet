40d18081dd0888feeb903a668c4fe6bd0b89c7ae
acrutt <acrutt@lbl.gov>
2022-09-02 10:32:04 -0700

    Add chunk size to StructureGroupBuilder

--- emmet-builders/emmet/builders/materials/electrodes.py
+++ emmet-builders/emmet/builders/materials/electrodes.py
@@ -88,6 +88,7 @@ class StructureGroupBuilder(Builder):
             stol: float = default_build_settings.STOL,
             angle_tol: float = default_build_settings.ANGLE_TOL,
             check_newer: bool = True,
+            chunk_size: int = 1000,
             **kwargs,
     ):
         """
@@ -109,10 +110,11 @@ class StructureGroupBuilder(Builder):
         self.stol = stol
         self.angle_tol = angle_tol
         self.check_newer = check_newer
+        self.chunk_size = chunk_size
 
         self.query["deprecated"] = False  # Ensure only non-deprecated materials are chosen
 
-        super().__init__(sources=[materials], targets=[sgroups], **kwargs)
+        super().__init__(sources=[materials], targets=[sgroups], chunk_size=chunk_size, **kwargs)
 
     def prechunk(self, number_splits: int) -> Iterator[Dict]:  # pragma: no cover
         """
