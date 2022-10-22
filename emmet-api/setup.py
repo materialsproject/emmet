3e64b4f5214a0481ce7f44eeff9442ef53c4330e
Jason Munro <jmunro@lbl.gov>
2022-10-22 14:24:05 -0700

    Force setuptools_scm to search parent dirs (#568)

--- emmet-api/setup.py
+++ emmet-api/setup.py
@@ -8,11 +8,12 @@ if "+" in fallback_version:
 setup(
     name="emmet-api",
     use_scm_version={
-        "root": "..",
+        "root": ".",
         "relative_to": __file__,
-        "write_to": "emmet-api/emmet/api/_version.py",
+        "write_to": "emmet/api/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "fallback_version": fallback_version,
+        "search_parent_directories": True,
     },
     setup_requires=["setuptools_scm"],
     description="Emmet API Library",
