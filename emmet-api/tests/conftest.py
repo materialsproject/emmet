cbdee9491a53573c204eea22d154056e7647644c
Patrick Huck <phuck@lbl.gov>
2022-11-04 16:17:59 -0700

    try different folder structure for tests

--- emmet-api/tests/conftest.py
+++ emmet-api/tests/conftest.py
@@ -5,4 +5,4 @@ import pytest
 
 @pytest.fixture(scope="session")
 def test_dir():
-    return Path(__file__).parent.joinpath("test_files").resolve()
+    return Path(__file__).parent.parent.joinpath("test_files").resolve()
