1d3c2a841405249ff6e82929ad08e7156a651147
Jason Munro <jmunro@lbl.gov>
2022-11-08 13:05:35 -0800

    Change docs python version to 3.8 (#590)
    * Change docs python version to 3.8
    
    * Add doc deps
    
    * Linting
    
    * Add mkdocs to test deps
    
    * Linting
    
    * Add proper extra doc reps
    
    * Fix doc reps
    
    * More docs dep fixing
    
    * Unpin doc reqs
    
    * Unpin jinja
    
    * Fix rebase
    
    * Add mkdocstrings to

--- emmet-core/setup.py
+++ emmet-core/setup.py
@@ -11,10 +11,7 @@ setup(
     author_email="feedback@materialsproject.org",
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
-    package_data={
-        "emmet.core.vasp.calc_types": ["*.yaml"],
-        "emmet.core.subtrates": ["*.json"],
-    },
+    package_data={"emmet.core.vasp.calc_types": ["*.yaml"], "emmet.core.subtrates": ["*.json"]},
     include_package_data=True,
     install_requires=[
         "pymatgen>=2021.3,<2023.0",
