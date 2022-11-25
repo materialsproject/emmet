0d8bf6cfd69b97b225d5bf72d9332070bfefd09d
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2022-11-25 07:42:01 -0800

    Fixed redox emmet-core test

--- tests/emmet-core/molecules/test_redox.py
+++ tests/emmet-core/molecules/test_redox.py
@@ -2,31 +2,57 @@ import pytest
 
 from monty.serialization import loadfn
 
+from emmet.core.qchem.task import TaskDocument
 from emmet.core.qchem.molecule import MoleculeDoc
+from emmet.core.molecules.thermo import ThermoDoc
 from emmet.core.molecules.redox import RedoxDoc
 
 
-#TODO: REGENERATE AND FIX
-
+@pytest.fixture(scope="session")
+def base_mol(test_dir):
+    mol = loadfn((test_dir / "redox_doc" / "base_mol.json").as_posix())
+    mol_doc = MoleculeDoc(**mol)
+    return mol_doc
 
 @pytest.fixture(scope="session")
-def test_mols(test_dir):
-    mols = loadfn((test_dir / "builder_mols_set.json").as_posix())
-    mols = [MoleculeDoc(**x) for x in mols]
+def base_thermo(test_dir):
+    thermo = loadfn((test_dir / "redox_doc" / "thermo.json").as_posix())
+    thermo_doc = ThermoDoc(**thermo)
+    return thermo_doc
 
-    return mols
+@pytest.fixture(scope="session")
+def red_thermo(test_dir):
+    thermo = loadfn((test_dir / "redox_doc" / "red_thermo.json").as_posix())
+    thermo_doc = ThermoDoc(**thermo)
+    return thermo_doc
 
+@pytest.fixture(scope="session")
+def ox_thermo(test_dir):
+    thermo = loadfn((test_dir / "redox_doc" / "ox_thermo.json").as_posix())
+    thermo_doc = ThermoDoc(**thermo)
+    return thermo_doc
 
-def test_redox(test_mols):
-    entries = list()
-    for m in test_mols:
-        for e in m.entries:
-            entries.append(e)
+@pytest.fixture(scope="session")
+def ie_task(test_dir):
+    task = loadfn((test_dir / "redox_doc" / "ie_task.json").as_posix())
+    task_doc = TaskDocument(**task)
+    return task_doc
 
-    redox_docs = RedoxDoc.from_entries(entries)
+@pytest.fixture(scope="session")
+def ea_task(test_dir):
+    task = loadfn((test_dir / "redox_doc" / "ea_task.json").as_posix())
+    task_doc = TaskDocument(**task)
+    return task_doc
 
-    assert len(redox_docs) == 40
-    assert redox_docs[0].property_name == "redox"
+def test_redox(base_mol, base_thermo, red_thermo, ox_thermo, ie_task, ea_task):
+    redox_doc = RedoxDoc.from_docs(base_molecule_doc=base_mol,
+                                   base_thermo_doc=base_thermo,
+                                   red_doc=red_thermo,
+                                   ox_doc=ox_thermo,
+                                   ea_doc=ea_task,
+                                   ie_doc=ie_task)
 
-    assert redox_docs[1].electron_affinity == pytest.approx(-7.1246847304349075)
-    assert redox_docs[1].ionization_energy == pytest.approx(13.396578621769333)
+    assert redox_doc.electron_affinity == pytest.approx(-3.3024638499209686)
+    assert redox_doc.ionization_energy == pytest.approx(4.903294672107222)
+    assert redox_doc.oxidation_free_energy == pytest.approx(3.9880055108133092)
+    assert redox_doc.reduction_free_energy == pytest.approx(-4.237271030198826)
