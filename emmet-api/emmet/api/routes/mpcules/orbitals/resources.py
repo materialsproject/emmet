25596115e7923947c27ac72104828d1ff13c3126
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2023-03-06 16:41:21 -0800

    redox query operator and resource

---
+++ emmet-api/emmet/api/routes/mpcules/orbitals/resources.py
@@ -0,0 +1,51 @@
+from maggma.api.resource import ReadOnlyResource
+from emmet.core.molecules.orbitals import OrbitalDoc
+
+from maggma.api.query_operator import PaginationQuery, SortQuery, SparseFieldsQuery
+
+from emmet.api.routes.mpcules.orbitals.query_operators import (
+    NBOPopulationQuery,
+    NBOLonePairQuery,
+    NBOBondQuery,
+    NBOInteractionQuery,
+)
+from emmet.api.routes.mpcules.molecules.query_operators import (
+    MultiMPculeIDQuery,
+    ExactCalcMethodQuery,
+    FormulaQuery,
+    ChemsysQuery,
+    ElementsQuery,
+    ChargeSpinQuery
+)
+from emmet.api.routes.mpcules.utils import MethodQuery
+from emmet.api.core.settings import MAPISettings
+from emmet.api.core.global_header import GlobalHeaderProcessor
+
+
+def orbital_resource(orbital_store):
+    resource = ReadOnlyResource(
+        orbital_store,
+        OrbitalDoc,
+        query_operators=[
+            MultiMPculeIDQuery(),
+            ExactCalcMethodQuery(),
+            FormulaQuery(),
+            ChemsysQuery(),
+            ElementsQuery(),
+            ChargeSpinQuery(),
+            MethodQuery(),
+            NBOPopulationQuery(),
+            NBOLonePairQuery(),
+            NBOBondQuery(),
+            NBOInteractionQuery(),
+            SortQuery(),
+            PaginationQuery(),
+            SparseFieldsQuery(OrbitalDoc, default_fields=["molecule_id", "property_id", "method", "solvent", "last_updated"],),
+        ],
+        header_processor=GlobalHeaderProcessor(),
+        tags=["Orbitals"],
+        disable_validation=True,
+        timeout=MAPISettings().TIMEOUT,
+    )
+
+    return resource
---
+++ emmet-api/emmet/api/routes/mpcules/redox/resources.py
@@ -0,0 +1,45 @@
+from maggma.api.resource import ReadOnlyResource
+from emmet.core.molecules.redox import RedoxDoc
+
+from maggma.api.query_operator import PaginationQuery, SortQuery, SparseFieldsQuery
+
+from emmet.api.routes.mpcules.redox.query_operators import (
+    RedoxPotentialQuery
+)
+from emmet.api.routes.mpcules.molecules.query_operators import (
+    MultiMPculeIDQuery,
+    ExactCalcMethodQuery,
+    FormulaQuery,
+    ChemsysQuery,
+    ElementsQuery,
+    ChargeSpinQuery
+)
+from emmet.api.routes.mpcules.utils import MethodQuery
+from emmet.api.core.settings import MAPISettings
+from emmet.api.core.global_header import GlobalHeaderProcessor
+
+
+def redox_resource(redox_store):
+    resource = ReadOnlyResource(
+        redox_store,
+        RedoxDoc,
+        query_operators=[
+            MultiMPculeIDQuery(),
+            ExactCalcMethodQuery(),
+            FormulaQuery(),
+            ChemsysQuery(),
+            ElementsQuery(),
+            ChargeSpinQuery(),
+            MethodQuery(),
+            RedoxPotentialQuery(),
+            SortQuery(),
+            PaginationQuery(),
+            SparseFieldsQuery(RedoxDoc, default_fields=["molecule_id", "property_id", "solvent", "last_updated"],),
+        ],
+        header_processor=GlobalHeaderProcessor(),
+        tags=["Redox"],
+        disable_validation=True,
+        timeout=MAPISettings().TIMEOUT,
+    )
+
+    return resource
