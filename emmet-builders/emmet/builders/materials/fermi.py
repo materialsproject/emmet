c93cd902152a5d3a262803c13f0c3b3c3fefdc3b
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2023-05-25 12:05:21 -0700

    Feat: fermi builder, first version

---
+++ emmet-builders/emmet/builders/materials/fermi.py
@@ -0,0 +1,160 @@
+from math import ceil
+from typing import Dict, Iterator, Optional
+
+from maggma.core import Builder, Store
+from maggma.utils import grouper
+
+from emmet.core.fermi import FermiDoc
+from emmet.core.utils import jsanitize
+
+from pymatgen.core.structure import Structure
+from pymatgen.electronic_structure.bandstructure import BandStructure
+
+
+class FermiBuilder(Builder):
+    def __init__(
+        self,
+        electronic_structures: Store,
+        tasks: Store,
+        bandstructures: Store,
+        fermi_surfaces: Store,
+        query: Optional[Dict] = None,
+        **kwargs,
+    ):
+        self.electronic_structures = electronic_structures
+        self.tasks = tasks
+        self.bandstructures = bandstructures
+        self.fermi_surfaces = fermi_surfaces
+        self.query = query or {}
+        self.kwargs = kwargs
+
+        self.electronic_structures.key = "material_id"
+        self.tasks.key = "task_id"
+        self.bandstructures.key = "fs_id"
+        self.fermi_surfaces.key = "material_id"
+
+        super().__init__(
+            sources=[electronic_structures, tasks, bandstructures],
+            targets=[fermi_surfaces],
+            **kwargs,
+        )
+
+    def prechunk(self, number_splits: int) -> Iterator[Dict]:  # pragma: no cover
+        """
+        Prechunk method to perform chunking by the key field
+        """
+        q = dict(self.query)
+
+        keys = self.fermi_surfaces.newer_in(
+            self.electronic_structures, criteria=q, exhaustive=True
+        )
+
+        N = ceil(len(keys) / number_splits)
+        for split in grouper(keys, N):
+            yield {"query": {self.electronic_structures.key: {"$in": list(split)}}}
+
+    def get_items(self):
+        """
+        Gets all items to process
+
+        Returns:
+            Generator or list of relevant materials
+        """
+
+        self.logger.info("Fermi Builder Started")
+
+        q = dict(self.query)
+
+        q.update({"deprecated": False})
+
+        elec_structure_ids = self.electronic_structures.distinct(
+            self.electronic_structures.key, criteria=q
+        )
+
+        fermi_ids = self.fermi_surfaces.distinct(self.fermi_surfaces.key)
+
+        mats_set = set(
+            self.fermi_surfaces.newer_in(
+                target=self.electronic_structures, criteria=q, exhaustive=True
+            )
+        ) | (set(elec_structure_ids) - set(fermi_ids))
+
+        mats = [mat for mat in mats_set]
+
+        self.total = len(mats)
+
+        self.logger.info(f"Processing {self.total} items")
+
+        for mat in mats:
+            doc = self._get_processed_doc(mat)
+
+            if doc is not None:
+                yield doc
+            else:
+                pass
+
+    def process_item(self, item):
+        fermi_doc = FermiDoc.from_structure(
+            material_id=item["material_id"],
+            task_id=item["task_id"],
+            bandstructure=item["bandstructure"],
+            last_updated=item["task_updated"],
+            gridfs_id=item["gridfs_id"],
+            state=item["state"],
+        )
+
+        doc = jsanitize(fermi_doc.dict(), allow_bson=True)
+
+        return doc
+
+    def update_targets(self, items):
+        """
+        Inserts the new fermi surface docs into the fermi_surface collection
+        """
+        docs = list(filter(None, items))
+
+        if len(docs) > 0:
+            self.logger.info(f"Found {len(docs)} fermi_surface docs to update")
+            self.fermi_surfaces.update(docs)
+        else:
+            self.logger.info("No items to update")
+
+    def _get_processed_doc(self, mat):
+        mat_doc = self.electronic_structures.query_one(
+            criteria={self.electronic_structures.key: mat},
+            properties=[self.electronic_structures.key, "task_id"],
+        )
+
+        task_id = mat_doc["task_id"]
+
+        task_query = self.tasks.query_one(
+            criteria={"task_id": task_id},
+            properties=["output", "state", "last_updated"],
+        )
+
+        task_updated = task_query["last_updated"]
+
+        bs_query = self.bandstructures.query_one(
+            criteria={"task_id": task_id}, properties=["fs_id", "data", "task_id"]
+        )
+
+        if bs_query:
+            bandstructure = BandStructure.from_dict(bs_query["data"])
+            if not bandstructure.structure:
+                bandstructure.structure = Structure.from_dict(
+                    task_query["output"]["structure"]
+                )
+        else:
+            return None
+
+        mat_doc.update(
+            {
+                "task_updated": task_updated,
+                "bandstructure": bandstructure,
+                "gridfs_id": bs_query["fs_id"],
+                "state": task_query["state"],
+                self.electronic_structures.key: mat_doc[self.electronic_structures.key],
+            }
+        )
+
+        return mat_doc
--- emmet-core/emmet/core/fermi.py
+++ emmet-core/emmet/core/fermi.py
@@ -1,7 +1,18 @@
+# import sys
+# import time
+
 from typing import List
 from datetime import datetime
 from monty.json import MontyDecoder
-from pydantic import BaseModel, Field, validator
+from pydantic import Field, validator, BaseModel
+
+from pymatgen.electronic_structure.bandstructure import BandStructure
+
+from emmet.core.mpid import MPID
+
+from ifermi.surface import FermiSurface
+from ifermi.interpolate import FourierInterpolator
+from ifermi.kpoints import kpoints_from_bandstructure
 
 
 class FermiDoc(BaseModel):
@@ -9,7 +20,25 @@ class FermiDoc(BaseModel):
     Fermi surfaces.
     """
 
-    fermi_surfaces: List[dict] = Field(
+    material_id: MPID = Field(
+        ...,
+        description="The Materials Project ID of the material, used as a universal reference across property documents."
+        "This comes in the form: mp-******.",
+    )
+
+    task_id: str = Field(
+        None,
+        description="The source calculation (task) ID that corresponds to the calculation that generated\
+            the band structure(s) that were used to generate these Fermi Surfaces."
+        "This has the same form as a Materials Project ID.",
+    )
+
+    last_updated: datetime = Field(
+        None,
+        description="Timestamp for the most recent calculation for this fermi surface document.",
+    )
+
+    fermi_surfaces: list[dict] = Field(
         None,
         description="List of IFermi FermiSurface objects.",
     )
@@ -20,15 +49,93 @@ class FermiDoc(BaseModel):
             Is either CBM or VBM for semiconductors, or fermi_surface for metals.",
     )
 
-    task_id: str = Field(
-        None,
-        description="The Materials Project ID of the material. This comes in the form: mp-******.",
-    )
+    gridfs_id: str = Field(None, description="")
 
-    last_updated: datetime = Field(
-        None,
-        description="Timestamp for the most recent calculation for this fermi surface document.",
-    )
+    state: str = Field(None, description="")
+
+    @classmethod
+    def from_structure(
+        cls,
+        material_id: MPID,
+        task_id: str,
+        bandstructure: BandStructure,
+        last_updated: datetime,
+        gridfs_id: str,
+        state: str,
+        **kwargs,
+    ):
+        interpolator = FourierInterpolator(bandstructure)
+        interpolated_bs, velocites = interpolator.interpolate_bands(
+            return_velocities=True
+        )
+
+        interpolated_kpoints = kpoints_from_bandstructure(interpolated_bs)
+
+        mu_values = {}
+
+        if interpolated_bs.is_metal():
+            mu_values["fermi_surface"] = 0
+        else:
+            window = 0.1
+            efermi = interpolated_bs.efermi
+
+            vbm_mu = interpolated_bs.get_vbm()["energy"] - efermi - window
+            cbm_mu = interpolated_bs.get_cbm()["energy"] - efermi + window
+
+            mu_values["VBM"] = vbm_mu
+            mu_values["CBM"] = cbm_mu
+
+        fermi_surfaces = []
+        surface_types = []
+
+        for key, mu in mu_values.items():
+            # temp_time0 = time.time()
+            temp_fs = FermiSurface.from_band_structure(
+                band_structure=interpolated_bs,
+                mu=mu,
+                property_data=velocites,
+                property_kpoints=interpolated_kpoints,
+                calculate_dimensionality=True,
+                wigner_seitz=True,
+            )
+            # temp_finish = time.time()
+
+            total_verts = 0
+            for spin, surfaces in temp_fs.isosurfaces.items():
+                for surface in surfaces:
+                    total_verts += len(surface.vertices)
+
+            desired_max_verts = 10000
+            decimation_factor = int(total_verts / desired_max_verts)
+
+            # smooth_time0 = time.time()
+            fs = FermiSurface.from_band_structure(
+                band_structure=interpolated_bs,
+                mu=mu,
+                property_data=velocites,
+                property_kpoints=interpolated_kpoints,
+                calculate_dimensionality=True,
+                wigner_seitz=True,
+                decimate_factor=decimation_factor,
+                # smooth=True,
+            )
+            # smooth_finish = time.time()
+
+            # print(f"\nTemp fs time is: {temp_finish - temp_time0}")
+            # print(f"Smooth fs time is: {smooth_finish - smooth_time0}")
+
+            fermi_surfaces.append(fs.as_dict())
+            surface_types.append(key)
+
+        return FermiDoc(
+            material_id=material_id,
+            task_id=task_id,
+            last_updated=last_updated,
+            fermi_surfaces=fermi_surfaces,
+            surface_types=surface_types,
+            gridfs_id=gridfs_id,
+            state=state,
+        )
 
     # Make sure that the datetime field is properly formatted
     @validator("last_updated", pre=True)
