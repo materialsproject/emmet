2aa6a1b4354808699e505b2eddb47da426879594
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2023-06-08 09:02:43 -0700

    Remove temp fermi surface, decimation factor, and smooth calcs to simplify builder testing

--- emmet-core/emmet/core/fermi.py
+++ emmet-core/emmet/core/fermi.py
@@ -1,11 +1,12 @@
-from typing import List
+from typing import List, Dict, Union
 from datetime import datetime
+
 from monty.json import MontyDecoder
 from pydantic import Field, validator, BaseModel
 
-from pymatgen.electronic_structure.bandstructure import BandStructure
-
 from emmet.core.mpid import MPID
+
+from pymatgen.electronic_structure.bandstructure import BandStructure
 
 from ifermi.surface import FermiSurface
 from ifermi.interpolate import FourierInterpolator
@@ -38,20 +39,16 @@ class FermiDoc(BaseModel):
         description="Timestamp for the most recent calculation for this fermi surface document.",
     )
 
-    fermi_surfaces: list[dict] = Field(
-        None,
+    fermi_surfaces: List[dict] = Field(
+        ...,
         description="List of IFermi FermiSurface objects.",
     )
 
     surface_types: List[str] = Field(
-        None,
+        ...,
         description="Type of each fermi surface in the fermi_surfaces list.\
             Is either CBM or VBM for semiconductors, or fermi_surface for metals.",
     )
-
-    fs_id: str = Field(None, description="")
-
-    state: str = Field(None, description="")
 
     @classmethod
     @timeout(10)  # Only for testing on low memory machines
@@ -61,8 +58,6 @@ class FermiDoc(BaseModel):
         task_id: str,
         bandstructure: BandStructure,
         last_updated: datetime,
-        fs_id: str,
-        state: str,
         **kwargs,
     ):
         interpolator = FourierInterpolator(bandstructure)
@@ -90,26 +85,6 @@ class FermiDoc(BaseModel):
         surface_types = []
 
         for key, mu in mu_values.items():
-            # temp_time0 = time.time()
-            temp_fs = FermiSurface.from_band_structure(
-                band_structure=interpolated_bs,
-                mu=mu,
-                property_data=velocites,
-                property_kpoints=interpolated_kpoints,
-                calculate_dimensionality=True,
-                wigner_seitz=True,
-            )
-            # temp_finish = time.time()
-
-            total_verts = 0
-            for spin, surfaces in temp_fs.isosurfaces.items():
-                for surface in surfaces:
-                    total_verts += len(surface.vertices)
-
-            desired_max_verts = 10000
-            decimation_factor = int(total_verts / desired_max_verts)
-
-            # smooth_time0 = time.time()
             fs = FermiSurface.from_band_structure(
                 band_structure=interpolated_bs,
                 mu=mu,
@@ -117,13 +92,7 @@ class FermiDoc(BaseModel):
                 property_kpoints=interpolated_kpoints,
                 calculate_dimensionality=True,
                 wigner_seitz=True,
-                decimate_factor=decimation_factor,
-                # smooth=True,
             )
-            # smooth_finish = time.time()
-
-            # print(f"\nTemp fs time is: {temp_finish - temp_time0}")
-            # print(f"Smooth fs time is: {smooth_finish - smooth_time0}")
 
             fermi_surfaces.append(fs.as_dict())
             surface_types.append(key)
@@ -131,11 +100,9 @@ class FermiDoc(BaseModel):
         return FermiDoc(
             material_id=material_id,
             task_id=task_id,
-            last_updated=last_updated,
             fermi_surfaces=fermi_surfaces,
             surface_types=surface_types,
-            fs_id=fs_id,
-            state=state,
+            last_updated=last_updated,
         )
 
     # Make sure that the datetime field is properly formatted
