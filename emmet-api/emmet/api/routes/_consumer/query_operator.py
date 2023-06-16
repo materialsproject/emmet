42e0973abd2aeaeab0a3aaef074cb7ba81868521
yang-ruoxi <roxie@Live.cn>
2023-06-16 16:31:23 -0700

    add update method

--- emmet-api/emmet/api/routes/_consumer/query_operator.py
+++ emmet-api/emmet/api/routes/_consumer/query_operator.py
@@ -30,6 +30,31 @@ class UserSettingsPostQuery(QueryOperator):
 
         return d
 
+class UserSettingsPatchQuery(QueryOperator):
+    """Query operators to provide user settings information to potch"""
+
+    def query(
+        self,
+        consumer_id: str = Query(
+            ...,
+            title="Consumer ID",
+        ),
+        settings: Dict = Body(
+            ...,
+            title="User settings",
+        ),
+    ) -> STORE_PARAMS:
+        crit = {"consumer_id": consumer_id, "settings": settings}
+
+        return {"criteria": crit}
+
+    def post_process(self, docs, query):
+        cid = query["criteria"]["consumer_id"]
+        settings = query["criteria"]["settings"]
+
+        d = [{"consumer_id": cid, "settings": settings}]
+
+        return d
 
 class UserSettingsGetQuery(QueryOperator):
     """Query operators to provide user settings information"""
