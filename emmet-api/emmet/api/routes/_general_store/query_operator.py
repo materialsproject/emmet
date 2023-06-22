c4fd763ee9b095b2fa4a1e9729ef5c30bd3fcbed
yang-ruoxi <roxie@Live.cn>
2023-06-22 10:44:03 -0700

    remove general store patch query

--- emmet-api/emmet/api/routes/_general_store/query_operator.py
+++ emmet-api/emmet/api/routes/_general_store/query_operator.py
@@ -36,27 +36,3 @@ class GeneralStoreGetQuery(QueryOperator):
         crit = {"kind": kind}
 
         return {"criteria": crit}
-
-class GeneralStorePatchQuery(QueryOperator):
-    """Query operators to provide general store information to patch"""
-
-    def query(
-        self,
-        kind: str = Query(..., title="Data type"),
-        markdown: str = Query(None, title="Markdown data"),
-        meta: Dict = Body(None, title="Metadata"),
-    ) -> STORE_PARAMS:
-        crit = {"kind": kind, "markdown": markdown, "meta": meta}
-
-        return {"criteria": crit}
-
-    def post_process(self, docs, query):
-        d = [
-            {
-                "kind": query["criteria"]["kind"],
-                "markdown": query["criteria"].get("markdown", None),
-                "meta": query["criteria"].get("meta", None),
-            }
-        ]
-
-        return d
