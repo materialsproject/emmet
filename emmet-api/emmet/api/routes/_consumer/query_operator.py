1058647b23ea9d47d20213dd1eb6f1197d3f5ff4
yang-ruoxi <roxie@Live.cn>
2023-06-22 10:34:14 -0700

    update patch query operators

--- emmet-api/emmet/api/routes/_consumer/query_operator.py
+++ emmet-api/emmet/api/routes/_consumer/query_operator.py
@@ -1,4 +1,5 @@
-from typing import Dict
+from datetime import datetime
+from typing import Dict, Optional
 from fastapi import Query, Body
 from maggma.api.utils import STORE_PARAMS
 from maggma.api.query_operator import QueryOperator
@@ -39,22 +40,21 @@ class UserSettingsPatchQuery(QueryOperator):
             ...,
             title="Consumer ID",
         ),
-        settings: Dict = Body(
-            ...,
-            title="User settings",
-        ),
+        fields_to_update: Optional[Dict] = Body(
+            None,
+            title="Field name and value to update in user settings"
+        )
     ) -> STORE_PARAMS:
-        crit = {"consumer_id": consumer_id, "settings": settings}
 
-        return {"criteria": crit}
+        crit = {"consumer_id": consumer_id}
 
-    def post_process(self, docs, query):
-        cid = query["criteria"]["consumer_id"]
-        settings = query["criteria"]["settings"]
+        if fields_to_update and "settings.message_last_read" in fields_to_update:
+            # Parse the ISO-formatted timestamp string into a datetime object
+            time = datetime.fromisoformat(fields_to_update["settings.message_last_read"])
+            fields_to_update["settings.message_last_read"] = time
 
-        d = [{"consumer_id": cid, "settings": settings}]
+        return dict(criteria=crit, update=fields_to_update) if fields_to_update else dict(criteria=crit)
 
-        return d
 
 class UserSettingsGetQuery(QueryOperator):
     """Query operators to provide user settings information"""
