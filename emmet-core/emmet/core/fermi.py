fe4bb4c98528e8db82c535edfbf981e2c1da50bc
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2023-06-26 14:00:34 -0700

    Remove timeout decorator and import from fermidoc

--- emmet-core/emmet/core/fermi.py
+++ emmet-core/emmet/core/fermi.py
@@ -12,9 +12,6 @@ from ifermi.surface import FermiSurface
 from ifermi.interpolate import FourierInterpolator
 from ifermi.kpoints import kpoints_from_bandstructure
 
-# Used only for testing on machines with low memory
-from wrapt_timeout_decorator import *
-
 
 class FermiDoc(BaseModel):
     """
@@ -51,7 +48,6 @@ class FermiDoc(BaseModel):
     )
 
     @classmethod
-    # @timeout(10)  # Only for testing on low memory machines
     def from_structure(
         cls,
         material_id: MPID,
