5cdb2f8a55f54cbc4e0e3e12b66a236b36b5df33
Jason Munro <jmunro@lbl.gov>
2023-07-20 12:53:49 -0700

    Pull initial keys from thermo in alloys (#787)
    * Pull initial keys from thermo in alloys
    
    * Linting
    
    * Bump pymatgen
    
    * Linting

--- emmet-builders/emmet/builders/materials/alloys.py
+++ emmet-builders/emmet/builders/materials/alloys.py
@@ -78,26 +78,31 @@ class AlloyPairBuilder(Builder):
             # if af != "AB":
             #     continue
 
-            docs = self.materials.query(
+            thermo_docs = self.thermo.query(
                 criteria={
                     "formula_anonymous": af,
                     "deprecated": False,
-                },  # , "material_id": {"$in": ["mp-804", "mp-661"]}},
-                properties=["structure", "material_id", "symmetry.number"],
-            )
-            docs = {d["material_id"]: d for d in docs}
-
-            mpids = list(docs.keys())
-
-            thermo_docs = self.thermo.query(
-                {"material_id": {"$in": mpids}, "thermo_type": self.thermo_type},
+                    "thermo_type": self.thermo_type,
+                },
                 properties=[
                     "material_id",
                     "energy_above_hull",
                     "formation_energy_per_atom",
                 ],
             )
+
             thermo_docs = {d["material_id"]: d for d in thermo_docs}
+
+            mpids = list(thermo_docs.keys())
+
+            docs = self.materials.query(
+                criteria={
+                    "material_id": {"$in": mpids},
+                    "deprecated": False,
+                },  # , "material_id": {"$in": ["mp-804", "mp-661"]}},
+                properties=["structure", "material_id", "symmetry.number"],
+            )
+            docs = {d["material_id"]: d for d in docs}
 
             electronic_structure_docs = self.electronic_structure.query(
                 {"material_id": {"$in": mpids}},
@@ -119,7 +124,7 @@ class AlloyPairBuilder(Builder):
             )
             oxi_states_docs = {d["material_id"]: d for d in oxi_states_docs}
 
-            for material_id in thermo_docs:
+            for material_id in mpids:
                 d = docs[material_id]
 
                 d["structure"] = Structure.from_dict(d["structure"])
