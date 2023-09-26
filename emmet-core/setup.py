86da59b97cbce1ba204baba1558122fad4f31020
Jason Munro <jason.munro@gmail.com>
2023-09-26 15:46:43 -0700

    Unpin setuptools_scm

--- emmet-core/setup.py
+++ emmet-core/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 setup(
     name="emmet-core",
     use_scm_version={"root": "..", "relative_to": __file__},
-    setup_requires=["setuptools_scm>=6,<8"],
+    setup_requires=["setuptools_scm"],
     description="Core Emmet Library",
     author="The Materials Project",
     author_email="feedback@materialsproject.org",
