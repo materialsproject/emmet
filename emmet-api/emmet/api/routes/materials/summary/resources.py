c6bac74ae420ae18968177012d4cba1c5c50d76e
munrojm <jmunro@lbl.gov>
2023-10-09 09:52:42 -0700

    Linting

--- emmet-api/emmet/api/routes/materials/summary/resources.py
+++ emmet-api/emmet/api/routes/materials/summary/resources.py
@@ -13,7 +13,7 @@ from emmet.api.routes.materials.materials.query_operators import (
     FormulaQuery,
     ChemsysQuery,
     SymmetryQuery,
-    LicenseQuery
+    LicenseQuery,
 )
 from emmet.api.routes.materials.oxidation_states.query_operators import (
     PossibleOxiStateQuery,
