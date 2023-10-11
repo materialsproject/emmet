215a800aa3afeb514f88b7e47220539ed7c9081a
Patrick Huck <phuck@lbl.gov>
2023-10-11 16:14:14 -0700

    add asgi-logger

--- emmet-api/app.py
+++ emmet-api/app.py
@@ -1,6 +1,8 @@
 import time
 start = time.perf_counter()
 import logging
+logging.getLogger("uvicorn.access").handlers = []
+from asgi_logger import AccessLoggerMiddleware
 
 from material_resources import resources as materials_resources
 from molecule_resources import resources as molecule_resources
@@ -21,5 +23,9 @@ api = MAPI(
     tags_meta=tags_meta,
 )
 app = api.app
+app.add_middleware(
+    AccessLoggerMiddleware,
+    format='%(h)s %(t)s %(m)s %(U)s?%(q)s %(H)s %(s)s %(b)s "%(f)s" "%(a)s" %(D)s %(p)s %({x-consumer-id}i)s'
+)
 delta = time.perf_counter() - start
 logger.warning(f"Startup took {delta:.1f}s")
