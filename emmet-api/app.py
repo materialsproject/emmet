f6d055ec4ad00255ceb0d7506826280183225e8e
Patrick Huck <phuck@lbl.gov>
2023-10-11 14:36:47 -0700

    print start up time

--- emmet-api/app.py
+++ emmet-api/app.py
@@ -1,3 +1,7 @@
+import time
+start = time.perf_counter()
+import logging
+
 from material_resources import resources as materials_resources
 from molecule_resources import resources as molecule_resources
 
@@ -5,6 +9,7 @@ from emmet.api.core.api import MAPI
 from emmet.api.core.documentation import description, tags_meta
 from emmet.api.core.settings import MAPISettings
 
+logger = logging.getLogger(__name__)
 default_settings = MAPISettings()
 
 resources = {**materials_resources, **molecule_resources}
@@ -16,3 +21,5 @@ api = MAPI(
     tags_meta=tags_meta,
 )
 app = api.app
+delta = time.perf_counter() - start
+logger.warning(f"Startup took {delta:.1f}s")
