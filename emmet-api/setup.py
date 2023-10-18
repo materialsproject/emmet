766a60a79014f8d816638e1bbd8b3f5270abf3f7
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-10-18 09:19:45 -0700

    same for emmet-(api|builders|cli)

--- emmet-api/setup.py
+++ emmet-api/setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from setuptools import find_namespace_packages, setup
 
 setup(
@@ -8,6 +7,8 @@ setup(
     description="Emmet API Library",
     author="The Materials Project",
     author_email="feedback@materialsproject.org",
+    long_description=open("../README.md").read(),  # noqa: SIM115
+    long_description_content_type="text/markdown",
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
     install_requires=[
