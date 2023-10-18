02d6d96064a014708778757eb2cee6604faee502
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-10-18 10:18:50 -0700

    Fix PyPI missing project description (#866)
    * fix PyPI missing long_description
    
    * same for emmet-(api|builders|cli)

--- emmet-builders/setup.py
+++ emmet-builders/setup.py
@@ -7,6 +7,8 @@ setup(
     description="Builders for the Emmet Library",
     author="The Materials Project",
     author_email="feedback@materialsproject.org",
+    long_description=open("../README.md").read(),  # noqa: SIM115
+    long_description_content_type="text/markdown",
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
     install_requires=[
--- emmet-core/setup.py
+++ emmet-core/setup.py
@@ -7,6 +7,8 @@ setup(
     description="Core Emmet Library",
     author="The Materials Project",
     author_email="feedback@materialsproject.org",
+    long_description=open("../README.md").read(),  # noqa: SIM115
+    long_description_content_type="text/markdown",
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
     package_data={
