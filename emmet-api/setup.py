ad5c6978377fc80a39545a0ff90506446c69be95
Patrick Huck <phuck@lbl.gov>
2023-10-23 14:01:49 -0700

    api: revert long description from README

--- emmet-api/setup.py
+++ emmet-api/setup.py
@@ -7,7 +7,7 @@ setup(
     description="Emmet API Library",
     author="The Materials Project",
     author_email="feedback@materialsproject.org",
-    long_description=open("../README.md").read(),  # noqa: SIM115
+    long_description="see README",  # noqa: SIM115
     long_description_content_type="text/markdown",
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
