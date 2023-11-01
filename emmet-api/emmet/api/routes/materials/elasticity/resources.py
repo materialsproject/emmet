1e65eebdda425ab406b021bd609b2c90ea26487a
Jason Munro <jmunro@lbl.gov>
2023-11-01 11:01:08 -0700

    Add material ID query to elasticity (#881)
    * Fix elasticity query_ops
    
    * Remove elasticity sub key in summary
    
    * Fix elasticity tests
    
    * Linting
    
    * Fix elasticity import
    
    * Fix nested elasticity data in summary
    
    * More nested summary fixes
    
    * Linting
    
    * Add multi material_id query to elasticity

--- emmet-api/emmet/api/routes/materials/elasticity/resources.py
+++ emmet-api/emmet/api/routes/materials/elasticity/resources.py
@@ -2,6 +2,7 @@ from maggma.api.resource import ReadOnlyResource
 from emmet.core.elasticity import ElasticityDoc
 from emmet.api.core.global_header import GlobalHeaderProcessor
 from maggma.api.query_operator import PaginationQuery, SortQuery, SparseFieldsQuery
+from emmet.api.routes.materials.materials.query_operators import MultiMaterialIDQuery
 from emmet.api.routes.materials.elasticity.query_operators import (
     ElasticityChemsysQuery,
     BulkModulusQuery,
@@ -17,6 +18,7 @@ def elasticity_resource(elasticity_store):
         elasticity_store,
         ElasticityDoc,
         query_operators=[
+            MultiMaterialIDQuery(),
             ElasticityChemsysQuery(),
             BulkModulusQuery(),
             ShearModulusQuery(),
