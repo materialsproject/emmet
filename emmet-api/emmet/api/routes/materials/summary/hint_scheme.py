fbe4fddee6c5816b942e26aa54445715a74ac989
Jason <jmunro@lbl.gov>
2023-11-16 17:36:50 -0800

    Fix has props and composition in hint

--- emmet-api/emmet/api/routes/materials/summary/hint_scheme.py
+++ emmet-api/emmet/api/routes/materials/summary/hint_scheme.py
@@ -11,20 +11,25 @@ class SummaryHintScheme(HintScheme):
         hints["agg_hint"] = hints["count_hint"]
 
         if list(query.get("criteria").keys()) != ["deprecated", "builder_meta.license"]:
-            for param in query["criteria"]:
-                if param not in [
-                    "deprecated",
-                    "builder_meta.license",
-                ] and ("has_props" not in param and "composition_reduced" not in param):
-                    hints["count_hint"] = {
-                        "deprecated": 1,
-                        "builder_meta.license": 1,
-                        "formula_pretty": 1,
-                        "material_id": 1,
-                        param: 1,
-                    }
-                    hints["agg_hint"] = hints["count_hint"]
-                    break
+            pure_params = [param.split(".")[0] for param in query["criteria"]]
+
+            if "has_props" in pure_params:
+                hints["count_hint"] = {"has_props.$**": 1}
+            elif "composition_reduced" in pure_params:
+                hints["count_hint"] = {"composition_reduced.$**": 1}
+            else:
+                for param in pure_params:
+                    if param not in ["deprecated", "builder_meta.license"]:
+                        hints["count_hint"] = {
+                            "deprecated": 1,
+                            "builder_meta.license": 1,
+                            "formula_pretty": 1,
+                            "material_id": 1,
+                            param: 1,
+                        }
+                        break
+
+            hints["agg_hint"] = hints["count_hint"]
 
         elif query.get("sort", {}):
             for param in query["sort"]:
