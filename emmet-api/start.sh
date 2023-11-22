dd4eab769a97a6abdc10e46d7636fc44a38da2bd
Patrick Huck <phuck@lbl.gov>
2023-11-22 12:01:55 -0800

    increase timeout to finish startup w less cpu

--- emmet-api/start.sh
+++ emmet-api/start.sh
@@ -7,5 +7,5 @@ exec ddtrace-run gunicorn --statsd-host $DD_AGENT_HOST:8125 \
     -b 0.0.0.0:$PORT -k uvicorn.workers.UvicornWorker -w $NUM_WORKERS \
     --access-logfile - --error-logfile - $RELOAD \
     --max-requests $MAX_REQUESTS --max-requests-jitter $MAX_REQUESTS_JITTER \
-    --timeout 60 \
+    --timeout 120 \
     app:app
