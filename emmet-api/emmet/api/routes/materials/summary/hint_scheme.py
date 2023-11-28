130e0ca3384a950cf6c63fd031fb2bee524d2db8
munrojm <jmunro@lbl.gov>
2023-11-27 17:54:57 -0800

    Fix summary hint scheme for excluding elements

--- emmet-api/emmet/api/routes/materials/summary/hint_scheme.py
+++ emmet-api/emmet/api/routes/materials/summary/hint_scheme.py
@@ -11,15 +11,23 @@ class SummaryHintScheme(HintScheme):
         hints["agg_hint"] = hints["count_hint"]
 
         if list(query.get("criteria").keys()) != ["deprecated", "builder_meta.license"]:
-            pure_params = [param.split(".")[0] for param in query["criteria"]]
+
+            pure_params = []
+            excluded_elements = False
+            for param, val in query["criteria"].items():
+                pure_param = param.split(".")[0]
+                pure_params.append(pure_param)
+                if pure_param == "composition_reduced" and val == {"$exists": False}:
+                    excluded_elements = True
+
 
             if "has_props" in pure_params:
                 hints["count_hint"] = {"has_props.$**": 1}
-            elif "composition_reduced" in pure_params:
+            elif "composition_reduced" in pure_params and not excluded_elements:
                 hints["count_hint"] = {"composition_reduced.$**": 1}
             else:
                 for param in query["criteria"]:
-                    if param not in ["deprecated", "builder_meta.license"]:
+                    if param not in ["deprecated", "builder_meta.license"] and "composition_reduced" not in param:
                         hints["count_hint"] = {
                             "deprecated": 1,
                             "builder_meta.license": 1,
