f27132d1020ea70816621cdadf5ce139882ca376
Evan Walter Clark Spotte-Smith, PhD <espottesmith@gmail.com>
2024-01-18 08:19:56 -0800

    Just reordering

--- emmet-builders/emmet/builders/molecules/summary.py
+++ emmet-builders/emmet/builders/molecules/summary.py
@@ -329,11 +329,11 @@ class SummaryBuilder(Builder):
                 "bonding": _group_docs(
                     list(self.bonds.query({"molecule_id": mol_id})), True
                 ),
-                "multipole_moments": _group_docs(
-                    list(self.multipoles.query({"molecule_id": mol_id})), False
-                ),
                 "metal_binding": _group_docs(
                     list(self.metal_binding.query({"molecule_id": mol_id})), True
+                ),
+                "multipole_moments": _group_docs(
+                    list(self.multipoles.query({"molecule_id": mol_id})), False
                 ),
                 "orbitals": _group_docs(
                     list(self.orbitals.query({"molecule_id": mol_id})), False
