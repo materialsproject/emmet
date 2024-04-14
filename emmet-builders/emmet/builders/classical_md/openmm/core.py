ee7da72e337f96e7bc8cc078d4fbf717618c30b5
orioncohen <orion@lbl.gov>
2024-04-14 00:04:02 -0700

    Finish process_items and rename builder to ElectrolyteBuilder

--- emmet-builders/emmet/builders/classical_md/openmm/core.py
+++ emmet-builders/emmet/builders/classical_md/openmm/core.py
@@ -1,30 +1,37 @@
 import warnings
 from typing import Optional, List
-from tempfile import TemporaryDirectory
 from pathlib import Path
+import tempfile
 
 import numpy as np
 
 from maggma.core import Builder, Store
 from openff.interchange import Interchange
 from emmet.core.classical_md.openmm import OpenMMTaskDocument
-from emmet.builders.classical_md.utils import create_universe
+from emmet.builders.classical_md.utils import (
+    create_universe,
+    create_solute,
+    identify_solute,
+    identify_networking_solvents,
+)
+from emmet.core.classical_md.solvation import SolvationDoc
 
 
-class OpenMMBuilder(Builder):
+class ElectrolyteBuilder(Builder):
     def __init__(
         self,
         md_docs: Store,
         blobs: Store,
         solute: Store,
         query: Optional[dict] = None,
+        solvation_fallback_radius: float = 3,
         chunk_size: int = 10,
     ):
         self.md_docs = md_docs
         self.blobs = blobs
         self.solute = solute
-
         self.query = query or {}
+        self.solvation_fallback_radius = solvation_fallback_radius
 
         if self.md_docs.key != "uuid":
             warnings.warn(
@@ -59,31 +66,39 @@ class OpenMMBuilder(Builder):
     #         yield {"query": {self.materials.key: {"$in": list(split)}}}
 
     def process_items(self, items):
+        solvation_docs = []
         for item in items:
             # create task_doc
             task_doc = OpenMMTaskDocument.parse_obj(item["output"])
+            calc = task_doc.calcs_reversed[0]
+
             # create interchange
             interchange_str = task_doc.interchange.decode("utf-8")
             interchange = Interchange.parse_raw(interchange_str)
-            # create temporary directory
-            with TemporaryDirectory() as temp_dir:
-                temp_path = Path(temp_dir)
-                calc_output = task_doc.calcs_reversed[0].output
-                # write trajectory to file
-                with open(temp_path / calc_output.traj_file, "wb") as f:
-                    f.write(calc_output.traj_blob)
-                create_universe(
-                    interchange,
-                    task_doc.molecule_specs,
-                    temp_path / calc_output.traj_file,
-                )
-                # identify_solute(u, task_doc)
-                # identify_networking_solvents(u, task_doc)
-                # TODO: fix hardcode
-                # create_solute(u, solute_name="O", networking_solvents=["O"])
-                # solute.run()
-                #
-                # doc = SolvationDoc.from_solute(solute)
+
+            # write the trajectory to a file
+            traj_file = tempfile.NamedTemporaryFile()
+            traj_path = Path(traj_file.name)
+            with open(traj_path, "wb") as f:
+                f.write(calc.output.traj_blob)
+            u = create_universe(
+                interchange,
+                task_doc.molecule_specs,
+                traj_path,
+                traj_format=calc.input.traj_file_type,
+            )
+
+            solute = create_solute(
+                u,
+                solute_name=identify_solute(u),
+                networking_solvents=identify_networking_solvents(u),
+                fallback_radius=self.solvation_fallback_radius,
+            )
+            solute.run()
+            solvation_docs.append(SolvationDoc.from_solute(solute))
+            traj_path.unlink()
+
+        return solvation_docs
 
     def get_items(self):
         hosts = self.md_docs.query(self.query, ["hosts"])
@@ -91,6 +106,7 @@ class OpenMMBuilder(Builder):
 
         job_groups = []
         for flow_id in flow_ids:
+            # get last item in hosts, which should be top level workflow
             mg_filter = {"$expr": {"$eq": [{"$arrayElemAt": ["$hosts", -1]}, flow_id]}}
             job_groups.append(list(self.md_docs.query(criteria=mg_filter)))
 
@@ -100,7 +116,6 @@ class OpenMMBuilder(Builder):
             # TODO: support branching flows
 
             # find the job with the most calcs in the flow, presumably the last
-            jobs = job_groups[0]
             len_calcs = [len(job["output"]["calcs_reversed"] or []) for job in jobs]
             last_job = jobs[np.argmax(len_calcs)]
 
@@ -111,6 +126,7 @@ class OpenMMBuilder(Builder):
 
             for calc in last_job["output"]["calcs_reversed"]:
                 traj_blob = calc["output"]["traj_blob"]
+
                 # inject trajectory blobs
                 if traj_blob:
                     traj_uuid = calc["output"]["traj_blob"]["blob_uuid"]
