3c927bee9b29b7201d7d5fc811e843556166ac1d
orioncohen <orion@lbl.gov>
2024-06-02 20:01:58 -0400

    Move pip installable requirements to emmet-core setup.py.

--- emmet-core/setup.py
+++ emmet-core/setup.py
@@ -41,6 +41,8 @@ setup(
             "robocrys>=0.2.8",
             "pymatgen-analysis-diffusion>=2023.8.15",
             "pymatgen-analysis-alloys>=0.0.3",
+            "solvation-analysis>=0.4.0",
+            "mdanalysis>=2.7.0",
         ],
         "ml": ["chgnet", "matgl"],
         "test": [
