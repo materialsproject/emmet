b9f9ca7a38afc96e710f0f24d412a71187ce2258
orioncohen <orion@lbl.gov>
2024-07-20 11:16:37 -0700

    Add transport analysis requirement.

--- emmet-builders/setup.py
+++ emmet-builders/setup.py
@@ -44,6 +44,7 @@ setup(
             "jinja2",
         ],
         "ml": ["emmet-core[ml]"],
+        "classical_md": ["transport-analysis>=0.1.0"],
     },
     python_requires=">=3.9",
     license="modified BSD",
