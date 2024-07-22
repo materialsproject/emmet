1dd395403f380d71ba6ec4d370d354a735ace5e5
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2024-07-22 10:18:09 -0700

    future annotations for type hint compatibility w/ py3.9

--- emmet-core/emmet/core/feff/task.py
+++ emmet-core/emmet/core/feff/task.py
@@ -1,4 +1,7 @@
 """ Core definition of a VASP Task Document """
+
+from __future__ import annotations
+
 from typing import Any, Dict, List
 
 from pydantic import Field
@@ -7,8 +10,8 @@ from pymatgen.core import Structure
 from pymatgen.core.periodic_table import Element, Species
 
 from emmet.core.structure import StructureMetadata
-from emmet.core.vasp.task_valid import TaskDocument as BaseTaskDocument
 from emmet.core.utils import ValueEnum
+from emmet.core.vasp.task_valid import TaskDocument as BaseTaskDocument
 
 
 class CalcType(ValueEnum):
