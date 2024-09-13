501ba7c7fabda93d58a0a9928158b6d795488d75
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2024-09-13 09:40:47 -0700

    make enum declaration clearer

--- emmet-core/emmet/core/qchem/calc_types/calc_types.py
+++ emmet-core/emmet/core/qchem/calc_types/calc_types.py
@@ -2,17 +2,21 @@
 
 from importlib.resources import files as import_resource_files
 from monty.serialization import loadfn
-import sys
 
 __author__ = "Evan Spotte-Smith <ewcspottesmith@lbl.gov>"
 
-for attr, vals in loadfn(
+_calc_type_config = loadfn(
     str(import_resource_files("emmet.core.qchem.calc_types") / "calc_types.yaml")
-).items():
-    setattr(sys.modules[__name__], attr, vals)
+)
+
+# NB: this would be easier with setattr but the following is less opaque
+FUNCTIONAL_CLASSES = _calc_type_config.get("FUNCTIONAL_CLASSES")
+TASK_TYPES = _calc_type_config.get("TASK_TYPES")
+BASIS_SETS = _calc_type_config.get("BASIS_SETS")
+SOLVENT_MODELS = _calc_type_config.get("SOLVENT_MODELS")
 
 FUNCTIONALS = [
     rt
-    for functionals in FUNCTIONAL_CLASSES.values()  # noqa: F821
+    for functionals in FUNCTIONAL_CLASSES.values()
     for rt in functionals
 ]
--- emmet-core/emmet/core/vasp/calc_types/enums.py
+++ emmet-core/emmet/core/vasp/calc_types/enums.py
@@ -4,14 +4,11 @@ Autogenerate enums for run, task, and calc type.
 Do not edit this by hand. Edit generate.py or run_types.yaml instead.
 """
 from importlib.resources import files as import_resource_files
-import sys
-
 from monty.serialization import loadfn
-
 from emmet.core.utils import IgnoreCaseEnum
 
-_BASE_ENUM_PATH = (
-    import_resource_files("emmet.core.vasp.calc_types") / "rtc_enums.json.gz"
-)
-for enum_name, elements in loadfn(str(_BASE_ENUM_PATH)).items():
-    setattr(sys.modules[__name__], enum_name, IgnoreCaseEnum(enum_name, elements))
+_BASE_ENUM_PATH = import_resource_files("emmet.core.vasp.calc_types") / "rtc_enums.json.gz"
+_VASP_ENUMS = loadfn(str(_BASE_ENUM_PATH))
+RunType = IgnoreCaseEnum("RunType",_VASP_ENUMS.get("RunType"))
+TaskType = IgnoreCaseEnum("TaskType",_VASP_ENUMS.get("TaskType"))
+CalcType = IgnoreCaseEnum("CalcType",_VASP_ENUMS.get("CalcType"))
