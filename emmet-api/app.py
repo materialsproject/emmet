ce3e927dce06f89c5043319c641ccdf6d14d9505
yang-ruoxi <roxie@Live.cn>
2024-11-06 14:12:27 -0800

    use cors when not debugging

--- emmet-api/app.py
+++ emmet-api/app.py
@@ -31,6 +31,7 @@ app.add_middleware(
     AccessLoggerMiddleware,
     format='%(h)s %(t)s %(m)s %(U)s?%(q)s %(H)s %(s)s %(b)s "%(f)s" "%(a)s" %(D)s %(p)s %({x-consumer-id}i)s',
 )
-app.add_middleware(CORSMiddleware, expose_headers=["x-consumer-id"])
+if default_settings.DEBUG:
+    app.add_middleware(CORSMiddleware, expose_headers=["x-consumer-id"])
 delta = time.perf_counter() - start
 logger.warning(f"Startup took {delta:.1f}s")
