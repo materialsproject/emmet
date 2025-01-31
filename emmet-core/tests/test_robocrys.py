843527bee4e1af293e0dbc8be6a24731a4f3ff18
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2025-01-31 15:40:52 -0800

    enable robocrys tests

--- emmet-core/tests/test_robocrys.py
+++ emmet-core/tests/test_robocrys.py
@@ -1,16 +1,19 @@
 import pytest
-from emmet.core.robocrys import RobocrystallogapherDoc
 from pymatgen.core import Structure
+from robocrys import __version__ as __robocrys_version__
 
 from . import test_structures
+from emmet.core.robocrys import RobocrystallogapherDoc
 
 
-@pytest.mark.skip(reason="Skip until matminer is released")
 @pytest.mark.parametrize("structure", test_structures.values())
 def test_robocrys(structure: Structure):
     """Very simple test to make sure this actually works"""
     print(f"Should work : {structure.composition}")
     doc = RobocrystallogapherDoc.from_structure(
-        structure=structure, material_id=33, deprecated=False
+        structure=structure,
+        material_id=33,
+        deprecated=False,
+        robocrys_version=__robocrys_version__,
     )
     assert doc is not None
