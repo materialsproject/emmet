d3bbd331ec6e5ab841aad087b0324f29502f6e90
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2025-02-13 12:45:38 -0800

    serialization adapter for pmg diffraction pattern

--- emmet-core/emmet/core/serialization_adapters/__init__.py
+++ emmet-core/emmet/core/serialization_adapters/__init__.py
@@ -1,6 +1,7 @@
 from emmet.core.serialization_adapters import (
     balanced_reaction_adapter,
     composition_adapter,
+    diffraction_pattern_adapter,
     ir_dielectric_tensor_adapter,
     phonon_adapter,
     trajectory_adapter,
---
+++ emmet-core/emmet/core/serialization_adapters/diffraction_pattern_adapter.py
@@ -0,0 +1,31 @@
+import pymatgen.analysis.diffraction.xrd
+from pydantic import RootModel
+from typing_extensions import TypedDict
+
+
+class TypedHKLDict(TypedDict):
+    hkl: list[int]
+    multiplicity: int
+
+
+TypedDiffractionPattern = TypedDict(
+    "TypedDiffractionPattern",
+    {
+        "@module": str,
+        "@class": str,
+        "@version": str,
+        "x": list[float],
+        "y": list[float],
+        "hkls": list[list[TypedHKLDict]],
+        "d_hkls": list[float],
+    },
+)
+
+
+class DiffractionPatternAdapter(RootModel):
+    root: TypedDiffractionPattern
+
+
+pymatgen.analysis.diffraction.xrd.DiffractionPattern.__pydantic_model__ = (
+    DiffractionPatternAdapter
+)
