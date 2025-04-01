1957e7f3620684f84018a9fc5050fe679c228b32
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2025-03-31 18:58:34 -0700

    context model for ref - likely remove later

--- emmet-core/emmet/core/common.py
+++ emmet-core/emmet/core/common.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timezone
+from typing import Any
 
 from monty.json import MontyDecoder
+from pydantic import BaseModel, ValidationInfo, model_validator
 
 from emmet.core.utils import ValueEnum, utcnow
 
@@ -35,3 +37,29 @@ class Status(ValueEnum):
 
     SUCCESS = "successful"
     FAILED = "failed"
+
+
+class ContextModel(BaseModel):
+    """
+    Overrides BaseModel's init to provide additional positional arg.
+    Context can be passed to model constructor to handle deserialization
+    of input data types that the default validation handler does not
+    understand.
+    """
+
+    def __init__(
+        self, __context: dict[str, Any] | None = None, /, **data: Any
+    ) -> None:  # type: ignore
+        __tracebackhide__ = True
+        self.__pydantic_validator__.validate_python(
+            data, self_instance=self, context=__context
+        )
+
+    @model_validator(mode="wrap")
+    def model_deserialization(cls, values, default_deserializer, info: ValidationInfo):
+        format = info.context.get("format") if info.context else "standard"
+        if format == "arrow":
+            print("would deserialize arrow inputs!")
+            return
+
+        return default_deserializer(values, info)
