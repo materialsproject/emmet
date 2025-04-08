a046e65e687225206670406f2539de3e42885cf6
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-04-08 10:04:11 -0700

    add links to matpes downloads in docstr

--- emmet-core/emmet/core/ml.py
+++ emmet-core/emmet/core/ml.py
@@ -382,6 +382,10 @@ class MatPESTrainDoc(MLTrainDoc):
     """
     Schema for VASP data in the Materials Potential Energy Surface (MatPES) effort.
 
+    This schema is used in the data entries for MatPES v2025.1,
+    which can be downloaded either:
+        - On [MPContribs](https://materialsproject-contribs.s3.amazonaws.com/index.html#MatPES_2025_1/)
+        - or on [the site](https://matpes.ai)
     """
 
     matpes_id: str | None = Field(None, description="MatPES identifier.")
