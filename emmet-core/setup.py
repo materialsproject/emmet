26a20a427a1037135cb9ff2aee0877a741ee4e1b
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-04-08 15:38:44 -0700

    remove py.typed from setup

--- emmet-core/setup.py
+++ emmet-core/setup.py
@@ -22,7 +22,6 @@ setup(
     url="https://github.com/materialsproject/emmet",
     packages=find_namespace_packages(include=["emmet.*"]),
     package_data={
-        "emmet.core": ["py.typed"],
         "emmet.core.vasp.calc_types": ["*.yaml"],
         "emmet.core.qchem.calc_types": ["*.yaml"],
         "emmet.core.subtrates": ["*.json"],
