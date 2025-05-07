b9a23bf9ac875003cd8c44e03815e7684850a177
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-05-07 15:45:20 -0700

    kludgey way to hopefully get test_files to resolve correctly in CI

--- emmet-api/tests/conftest.py
+++ emmet-api/tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 
-from emmet.core.testing_utils import TEST_FILES_DIR
+from emmet.core.testing_utils import _get_test_files_dir
 
 
 @pytest.fixture(scope="session")
 def test_dir():
-    return TEST_FILES_DIR
+    return _get_test_files_dir("emmet.api")
--- emmet-builders/tests/conftest.py
+++ emmet-builders/tests/conftest.py
@@ -1,7 +1,7 @@
-from emmet.core.testing_utils import TEST_FILES_DIR
+from emmet.core.testing_utils import _get_test_files_dir
 import pytest
 
 
 @pytest.fixture(scope="session")
 def test_dir():
-    return TEST_FILES_DIR
+    return _get_test_files_dir("emmet.builders")
