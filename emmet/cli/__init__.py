da2745ff36d6bce768a0557010681d5d1d2fd3bb
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-05-07 15:20:40 -0700

    undo directory structure change

--- emmet/cli/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from emmet.cli.settings import EmmetCLISettings
-
-SETTINGS = EmmetCLISettings()
-logging.basicConfig(
-    level=logging.INFO, format="%(name)-12s: %(levelname)-8s %(message)s"
-)
