aee04530b40c57e236d686b74cd574e325042a2c
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-05-07 16:01:38 -0700

    mypy is picky

--- emmet-core/emmet/core/testing_utils.py
+++ emmet-core/emmet/core/testing_utils.py
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
 def _get_test_files_dir(module: str) -> Path:
     """Get path to test files directory."""
     return (
-        Path(import_resource(module))
+        Path(str(import_resource(module)))
         .parent.parent.parent.joinpath("test_files")
         .resolve()
     )
