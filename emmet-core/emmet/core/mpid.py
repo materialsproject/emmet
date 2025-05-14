7c384d60a0e51a765c3fc1bdc2eaebc608230cf4
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-05-14 11:36:49 -0700

    redraft alpha id

--- emmet-core/emmet/core/mpid.py
+++ emmet-core/emmet/core/mpid.py
@@ -1,11 +1,18 @@
-# %%
+"""Define identifier schemas used in MP."""
+from __future__ import annotations
+
+from math import log, floor
 import re
-from typing import Union, Any, Callable
+from string import ascii_lowercase, digits
+from typing import Union, Any, Callable, TYPE_CHECKING
 
 from pydantic_core import CoreSchema, core_schema
 
 from pydantic import GetJsonSchemaHandler
 from pydantic.json_schema import JsonSchemaValue
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
 
 
 # matches "mp-1234" or "1234" followed by and optional "-(Alphanumeric)"
@@ -126,10 +133,6 @@ class MPID(str):
 
         raise ValueError("Invalid MPID Format")
 
-
-# %%
-
-
 class MPculeID(str):
     """
     A Materials Project Molecule ID with a prefix, hash, and two integer values
@@ -225,3 +228,224 @@ class MPculeID(str):
             return MPculeID(__input_value)
 
         raise ValueError("Invalid MPculeID Format")
+
+
+class AlphaID(str):
+    """Identifier based on representing an integer as an alphabetical string.
+
+    Args:
+        _alphabet (str) : The alphabet to use, defaults to lowercase Roman.
+        _identifier (str) : The alphabetical string identifier.
+        _padlen (int) : The minimum length of an identifier to pad left with zeroes.
+        _prefix (str) : The ID prefix, ex. "mp"
+        _separator (str) : The separator between `_prefix` and `_identifier`, ex: "-"
+    """
+
+    _identifier : str
+    _padlen : int
+    _prefix : str
+    _separator : str
+    _alphabet: str = ascii_lowercase
+    _value : int | None = None
+
+    def __new__(
+        cls,
+        identifier: str | int | MPID,
+        padlen: int = 0,
+        prefix: str | None = None,
+        separator: str = "-",
+    ) -> Self:
+        """Define a new instance of AlphaID.
+
+        Args:
+            identifier (str, int, or MPID) : the identifier, either a string with characters belonging
+                to AlphaID._alphabet, an integer to represent as a string, or an MPID.
+            padlen (int, default = 0) : the amount of characters to pad to if a character
+                string is too short. For example, the integer 149 has alpha representation "ft",
+                and thus `AlphaID(149, padlen = 6)` would present as "aaaaft".
+                Both "ft" and "aaaaft" have the same integer value.
+            prefix (str or None, default = None) : if a str, the prefix to use.
+                For example, `AlphaID(149, padlen = 6, prefix="mp")` would present as "mp-aaaaft".
+            separator (str, default = "-") : the separator to use between the prefix and the
+                string representation of the integer, if the prefix is non-empty.
+                For example, `AlphaID(149, padlen = 6, prefix="mp", separator = ":")` would
+                present as "mp:aaaaft".
+        """
+
+        if isinstance(identifier, str):
+            if len(
+                non_alpha_char := set(identifier).difference(cls._alphabet + digits)
+            ) == 1:
+                separator = list(non_alpha_char)[0]
+                prefix, identifier = identifier.split(separator)
+            elif len(non_alpha_char) > 1:
+                raise ValueError(f"Too many non-alpha-numeric characters: {', '.join(non_alpha_char)}")
+            
+        elif isinstance(identifier, MPID):
+            split_mpid = identifier.string.split("-")
+            if len(split_mpid) == 1:
+                identifier = split_mpid[0]
+            else:
+                prefix, identifier = split_mpid
+                separator = "-"
+
+        if isinstance(identifier,str):
+            identifier = int(identifier)
+        if isinstance(identifier, int):
+            identifier = cls._integer_to_alpha_rep(identifier)
+            
+        prefix = prefix or ""
+        if len(prefix) == 0:
+            separator = ""
+
+        padded = max(0, padlen - len(identifier)) * cls._alphabet[0]
+        new_cls = str.__new__(cls, prefix + separator + padded + identifier)
+        new_cls._identifier = identifier
+        new_cls._padlen = padlen
+        new_cls._prefix = prefix
+        new_cls._separator = separator
+        return new_cls
+
+    @classmethod
+    def _string_to_base_10_value(cls, string: str) -> int:
+        """Obtain the integer value of an alphabetical string."""
+        value = 0
+        rev_codex = {letter: idx for idx, letter in enumerate(cls._alphabet)}
+        base = len(cls._alphabet)
+        for ipow, char in enumerate(string[::-1]):
+            value += rev_codex[char] * base**ipow
+        return value
+
+    @classmethod
+    def _integer_to_alpha_rep(cls, integer: int) -> str:
+        """Obtain the string representation of an integer."""
+        if integer == 0:
+            return cls._alphabet[0]
+
+        base = len(cls._alphabet)
+        max_pow = floor(log(integer) / log(base))
+        string: str = ""
+        rem = integer
+        for pow in range(max_pow, -1, -1):
+            if rem == 0:
+                string += cls._alphabet[0]
+                continue
+            mult = base**pow
+            for coeff in range(base - 1, -1, -1):
+                if coeff * mult <= rem:
+                    string += cls._alphabet[coeff]
+                    rem -= coeff * mult
+                    break
+        return string    
+
+    def __int__(self) -> int:
+        """Get and cache the current AlphaID's integer value."""
+        if self._value is None:
+            self._value = self._string_to_base_10_value(self._identifier)
+        return self._value
+
+    def __repr__(self):
+        """Set AlphaID display name to distinguish from base string class."""
+        return "AlphaID(" + self + ")"
+    
+    def __eq__(self, other : Any) -> bool:
+        """Define equality of AlphaID.
+
+        If other is an int, returns True if the integer value of the
+        current instance equals the other.
+
+        If other is a base str, returns True if the string reprentations
+        are equal.
+
+        If other is an AlphaID, returns True only if the prefix, separator,
+        and value of the two are equal.
+        """
+        if isinstance(other, MPID):
+            test = AlphaID(other)
+        else:
+            test = other
+
+        if isinstance(test,int):
+            return int(self) == test
+        elif isinstance(test,AlphaID):
+            return (
+                test._prefix == self._prefix
+                and test._separator == self._separator
+                and int(test) == int(self)
+            )
+        elif isinstance(test,str):
+            return test == str(self)
+        return False
+
+    def __ne__(self, other : Any) -> bool:
+        """Define inverse equality for AlphaID."""
+        return not self.__eq__(other)
+
+    def __add__(self, other : Any) -> "AlphaID":
+        """Define addition of AlphaID.
+
+        Returns an AlphaID with the same `padlen` as the current instance, not `other`.
+        Thus the order of addition can change the presentation of AlphaID, but not
+        its value.
+
+        If other is also an AlphaID, but its `prefix` and `separator` do not match,
+        will not add the two. Only checks the separator if prefixes are both non-null.
+
+        Args:
+            other (str or int) : the value to add to the current identifier.
+                If a string, its integer value is first computed.
+                The integer values are then added, and a new instance
+                of AlphaID is returned.
+        Returns:
+            AlphaID representing the sum of the current and other values.
+        """
+
+        if isinstance(other, MPID):
+            test = AlphaID(other)
+        else:
+            test = other
+
+        if isinstance(test, AlphaID):
+
+            exc_str = ""
+            if test._prefix != self._prefix:
+                exc_str += f"Prefixes do not match: left = {self._prefix or None}, right {test._prefix or None}. "
+
+            if (
+                test._prefix and self._prefix and test._separator != self._separator
+            ):
+                exc_str += f"Separators do not match: left = {self._separator}, right {test._separator}."
+
+            if exc_str:
+                raise TypeError(exc_str)
+            
+            test = self._string_to_base_10_value(test._identifier)
+        elif isinstance(test, str):
+            test = self._string_to_base_10_value(test)
+        elif not isinstance(test,int ):
+            raise NotImplementedError(f"Cannot add AlphaID to type {type(test)}")
+
+        return AlphaID(
+            int(self) + test,
+            padlen=self._padlen,
+            prefix=self._prefix,
+            separator=self._separator,
+        )
+
+    def __sub__(self, other : Any) -> "AlphaID":
+        """Define subtraction of AlphaID.
+
+        See the docstring for `__add__`. The resultant `padlen` is taken
+        from the current instance, not `other`.
+
+        Will not subtract two AlphaIDs if `prefix` and `separator` do not match.
+        """
+        if isinstance(other, AlphaID):
+            test = self._string_to_base_10_value(other._identifier)
+        elif isinstance(other,MPID):
+            test = self._string_to_base_10_value(AlphaID(other)._identifier)
+        elif isinstance(other, str):
+            test = self._string_to_base_10_value(other)
+        else:
+            test = other
+        return self.__add__(-test)
--- emmet-core/tests/test_mpid.py
+++ emmet-core/tests/test_mpid.py
@@ -1,4 +1,4 @@
-from emmet.core.mpid import MPID, MPculeID
+from emmet.core.mpid import MPID, MPculeID, AlphaID
 import pytest
 
 
@@ -56,3 +56,74 @@ def test_to_str():
         str(MPculeID("b9ba54febc77d2a9177accf4605767db-F6Li1P1-1-2"))
         == "b9ba54febc77d2a9177accf4605767db-F6Li1P1-1-2"
     )
+
+
+def test_alpha_id():
+
+    # Test initialization from MPID-like string
+    mpid_like_idx = AlphaID("mp-149")
+    assert mpid_like_idx == AlphaID(149,prefix="mp",separator="-")
+    assert mpid_like_idx._prefix == "mp"
+    assert mpid_like_idx._separator == "-"
+    assert int(mpid_like_idx) == 149
+    assert mpid_like_idx == MPID("mp-149")
+    assert mpid_like_idx == AlphaID(MPID("mp-149"))
+    assert AlphaID("mvc-160") == MPID("mvc-160")
+    assert AlphaID(160) == MPID("160")
+    assert mpid_like_idx + MPID("mp-140") == AlphaID("mp-289")
+    assert mpid_like_idx - MPID("mp-140") == AlphaID("mp-9")
+    
+    majik_num = 137
+
+    # Test initialization from int
+    for pfx in (None, "iamaprefix",):
+
+        for separator in ("-",":",">"):
+            idx = AlphaID(majik_num,prefix=pfx,separator=separator)
+            assert int(idx) == majik_num
+
+            # Test integer equality
+            assert idx == majik_num
+
+            # Test in/equality when prefix and separators do not match
+            assert idx != AlphaID(majik_num,prefix="unusedprefix")
+
+            # Test equality when separators differ but prefixes are None
+            # (separators are unset)
+            idx_diff_sep = AlphaID(majik_num,prefix=pfx,separator="&")
+            if pfx is None:
+                assert idx == idx_diff_sep
+            else:
+                assert idx != idx_diff_sep
+
+            # Test integer addition
+            assert idx + 1 == majik_num + 1
+            assert idx + 1 == AlphaID( majik_num + 1, prefix=pfx, separator=separator)
+
+            # Test string addition/subtraction
+            assert idx + "y" == AlphaID(majik_num + 24, prefix=pfx,separator=separator)
+            assert idx - "z" == AlphaID(majik_num - 25, prefix=pfx,separator=separator)
+
+            # Test equality, addition, subtraction of AlphaID
+            other_idx = AlphaID(100)
+            assert other_idx != idx
+            if pfx is None:
+                assert idx - other_idx == majik_num - 100
+                assert idx - other_idx == AlphaID(majik_num - 100)
+
+                assert idx + other_idx == majik_num + 100
+                assert idx + other_idx == AlphaID(majik_num + 100)
+    
+    # test iterative addition / subtraction
+    last_val = None
+    next_val = None
+    for i in range(5000):
+        alpha = AlphaID(i)
+        assert int(alpha) == i
+        if i > 0:
+            assert alpha - 1 == last_val
+            assert alpha - AlphaID(1) == last_val
+            assert alpha == next_val
+            
+        last_val = alpha
+        next_val = alpha + 1
