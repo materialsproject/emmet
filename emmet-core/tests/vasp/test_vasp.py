d7850fa6945d920d91c96693007608fc3b07306c
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-06-11 14:50:59 -0700

    temporarily nuke validator tests since this is tested externally

--- emmet-core/tests/vasp/test_vasp.py
+++ emmet-core/tests/vasp/test_vasp.py
@@ -1,133 +1,24 @@
-import json
-
-import pytest
-from monty.io import zopen
-
 from emmet.core.tasks import TaskDoc
-from emmet.core.vasp.calc_types import RunType, TaskType, run_type, task_type
-from emmet.core.vasp.task_valid import TaskDocument
 from emmet.core.vasp.validation import ValidationDoc
 
-
-def test_task_type():
-    # TODO: Switch this to actual inputs?
-    input_types = [
-        ("NSCF Line", {"incar": {"ICHARG": 11}, "kpoints": {"labels": ["A"]}}),
-        ("NSCF Uniform", {"incar": {"ICHARG": 11}}),
-        ("Dielectric", {"incar": {"LEPSILON": True}}),
-        ("DFPT Dielectric", {"incar": {"LEPSILON": True, "IBRION": 7}}),
-        ("DFPT Dielectric", {"incar": {"LEPSILON": True, "IBRION": 8}}),
-        ("DFPT", {"incar": {"IBRION": 7}}),
-        ("DFPT", {"incar": {"IBRION": 8}}),
-        ("Static", {"incar": {"NSW": 0}}),
-    ]
-
-    for _type, inputs in input_types:
-        assert task_type(inputs) == TaskType(_type)
+# As the validator itself is a separate package with its own tests,
+# only check that its integration within emmet-core works.
 
 
-def test_run_type():
-    params_sets = [
-        ("GGA", {"GGA": "--"}),
-        ("GGA+U", {"GGA": "--", "LDAU": True}),
-        ("SCAN", {"METAGGA": "Scan"}),
-        ("SCAN+U", {"METAGGA": "Scan", "LDAU": True}),
-        ("r2SCAN", {"METAGGA": "R2SCAN"}),
-        ("r2SCAN+U", {"METAGGA": "R2SCAN", "LDAU": True}),
-        ("HFCus", {"LHFCALC": True}),
-    ]
-
-    for _type, params in params_sets:
-        assert run_type(params) == RunType(_type)
-
-
-@pytest.fixture(scope="session")
-def tasks(test_dir):
-    with zopen(test_dir / "test_si_tasks.json.gz") as f:
-        data = json.load(f)
-
-    return [TaskDoc(**d) for d in data]
-
-
-def test_validator(tasks):
-    validation_docs = [ValidationDoc.from_task_doc(task) for task in tasks]
-
-    assert len(validation_docs) == len(tasks)
-    assert all([doc.valid for doc in validation_docs])
-
-
-def test_validator_magmom(test_dir):
-    # Task with Cr in structure - this is only element with MAGMOM check
-
-    def cr_task_dict():
-        # Model validation for TaskDoc serializes some fields.
-        # Need to refresh reference unserialized dict to have
-        # serialization for TaskDocument also work.
-        with zopen(test_dir / "task_doc_mp-2766060.json.gz") as f:
-            _cr_task_dict = json.load(f)
-        return _cr_task_dict
-
-    taskdoc = TaskDoc(**cr_task_dict())
-    assert ValidationDoc.from_task_doc(taskdoc).valid
-
-    # test backwards compatibility
-    taskdocument = TaskDocument(
-        **{k: v for k, v in cr_task_dict().items() if k != "last_updated"}
-    )
-    assert ValidationDoc.from_task_doc(taskdocument).valid
-
-    # Change MAGMOM on Cr to fail magmom test
-    td_bad_mag = TaskDoc(**cr_task_dict())
-    td_bad_mag.calcs_reversed[0].output.outcar["magnetization"] = [
-        {"tot": 6.0} if td_bad_mag.structure[ientry].species_string == "Cr" else entry
-        for ientry, entry in enumerate(
-            td_bad_mag.calcs_reversed[0].output.outcar["magnetization"]
-        )
-    ]
-    assert not (valid_doc := ValidationDoc.from_task_doc(td_bad_mag)).valid
-    assert any("MAG" in repr(reason) for reason in valid_doc.reasons)
-
-    # Remove magnetization tag to simulate spin-unpolarized (ISPIN = 1) calculation
-    td_no_mag = TaskDoc(**cr_task_dict())
-    del td_no_mag.calcs_reversed[0].output.outcar["magnetization"]
-    assert ValidationDoc.from_task_doc(td_no_mag).valid
-
-
-def test_validator_failed_symmetry(test_dir):
-    with zopen(test_dir / "failed_elastic_task.json.gz", "r") as f:
-        failed_task = json.load(f)
-    taskdoc = TaskDoc(**failed_task)
-    validation = ValidationDoc.from_task_doc(taskdoc)
-    assert any("SYMMETRY" in repr(reason) for reason in validation.reasons)
-
-
-def test_computed_entry(tasks):
-    entries = [task.entry for task in tasks]
-    ids = {e.entry_id for e in entries}
-    assert ids == {"mp-1141021", "mp-149", "mp-1686587", "mp-1440634"}
-
-
-@pytest.fixture(scope="session")
-def task_ldau(test_dir):
-    with zopen(test_dir / "test_task.json") as f:
-        data = json.load(f)
-
-    return TaskDoc(**data)
-
-
-def test_ldau(task_ldau):
-    task_ldau.input.is_hubbard = True
-    assert task_ldau.run_type == RunType.GGA_U
-    assert not ValidationDoc.from_task_doc(task_ldau).valid
-
-
-def test_ldau_validation(test_dir):
-    with open(test_dir / "old_aflow_ggau_task.json") as f:
-        data = json.load(f)
-
-    task = TaskDoc(**data)
-    assert task.run_type == "GGA+U"
-
-    valid = ValidationDoc.from_task_doc(task)
-
-    assert valid.valid
+def test_validator_integration(test_dir):
+    calc_dirs = {
+        "Si_old_double_relax": ["VASP VERSION"],
+        "Si_uniform": [],
+        "Si_static": [],
+    }
+    for calc_dir, ref_reasons in calc_dirs.items():
+        task_doc = TaskDoc.from_directory(test_dir / "vasp" / calc_dir)
+        valid_doc = ValidationDoc.from_task_doc(task_doc, check_potcar=False)
+        if len(ref_reasons) == 0:
+            assert valid_doc.valid
+        else:
+            assert not valid_doc.valid
+            assert all(
+                any(reason_match in reason for reason in valid_doc.reasons)
+                for reason_match in ref_reasons
+            )
