5cbc158828218ddb014c2a7382833bd3bb802b3e
Aaron Kaplan <kaplan@temple.edu>
2025-08-01 09:20:19 +0200

    api test

--- emmet-api/tests/materials/tasks/test_utils.py
+++ emmet-api/tests/materials/tasks/test_utils.py
@@ -10,7 +10,7 @@ from emmet.api.routes.materials.tasks.utils import (
 
 def test_calcs_reversed_to_trajectory():
     with open(
-        os.path.join(MAPISettings().TEST_FILES, "calcs_reversed_mp_1031016.json")
+        os.path.join(MAPISettings().TEST_FILES, "calcs_reversed_mp_1031016.json.gz")
     ) as file:
         calcs_reversed = load(file)
         trajectories = calcs_reversed_to_trajectory(calcs_reversed)
@@ -24,7 +24,7 @@ def test_calcs_reversed_to_trajectory():
 
 
 def test_task_to_entry():
-    with open(os.path.join(MAPISettings().TEST_FILES, "test_task.json")) as file:
+    with open(os.path.join(MAPISettings().TEST_FILES, "test_task.json.gz")) as file:
         doc = load(file)
 
     entry_dict = task_to_entry(doc)
