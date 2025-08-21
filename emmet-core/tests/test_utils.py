b4199f12041c1e54ed21733c8531cb5e881d534a
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-08-21 09:51:42 -0700

    add test for data archive class

--- emmet-core/tests/test_utils.py
+++ emmet-core/tests/test_utils.py
@@ -14,7 +14,7 @@ from emmet.core.utils import (
     dynamic_import,
 )
 from monty.json import MSONable
-from monty.serialization import dumpfn
+from monty.serialization import dumpfn, loadfn
 
 from emmet.core.tasks import TaskDoc
 
@@ -138,3 +138,36 @@ def test_model_flatten():
 def test_import():
 
     assert dynamic_import("emmet.core.tasks.TaskDoc") == TaskDoc
+
+
+def test_data_archive(tmp_dir):
+
+    from monty.io import zopen
+    from emmet.core.testing_utils import DataArchive
+
+    def listify_file(file_name):
+        with zopen(file_name, "rt") as f:
+            return list(f.read())
+
+    test_str = "the quick brown fox jumped over the lazy dog"
+    test_files = {"test_file", "test_gzipped.gz"}
+    for p in test_files:
+        with zopen(p, "wt") as f:
+            f.write(test_str)
+
+    archive = DataArchive.from_directory(".")
+
+    archive_path = Path("test.json.gz")
+    archive.to_json(archive_path)
+    assert archive_path.is_file()
+    loaded = loadfn(archive_path)
+    assert set(loaded) == test_files
+    assert loaded["test_file"]["0"] == test_str.encode().hex()
+
+    with DataArchive.extract(archive_path) as temp_dir:
+        assert {p.name for p in temp_dir.glob("*")} == test_files
+
+    for fname in test_files:
+        assert DataArchive.extract_obj(archive_path, fname, listify_file) == list(
+            test_str
+        )
