87c678f29b944235001972b573c2e0b51161e7fb
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-09-08 11:11:53 -0700

    maximize mol/qchem test coverage while removing openbabel from normal test stack

--- emmet-core/tests/molecules/test_redox.py
+++ emmet-core/tests/molecules/test_redox.py
@@ -1,51 +1,51 @@
 import pytest
 
-from monty.serialization import loadfn
-
 from emmet.core.qchem.task import TaskDocument
 from emmet.core.qchem.molecule import MoleculeDoc
 from emmet.core.molecules.thermo import MoleculeThermoDoc
 from emmet.core.molecules.redox import RedoxDoc
 
+from tests.conftest_qchem import safe_load
+
 
 @pytest.fixture(scope="session")
 def base_mol(test_dir):
-    mol = loadfn((test_dir / "redox_doc" / "base_mol.json.gz").as_posix())
+    mol = safe_load((test_dir / "redox_doc" / "base_mol.json.gz").as_posix())
     mol_doc = MoleculeDoc(**mol)
     return mol_doc
 
 
 @pytest.fixture(scope="session")
 def base_thermo(test_dir):
-    thermo = loadfn((test_dir / "redox_doc" / "thermo.json.gz").as_posix())
+    thermo = safe_load((test_dir / "redox_doc" / "thermo.json.gz").as_posix())
     thermo_doc = MoleculeThermoDoc(**thermo)
     return thermo_doc
 
 
 @pytest.fixture(scope="session")
 def red_thermo(test_dir):
-    thermo = loadfn((test_dir / "redox_doc" / "red_thermo.json.gz").as_posix())
+    thermo = safe_load((test_dir / "redox_doc" / "red_thermo.json.gz").as_posix())
     thermo_doc = MoleculeThermoDoc(**thermo)
     return thermo_doc
 
 
 @pytest.fixture(scope="session")
 def ox_thermo(test_dir):
-    thermo = loadfn((test_dir / "redox_doc" / "ox_thermo.json.gz").as_posix())
+    thermo = safe_load((test_dir / "redox_doc" / "ox_thermo.json.gz").as_posix())
     thermo_doc = MoleculeThermoDoc(**thermo)
     return thermo_doc
 
 
 @pytest.fixture(scope="session")
 def ie_task(test_dir):
-    task = loadfn((test_dir / "redox_doc" / "ie_task.json.gz").as_posix())
+    task = safe_load((test_dir / "redox_doc" / "ie_task.json.gz").as_posix())
     task_doc = TaskDocument(**task)
     return task_doc
 
 
 @pytest.fixture(scope="session")
 def ea_task(test_dir):
-    task = loadfn((test_dir / "redox_doc" / "ea_task.json.gz").as_posix())
+    task = safe_load((test_dir / "redox_doc" / "ea_task.json.gz").as_posix())
     task_doc = TaskDocument(**task)
     return task_doc
 
