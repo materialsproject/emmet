7c146322f32671cb89cd3b80c5fda581af6430b7
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-10-23 13:55:15 -0700

    move xas field validation to existing type adapter/annotation

--- emmet-core/emmet/core/types/pymatgen_types/xas_adapter.py
+++ emmet-core/emmet/core/types/pymatgen_types/xas_adapter.py
@@ -34,10 +34,12 @@ TypedXASSpectrumDict = TypedDict(
 XASTypeVar = TypeVar("XASTypeVar", XAS, TypedXASSpectrumDict)
 
 
-def pop_xas_empty_structure_fields(xas: XASTypeVar):
+def pop_xas_empty_structure_fields(xas: XASTypeVar, eps: float = 1e-12):
+    """Remove empty keys in the XAS dict and ensure that the spectral data are positive."""
     if isinstance(xas, dict):
         clean_structure = pop_empty_structure_keys(xas["structure"])
         xas["structure"] = clean_structure
+        xas["y"] = [y if y > 0.0 else abs(eps) for y in xas["y"]]
         return XAS.from_dict(xas)
 
     return xas
