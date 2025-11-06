f21cb25004333131020a002fd3e5be24dc666108
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-11-06 08:54:48 -0800

    mp aloe docs

--- emmet-core/emmet/core/ml.py
+++ emmet-core/emmet/core/ml.py
@@ -457,3 +457,24 @@ class MPtrjTrainDoc(MLTrainDoc):
     provenance: MPtrjProvenance | None = Field(
         None, description="Metadata for this frame."
     )
+
+
+class MPAloeTrainDoc(MatPESTrainDoc):
+    """Schematize MP-ALOE data."""
+
+    mp_aloe_id: str | None = Field(
+        None, description="The identifier of this entry in MP-ALOE."
+    )
+    ionic_step_number: int | None = Field(
+        None, description="The ionic step index of this frame."
+    )
+    prototype_number: int | None = Field(
+        None, description="The index of the prototype structure used in generation."
+    )
+    is_charge_balanced: bool | None = Field(
+        None, description="Whether the structure is likely charge balanced."
+    )
+    has_overlapping_pseudo_cores: bool | None = Field(
+        None,
+        description="Whether the pseudopotential cores overlap for at least one set of nearest neighbors.",
+    )
