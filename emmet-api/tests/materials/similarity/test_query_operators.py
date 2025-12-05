43ef0955d06d8933df06ec76575bbf8aab7d63b0
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-12-05 13:06:04 -0800

    fix api test

--- emmet-api/tests/materials/similarity/test_query_operators.py
+++ emmet-api/tests/materials/similarity/test_query_operators.py
@@ -1,14 +1,17 @@
 """Test crystalline similarity vector search."""
 
+import numpy as np
 import pytest
-import zlib
 
-from emmet.core.similarity import SimilarityMethod
+from emmet.core.similarity import (
+    SimilarityMethod,
+    _vector_to_hex_and_norm,
+    _vector_from_hex_and_norm,
+)
 from emmet.api.routes.materials.similarity.query_operators import (
     SimilarityFeatureVectorQuery,
     SIM_METHOD_TO_FEAT_VEC_LENGTH,
 )
-import numpy as np
 
 
 @pytest.mark.parametrize(
@@ -20,7 +23,7 @@ def test_similarity_structure_search(test_dir, method):
     fv = np.random.rand(
         SIM_METHOD_TO_FEAT_VEC_LENGTH[SimilarityMethod.CRYSTALNN]
     ).tolist()
-    fv_hex = zlib.compress(np.array(fv).tobytes()).hex()
+    fv_hex, fv_norm = _vector_to_hex_and_norm(fv)
 
     limit = 10
     q = {
@@ -29,7 +32,7 @@ def test_similarity_structure_search(test_dir, method):
                 "$vectorSearch": {
                     "index": "similarity_feature_vector",
                     "path": "feature_vector",
-                    "queryVector": fv,
+                    "queryVector": _vector_from_hex_and_norm(fv_hex, fv_norm),
                     "numCandidates": limit,
                     "limit": limit,
                 }
@@ -45,7 +48,12 @@ def test_similarity_structure_search(test_dir, method):
         ]
     }
 
-    manual_q = op.query(feature_vector_hex=fv_hex, method=method, _limit=limit)
+    manual_q = op.query(
+        feature_vector_hex=fv_hex,
+        feature_vector_norm=fv_norm,
+        method=method,
+        _limit=limit,
+    )
     assert manual_q == q
 
     doc = [{"meta": {"count": {"total": limit}}}]
