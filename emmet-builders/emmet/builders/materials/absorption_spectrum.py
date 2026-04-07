18311345f92285422f6d82d0836872f39e1be344
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2026-04-07 15:34:46 -0700

    blessed task sort func for absorption builder

--- emmet-builders/emmet/builders/materials/absorption_spectrum.py
+++ emmet-builders/emmet/builders/materials/absorption_spectrum.py
@@ -1,14 +1,18 @@
+from datetime import datetime
+
 from emmet.builders.base import BaseBuilderInput
-from emmet.builders.utils import filter_map
+from emmet.builders.utils import _parse_kpoints, filter_map
 from emmet.core.absorption import AbsorptionDoc
 from emmet.core.material import PropertyOrigin
+from emmet.core.mpid_ext import MaterialIdentifierType
+from emmet.core.tasks import CoreTaskDoc
 from emmet.core.types.typing import DateTimeType
 
 
 class AbsorptionBuilderInput(BaseBuilderInput):
     energies: list[float]
-    real_d: list[float]
-    imag_d: list[float]
+    real_d: list[list[float]]
+    imag_d: list[list[float]]
     absorption_co: list[float]
     bandgap: float | None
     nkpoints: int | None
@@ -57,3 +61,76 @@ def build_absorption_docs(
             **kwargs
         )
     )
+
+
+def obtain_blessed_absorption_builder_input(
+    tasks: list[CoreTaskDoc],
+    material_id: MaterialIdentifierType | None = None,
+    material_last_updated: datetime | None = None,
+) -> AbsorptionBuilderInput:
+    """
+    Yield an AbsorptionBuilderInput
+    from a list of CoreTaskDocs using the 'best' document in the list.
+
+    [Optional] Relevant material properties needed if running in context of
+    building properties for 'material's:
+       - material_id -> anchor identifier for all ``tasks``
+       - material_last_updated -> when anchor material document was last updated
+
+    Relevant CoreTaskDoc fields needed:
+       - task_id
+       - last_updated
+       - input.kpoints
+       - input.structure
+       - orig_inputs.kpoints
+       - output.frequency_dependent_dielectric
+       - output.optical_absorption_coeff
+       - output.bandgap
+
+    CoreTaskDocs need to have non-null optical_absorption_coeff
+    and frequency_dependent_dielectric
+    """
+    relevant_tasks = [
+        {
+            "structure": task.input.structure,
+            "task_id": task.task_id,
+            "nkpoints": _parse_kpoints(task),
+            "energies": task.output.frequency_dependent_dielectric.energy,
+            "real_d": task.output.frequency_dependent_dielectric.real,
+            "imag_d": task.output.frequency_dependent_dielectric.imaginary,
+            "absorption_co": task.output.optical_absorption_coeff,
+            "bandgap": task.output.bandgap,
+            "task_last_updated": task.last_updated,
+        }
+        for task in tasks
+        if task.output.optical_absorption_coeff is not None
+        and task.output.frequency_dependent_dielectric.energy is not None
+        and task.output.frequency_dependent_dielectric.real is not None
+        and task.output.frequency_dependent_dielectric.imaginary is not None
+    ]
+
+    best_task = sorted(
+        relevant_tasks,
+        key=lambda doc: (
+            # Entries with any None sort last (False < True, reversed)
+            doc["nkpoints"] is not None and doc["task_last_updated"] is not None,
+            doc["nkpoints"],
+            doc["task_last_updated"],
+        ),
+        reverse=True,
+    )[0]
+
+    doc = {
+        "material_id": material_id,
+        "last_updated": material_last_updated,
+        "origins": [
+            PropertyOrigin(
+                name="absorption",
+                task_id=best_task["task_id"],
+                last_updated=best_task["task_last_updated"],
+            )
+        ],
+        **best_task,
+    }
+
+    return AbsorptionBuilderInput(**doc)
