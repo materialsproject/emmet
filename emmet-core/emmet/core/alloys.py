001923c5ab43d012b2a4620efdaefe8ee3ba594c
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2026-04-29 16:01:13 -0700

    alloy endpoint compat (#1443)

--- emmet-core/emmet/core/alloys.py
+++ emmet-core/emmet/core/alloys.py
@@ -1,36 +1,9 @@
-from typing_extensions import TypedDict
-
 from emmet.core.base import EmmetBaseModel
 from emmet.core.types.pymatgen_types.alloy_adapter import (
     AlloyPairType,
     AlloySystemType,
     PairID,
 )
-from emmet.core.types.typing import MaterialIdentifierType
-
-
-class TypedBoolDict(TypedDict):
-    min: bool
-    max: bool
-
-
-class TypedRangeDict(TypedDict):
-    min: float
-    max: float
-
-
-class TypedSearchDict(TypedDict):
-    alloying_element: list[str]
-    band_gap: TypedRangeDict
-    energy_above_hull: TypedRangeDict
-    formation_energy_per_atom: TypedRangeDict
-    formula: list[str]
-    id: list[MaterialIdentifierType]
-    is_gap_direct: TypedBoolDict
-    member_ids: list[MaterialIdentifierType]
-    spacegroup_intl_number: TypedRangeDict
-    theoretical: TypedBoolDict
-    volume_cube_root: TypedRangeDict
 
 
 class AlloyPairDoc(EmmetBaseModel):
@@ -38,12 +11,9 @@ class AlloyPairDoc(EmmetBaseModel):
 
     pair_id: PairID
 
-    # fields useful for building search indices
-    _search: TypedSearchDict
-
     @classmethod
     def from_pair(cls, pair):
-        return cls(alloy_pair=pair, pair_id=pair.pair_id, _search=pair.search_dict())
+        return cls(alloy_pair=pair, pair_id=pair.pair_id)
 
 
 class AlloySystemDoc(EmmetBaseModel):
