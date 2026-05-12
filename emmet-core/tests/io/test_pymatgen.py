51f1a1256355123550bb832cbdccdb9b93a4d712
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2026-05-12 13:51:36 -0700

    reduce import string length

--- emmet-core/tests/io/test_pymatgen.py
+++ emmet-core/tests/io/test_pymatgen.py
@@ -8,7 +8,7 @@ from emmet.core.io.pymatgen import (
 def test_dir():
     pmg_list_dir = pmg_io_dir()
     assert pmg_list_dir == sorted(
-        ".".join([base_import, _name_collision_aliases.get(name, name)])
+        ".".join(["pymatgen", base_import, _name_collision_aliases.get(name, name)])
         for name, base_import in _class_map.items()
     )
     assert all(isinstance(v, str) for v in pmg_list_dir)
--- emmet-core/tests/test_ml.py
+++ emmet-core/tests/test_ml.py
@@ -1,8 +1,7 @@
 import pytest
 
 # from matcalc.utils import get_universal_calculator
-from emmet.core.io.pymatgen import Structure
-from pymatgen.util.testing import STRUCTURES_DIR
+from emmet.core.io.pymatgen import Structure, STRUCTURES_DIR
 
 from emmet.core.elasticity import BulkModulus, ElasticTensorDoc, ShearModulus
 from emmet.core.ml import MLDoc
