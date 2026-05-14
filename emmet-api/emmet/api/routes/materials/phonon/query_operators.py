b4b372ff4bf56bbf946da13137a49e1412354722
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2026-05-14 16:12:30 -0700

    updated phonon query operator for more generic 'identifier' id field

--- emmet-api/emmet/api/routes/materials/phonon/query_operators.py
+++ emmet-api/emmet/api/routes/materials/phonon/query_operators.py
@@ -1,9 +1,31 @@
 from __future__ import annotations
 
+from collections.abc import Callable
+from dataclasses import dataclass, field
+from functools import partial
+
 from fastapi import Query
 
-from emmet.api.query_operator import QueryOperator
-from emmet.api.utils import STORE_PARAMS
+from emmet.api.query_operator import InQuery, QueryOperator
+from emmet.api.utils import STORE_PARAMS, process_identifiers
+
+
+@dataclass
+class MultiPhononIDQuery(InQuery):
+    """Generate a query for different phonon ids."""
+
+    field_name: str = "identifier"
+    pre_processor: Callable[[str], list[str]] = field(
+        default=partial(process_identifiers, use_prefix=False)
+    )
+
+    def query(
+        self,
+        phonon_ids: str | None = Query(
+            None, description="Comma-separated list of phonon_ids to query on"
+        ),
+    ) -> STORE_PARAMS:
+        return self._prepare_query(phonon_ids)
 
 
 class PhononMethodQuery(QueryOperator):
