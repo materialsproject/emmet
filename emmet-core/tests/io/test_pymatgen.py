b274f0b65bc0733b7e1f82783888278159488650
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2026-05-14 13:56:43 -0700

    test that all imports work in ci

--- emmet-core/tests/io/test_pymatgen.py
+++ emmet-core/tests/io/test_pymatgen.py
@@ -1,3 +1,6 @@
+"""Test that pymatgen-related imports work."""
+
+import pytest
 from emmet.core.io.pymatgen import (
     _class_map,
     __dir__ as pmg_io_dir,
@@ -12,3 +15,20 @@ def test_dir():
         for name, base_import in _class_map.items()
     )
     assert all(isinstance(v, str) for v in pmg_list_dir)
+
+
+def test_bad_import():
+    with pytest.raises(ImportError, match="cannot import name"):
+        from emmet.core.io.pymatgen import foobar  # noqa: F401
+
+
+@pytest.mark.parametrize("object_name", sorted(_class_map))
+def test_imports(object_name: str):
+    """Test that all imports defined in the I/O layer work."""
+    import emmet.core.io.pymatgen as pmg_io_layer
+
+    try:
+        getattr(pmg_io_layer, object_name)
+    except (ImportError, AttributeError) as exc:
+        import_str = ".".join(["pymatgen", object_name, _class_map[object_name]])
+        pytest.fail(f"Import of {import_str!r} failed with exception {exc}")
